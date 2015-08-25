900dd2340f0a15e17b501a162f0a733187640312
zhaojohn <john.zhao@intel.com>
2015-08-25 10:35:11 -0700

    CHROMIUM: mmc: Hynix: add QUIRK_NOTIFY_POWEROFF_ON_SLEEP
    Hynix eMMC devices sometimes take 50% longer to resume from sleep.
    Based on a recommendation from Hynix, send a Power-Off Notification
    before going to S3 to restore a resume time consistently within spec.
    
    BUG=chrome-os-partner:42866
    TEST=run powerd_dbus_suspend and verify all the resume times.
    
    Change-Id: Ib7da97f7457848b0a405930e7630fef523428403
    Signed-off-by: zhaojohn <john.zhao@intel.com>
    Signed-off-by: Arindam Roy <arindam.roy@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/297826
    Commit-Ready: Freddy Paul <freddy.paul@intel.com>
    Tested-by: Freddy Paul <freddy.paul@intel.com>
    Reviewed-by: Icarus W Sparry <icarus.w.sparry@intel.com>
    Reviewed-by: Marc Herbert <marc.herbert@intel.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    
    Conflicts:
    	drivers/mmc/card/block.c
    
    [briannorris: 4.4 rebase. flag numbering and context conflicts]
    
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    
    Conflicts:
    	drivers/mmc/card/block.c
    	drivers/mmc/core/quirks.h
    
    [rebase412(groeck): flag numbering and context conflicts;
    	quirks moved from block.c to quirks.h]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -268,6 +268,8 @@ struct mmc_card {
 #define MMC_QUIRK_BROKEN_IRQ_POLLING	(1<<11)	/* Polling SDIO_CCCR_INTx could create a fake interrupt */
 #define MMC_QUIRK_TRIM_BROKEN	(1<<12)		/* Skip trim */
 #define MMC_QUIRK_BROKEN_HPI	(1<<13)		/* Disable broken HPI support */
+/* Chromium specifc quirk for Hynix EMMC */
+#define MMC_QUIRK_NOTIFY_POWEROFF_ON_SLEEP  (1<<14) /* Poweroff notification*/
 
 	bool			reenable_cmdq;	/* Re-enable Command Queue */
 
