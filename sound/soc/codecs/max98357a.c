dc25a48a6055dba1fab078d6793e445f8e7fddb9
Mac Chiang <mac.chiang@intel.com>
2016-03-22 13:49:15 +0800

    CHROMIUM: ASoC: max98357a: fix speaker pop when starting playback
    Loud speaker pop happens during pcm playback even a slience
    case. Specify Max98357a amp delay times in acpi table to make sure
    clocks are ready earlier than sdmode enabled.
    
    BUG=chrome-os-partner:55709
    TEST=check no 'pop' in below cases
    1. alt+ctl+t
    2. headphone plug/unplug during playback
    3. #sox -b 16 -n -t alsa hw:0,0 synth 3 sine 0 sine 0
    
    Signed-off-by: Mac Chiang <mac.chiang@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/334175
    Commit-Ready: Keith Tzeng <keith.tzeng@quantatw.com>
    Reviewed-by: Ben Zhang <benzh@google.com>
    
    (cherry picked from commit 6a750b95cf6354333d03dc024a280cb87dcce6b7)
    Changed cancel_delayed_work_sync to cancel_delayed_work to avoid sleeping
    while interrupts are disabled.
    
    Change-Id: I399c12161ff7fb493fafd40813d7546786dd07d0
    Signed-off-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/373019
    Commit-Ready: Wonjoon Lee <woojoo.lee@samsung.com>
    Reviewed-by: Ben Zhang <benzh@chromium.org>
    Conflicts:
    	sound/soc/codecs/max98357a.c
    [rebase49(groeck): Context conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- sound/soc/codecs/max98357a.c
+++ sound/soc/codecs/max98357a.c
@@ -27,24 +27,43 @@
 #include <sound/soc-dai.h>
 #include <sound/soc-dapm.h>
 
+struct max98357a_priv {
+	struct delayed_work enable_sdmode_work;
+	struct gpio_desc *sdmode;
+	unsigned int sdmode_delay;
+};
+
+static void max98357a_enable_sdmode_work(struct work_struct *work)
+{
+	struct max98357a_priv *max98357a =
+	container_of(work, struct max98357a_priv,
+			enable_sdmode_work.work);
+
+	gpiod_set_value(max98357a->sdmode, 1);
+}
+
 static int max98357a_daiops_trigger(struct snd_pcm_substream *substream,
 		int cmd, struct snd_soc_dai *dai)
 {
-	struct gpio_desc *sdmode = snd_soc_dai_get_drvdata(dai);
+	struct snd_soc_codec *codec = dai->codec;
+	struct max98357a_priv *max98357a = snd_soc_codec_get_drvdata(codec);
 
-	if (!sdmode)
+	if (!max98357a->sdmode)
 		return 0;
 
 	switch (cmd) {
 	case SNDRV_PCM_TRIGGER_START:
 	case SNDRV_PCM_TRIGGER_RESUME:
 	case SNDRV_PCM_TRIGGER_PAUSE_RELEASE:
-		gpiod_set_value(sdmode, 1);
+		queue_delayed_work(system_power_efficient_wq,
+				&max98357a->enable_sdmode_work,
+				msecs_to_jiffies(max98357a->sdmode_delay));
 		break;
 	case SNDRV_PCM_TRIGGER_STOP:
 	case SNDRV_PCM_TRIGGER_SUSPEND:
 	case SNDRV_PCM_TRIGGER_PAUSE_PUSH:
-		gpiod_set_value(sdmode, 0);
+		cancel_delayed_work(&max98357a->enable_sdmode_work);
+		gpiod_set_value(max98357a->sdmode, 0);
 		break;
 	}
 
@@ -61,19 +80,34 @@ static const struct snd_soc_dapm_route max98357a_dapm_routes[] = {
 
 static int max98357a_codec_probe(struct snd_soc_codec *codec)
 {
-	struct gpio_desc *sdmode;
+	struct max98357a_priv *max98357a = snd_soc_codec_get_drvdata(codec);
 
-	sdmode = devm_gpiod_get_optional(codec->dev, "sdmode", GPIOD_OUT_LOW);
-	if (IS_ERR(sdmode))
-		return PTR_ERR(sdmode);
+	max98357a->sdmode = devm_gpiod_get_optional(codec->dev,
+						"sdmode", GPIOD_OUT_LOW);
 
-	snd_soc_codec_set_drvdata(codec, sdmode);
+	if (IS_ERR(max98357a->sdmode))
+		return PTR_ERR(max98357a->sdmode);
+
+	snd_soc_codec_set_drvdata(codec, max98357a);
+
+	INIT_DELAYED_WORK(&max98357a->enable_sdmode_work,
+				max98357a_enable_sdmode_work);
+
+	return 0;
+}
+
+static int max98357a_codec_remove(struct snd_soc_codec *codec)
+{
+	struct max98357a_priv *max98357a = snd_soc_codec_get_drvdata(codec);
+
+	cancel_delayed_work_sync(&max98357a->enable_sdmode_work);
 
 	return 0;
 }
 
 static const struct snd_soc_codec_driver max98357a_codec_driver = {
 	.probe			= max98357a_codec_probe,
+	.remove			= max98357a_codec_remove,
 	.component_driver = {
 		.dapm_widgets		= max98357a_dapm_widgets,
 		.num_dapm_widgets	= ARRAY_SIZE(max98357a_dapm_widgets),
@@ -107,6 +141,25 @@ static struct snd_soc_dai_driver max98357a_dai_driver = {
 
 static int max98357a_platform_probe(struct platform_device *pdev)
 {
+	struct max98357a_priv *max98357a;
+	int ret;
+
+	max98357a = devm_kzalloc(&pdev->dev, sizeof(*max98357a), GFP_KERNEL);
+
+	if (!max98357a)
+		return -ENOMEM;
+
+	ret = device_property_read_u32(&pdev->dev, "sdmode-delay",
+			&max98357a->sdmode_delay);
+
+	if (ret) {
+		max98357a->sdmode_delay = 0;
+		dev_dbg(&pdev->dev,
+			"no optional property 'sdmode-delay' found, default: no delay\n");
+	}
+
+	dev_set_drvdata(&pdev->dev, max98357a);
+
 	return snd_soc_register_codec(&pdev->dev, &max98357a_codec_driver,
 			&max98357a_dai_driver, 1);
 }
