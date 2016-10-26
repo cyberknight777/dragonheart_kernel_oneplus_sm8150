f172928b31d60db27f02f7824a6f0714973d6f8f
Douglas Anderson <dianders@chromium.org>
2016-10-26 15:57:09 -0700

    CHROMIUM: mfd: cros_ec: Don't try to grab log when suspended
    We should stop our worker thread while we're suspended.  If we don't
    then we'll get messages like:
    
      cros-ec-spi spi5.0: spi transfer failed: -108
      cros-ec-spi spi5.0: cs-deassert spi transfer failed: -108
      cros-ec-ctl cros-ec-ctl.0.auto: EC communication failed
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    (cherry picked from commit 6a89a0d3a44ac7c2bbc1d1a5c1c87ec7bf88383b)
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- drivers/platform/chrome/cros_ec_dev.c
+++ drivers/platform/chrome/cros_ec_dev.c
@@ -311,6 +311,7 @@ static __maybe_unused int ec_device_suspend(struct device *dev)
 	struct cros_ec_dev *ec = dev_get_drvdata(dev);
 
 	lb_suspend(ec);
+	cros_ec_debugfs_suspend(ec);
 
 	return 0;
 }
@@ -320,6 +321,8 @@ static __maybe_unused int ec_device_resume(struct device *dev)
 	struct cros_ec_dev *ec = dev_get_drvdata(dev);
 
 	lb_resume(ec);
+
+	cros_ec_debugfs_resume(ec);
 
 	return 0;
 }
