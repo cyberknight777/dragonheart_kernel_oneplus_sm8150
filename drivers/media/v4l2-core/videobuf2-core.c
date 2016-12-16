c299b5a92a7da1a088ea1c566c920f0595ff8be5
Sakari Ailus <sakari.ailus@linux.intel.com>
2016-12-16 03:24:17 +0200

    FROMLIST: vb2: Move cache synchronisation from buffer done to dqbuf handler
    The cache synchronisation may be a time consuming operation and thus not
    best performed in an interrupt which is a typical context for
    vb2_buffer_done() calls. This may consume up to tens of ms on some
    machines, depending on the buffer size.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    
    (am from https://patchwork.kernel.org/patch/9477165/)
    Signed-off-by: Ricky Liang <jcliang@chromium.org>
    
    BUG=chrome-os-partner:60805
    TEST=build chromeos-4.4 for elm and boot
    TEST=build chromeos-4.4 for kevin and boot
    
    BUG=chrome-os-partner:43703
    TEST=Compiled and tested on Elm.
    
    Reviewed-on: https://chromium-review.googlesource.com/422719
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    
    Change-Id: I6b454f461ad57d0fa80c32aa4ec45d71e881435a
    Reviewed-on: https://chromium-review.googlesource.com/427575
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>

--- drivers/media/v4l2-core/videobuf2-core.c
+++ drivers/media/v4l2-core/videobuf2-core.c
@@ -863,7 +863,6 @@ void vb2_buffer_done(struct vb2_buffer *vb, enum vb2_buffer_state state)
 {
 	struct vb2_queue *q = vb->vb2_queue;
 	unsigned long flags;
-	unsigned int plane;
 
 	if (WARN_ON(vb->state != VB2_BUF_STATE_ACTIVE))
 		return;
@@ -883,10 +882,6 @@ void vb2_buffer_done(struct vb2_buffer *vb, enum vb2_buffer_state state)
 #endif
 	dprintk(4, "done processing on buffer %d, state: %d\n",
 			vb->index, state);
-
-	/* sync buffers */
-	for (plane = 0; plane < vb->num_planes; ++plane)
-		call_void_memop(vb, finish, vb->planes[plane].mem_priv);
 
 	spin_lock_irqsave(&q->done_lock, flags);
 	if (state == VB2_BUF_STATE_QUEUED ||
@@ -1518,6 +1513,10 @@ static void __vb2_dqbuf(struct vb2_buffer *vb)
 		return;
 
 	vb->state = VB2_BUF_STATE_DEQUEUED;
+
+	/* sync buffers */
+	for (i = 0; i < vb->num_planes; ++i)
+		call_void_memop(vb, finish, vb->planes[i].mem_priv);
 
 	if (q->memory == VB2_MEMORY_USERPTR) {
 		call_void_vb_qop(vb, buf_cleanup, vb);
