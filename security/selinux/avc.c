18c310bf1a8c55cede1d4601c54ebfff72edd8b8
Luis Hector Chavez <lhchavez@google.com>
2017-01-05 16:39:59 -0800

    CHROMIUM: Add CONFIG_SECURITY_SELINUX_PERMISSIVE_DONTAUDIT
    It's still useful to have SECURITY_SELINUX_DEVELOP=y by default so that
    external developers can set their machines to developer mode and set
    permissive SELinux without having to re-build their kernels, but it's
    still useful to have it not spam the logs by default due to Chrome OS'
    large number of permissive rules.  This change adds a new flag that lets
    the user control whether or not to log permissive "denials".
    
    Signed-off-by: Luis Hector Chavez <lhchavez@google.com>
    
    BUG=b:33437013
    TEST=build with USE=selinux_develop, see lots of logs
    
    Change-Id: I53a877ba99ebc4eb040cd7b8c916b0bc3ba48a73
    Reviewed-on: https://chromium-review.googlesource.com/424948
    Commit-Ready: Luis Hector Chavez <lhchavez@chromium.org>
    Tested-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    (cherry picked from commit fceee0e3f5cb71f40ccda379bbc2dc6265123fb4)
    Reviewed-on: https://chromium-review.googlesource.com/425786
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -742,9 +742,14 @@ noinline int slow_avc_audit(u32 ssid, u32 tsid, u16 tclass,
 	struct common_audit_data stack_data;
 	struct selinux_audit_data sad;
 
-	/* Only log permissive=1 messages for SECURITY_SELINUX_DEVELOP */
-	if (!IS_ENABLED(CONFIG_SECURITY_SELINUX_DEVELOP) && denied && !result)
+	/*
+	 * Avoid logging permissive=1 messages for
+	 * SECURITY_SELINUX_PERMISSIVE_DONTAUDIT.
+	 */
+	if (IS_ENABLED(CONFIG_SECURITY_SELINUX_PERMISSIVE_DONTAUDIT) && denied
+	    && !result) {
 		return 0;
+	}
 
 	if (!a) {
 		a = &stack_data;
