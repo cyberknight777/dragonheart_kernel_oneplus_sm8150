8b14fae04dd9a80c5825e2c8348a652f11f28004
Boris Ostrovsky <boris.ostrovsky@oracle.com>
2017-09-11 14:51:11 -0400

    UPSTREAM: x86/timers: Move simple_udelay_calibration() past kvmclock_init()
    simple_udelay_calibration() relies on x86_platform's calibration ops.
    For KVM these ops are set late in setup_arch() and so
    simple_udelay_calibration() ends up using native version.
    
    Besides being possibly incorrect, this significantly increases kernel
    boot time. For example, on my laptop executing start_kernel() by a guest
    takes ~10 times more than when KVM's ops are used.
    
    Since early_xdbc_setup_hardware() relies on calibration having been
    performed move it too.
    
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: baolu.lu@linux.intel.com
    Link: https://lkml.kernel.org/r/20170911185111.20636-1-boris.ostrovsky@oracle.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    (cherry picked from commit ccb64941f375a6eb21b1b20136730eb7d1716068)
    
    BUG=b:112103715, b:112159884
    TEST=Verified that no hang is observed on yorp for 5000+ warm reboots
    
    Change-Id: Ib24b919421b24364eb7419c49f3a4d6646ad9034
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1214026
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Yu Zhao <yuzhao@chromium.org>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1026,8 +1026,6 @@ void __init setup_arch(char **cmdline_p)
 	 */
 	init_hypervisor_platform();
 
-	tsc_early_delay_calibrate();
-
 	x86_init.resources.probe_roms();
 
 	/* after parse_early_param, so could debug it */
@@ -1111,9 +1109,6 @@ void __init setup_arch(char **cmdline_p)
 
 	memblock_set_current_limit(ISA_END_ADDRESS);
 	e820__memblock_setup();
-
-	if (!early_xdbc_setup_hardware())
-		early_xdbc_register_console();
 
 	reserve_bios_regions();
 
@@ -1219,6 +1214,10 @@ void __init setup_arch(char **cmdline_p)
 #ifdef CONFIG_KVM_GUEST
 	kvmclock_init();
 #endif
+
+	tsc_early_delay_calibrate();
+	if (!early_xdbc_setup_hardware())
+		early_xdbc_register_console();
 
 	x86_init.paging.pagetable_init();
 
