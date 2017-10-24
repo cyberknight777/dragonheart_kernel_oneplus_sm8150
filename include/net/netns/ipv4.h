305854be2c96ae1ae5fa677c8bb5184d2450f471
Kirtika Ruchandani <kirtika@chromium.org>
2017-10-24 14:59:21 -0600

    CHROMIUM: net: Add per-netns tcp_default_init_rwnd
    tcp_default_init_rwnd is currently not visible on a per-net-namespace
    basis. Different OSes uses different values for this file and the TCP
    receive window, make this visible separately to each net namespace.
    
    BUG=b/27932956
    TEST=Ran on samus-cheets, Confirmed that tcp_default_init_rwnd can be tuned
    differently in each namespace with the following steps:
    $ ip netns add blue # Add a net namespace called blue
    $ ip netns exec blue cat /proc/sys/ipv4/tcp_default_init_rwnd # Should be 20 for CrOS
    $ echo 60 > /proc/sys/ipv4/tcp_default_init_rwnd # Make it 60 outside blue namespace
    Then confirm that the values are different outside and inside blue with the
    below:
    $ cat  /proc/sys/ipv4/tcp_default_init_rwnd # Returns 60
    $ ip netns exec blue cat /proc/sys/ipv4/tcp_default_init_rwnd # Returns 20
    
    (cherry picked from commit 97ef28830ca0499f908ba7bc5e6e7049ae2f3816)
    
    Signed-off-by: Kirtika Ruchandani <kirtika@chromium.org>
    Change-Id: I24482a4d4bfd6229293f4ddf457fa924cf0b01a3
    Reviewed-on: https://chromium-review.googlesource.com/362695
    Commit-Ready: Kirtika Ruchandani <kirtika@google.com>
    Tested-by: Kirtika Ruchandani <kirtika@google.com>
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    
    Conflicts:
    	include/net/netns/ipv4.h
    	net/ipv4/sysctl_net_ipv4.c
    	net/ipv4/tcp_output.c
    
    [rebase412(groeck): Resolved conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    
    Conflicts:
    	net/ipv4/sysctl_net_ipv4.c
    	net/ipv4/tcp_output.c
    [rebase414(lannm): Resolved conflicts]
    Signed-off-by: Lann Martin <lannm@chromium.org>

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -103,6 +103,7 @@ struct netns_ipv4 {
 #ifdef CONFIG_NET_L3_MASTER_DEV
 	int sysctl_tcp_l3mdev_accept;
 #endif
+	int sysctl_tcp_default_init_rwnd;
 	int sysctl_tcp_mtu_probing;
 	int sysctl_tcp_base_mss;
 	int sysctl_tcp_probe_threshold;
