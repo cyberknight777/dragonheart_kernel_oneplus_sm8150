bbc6b19141fdd43db06b7aebc1f734861721854a
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2017-11-02 10:02:04 +0200

    CHROMIUM: iwl7000: print the version number of the firmware in hex
    Starting from a version 35, the minor version should be
    printed in hexa.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: c7d768585cae843deb9fec548887a6f8ea94fdba

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-drv.c
@@ -1162,9 +1162,14 @@ fw_dbg_conf:
 			minor = le32_to_cpup(ptr++);
 			local_comp = le32_to_cpup(ptr);
 
-			snprintf(drv->fw.fw_version,
-				 sizeof(drv->fw.fw_version), "%u.%u.%u",
-				 major, minor, local_comp);
+			if (major >= 35)
+				snprintf(drv->fw.fw_version,
+					 sizeof(drv->fw.fw_version),
+					"%u.%08x.%u", major, minor, local_comp);
+			else
+				snprintf(drv->fw.fw_version,
+					 sizeof(drv->fw.fw_version),
+					"%u.%u.%u", major, minor, local_comp);
 			break;
 			}
 		case IWL_UCODE_TLV_FW_DBG_DEST: {
