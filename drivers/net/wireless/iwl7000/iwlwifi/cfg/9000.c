e4813dd6827390268fc584407524c47d5cd6fd92
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2017-11-15 12:39:08 +0200

    CHROMIUM: iwl7000: bump MAX API to 36 for supporting devices
    8000, 9000 and a000 are now able to load -36.ucode
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    iwl7000-tree: e0d99c71d92c2a42bdf5af1f81aedcd3eca9b16b

--- drivers/net/wireless/iwl7000/iwlwifi/cfg/9000.c
+++ drivers/net/wireless/iwl7000/iwlwifi/cfg/9000.c
@@ -55,7 +55,7 @@
 #include "iwl-agn-hw.h"
 
 /* Highest firmware API version supported */
-#define IWL9000_UCODE_API_MAX	35
+#define IWL9000_UCODE_API_MAX	36
 
 /* Lowest firmware API version supported */
 #define IWL9000_UCODE_API_MIN	30
