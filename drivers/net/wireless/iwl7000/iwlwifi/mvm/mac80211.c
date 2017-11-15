b3ee3a82c1e2792025713b5275a73517127b07c7
Luca Coelho <luciano.coelho@intel.com>
2017-11-15 15:07:34 +0200

    CHROMIUM: iwl7000: rename the temporary name of A000 to the official 22000
    The family name A000 was just a place-holder when we didn't know what
    the official name would be yet.  Now we know that the family name is
    22000, so rename all occurrences accordingly.
    
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 9b77efa4a2ae0db750d0309ac123bd25f77d9891

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
@@ -4759,9 +4759,9 @@ void iwl_mvm_sync_rx_queues_internal(struct iwl_mvm *mvm,
 
 	lockdep_assert_held(&mvm->mutex);
 
-	/* TODO - remove a000 disablement when we have RXQ config API */
+	/* TODO - remove 22000 disablement when we have RXQ config API */
 	if (!iwl_mvm_has_new_rx_api(mvm) ||
-	    mvm->trans->cfg->device_family == IWL_DEVICE_FAMILY_A000)
+	    mvm->trans->cfg->device_family == IWL_DEVICE_FAMILY_22000)
 		return;
 
 	notif->cookie = mvm->queue_sync_cookie;
