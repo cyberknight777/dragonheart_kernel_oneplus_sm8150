954bdef9a306f83fa94cc751a78ccd21ef60295f
Thierry Escande <thierry.escande@collabora.com>
2017-11-20 17:15:26 +0100

    UPSTREAM: cros_ec: Move cros_ec_dev module to drivers/mfd
    The cros_ec_dev module is responsible for registering the MFD devices
    attached to the ChromeOS EC. This patch moves this module to drivers/mfd
    so calls to mfd_add_devices() are not done from outside the MFD subtree
    anymore.
    
     Conflicts:
    	drivers/platform/chrome/Kconfig
    
    BUG=chromium:785506
    TEST=Using the whole suite, check on eve,cyan,glimmer AIDA64 works.
    Check on pixel 2 usb-pd-charger exits.
    
    Signed-off-by: Thierry Escande <thierry.escande@collabora.com>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Guenter Roeck <groeck@chromium.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    (cherry picked from commit 5e0115581bbc367c7958bf5ab8c511b808558533)
    [Fix Kconfig conflict]
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    
    Change-Id: I69d7401036b9ff53fd24ab8aa6cb1ef0d466cb2c

--- include/linux/mfd/cros_ec.h
+++ include/linux/mfd/cros_ec.h
@@ -322,5 +322,9 @@ extern struct attribute_group cros_ec_attr_group;
 extern struct attribute_group cros_ec_lightbar_attr_group;
 extern struct attribute_group cros_ec_vbc_attr_group;
 
+/* debugfs stuff */
+int cros_ec_debugfs_init(struct cros_ec_dev *ec);
+void cros_ec_debugfs_remove(struct cros_ec_dev *ec);
+
 
 #endif /* __LINUX_MFD_CROS_EC_H */
