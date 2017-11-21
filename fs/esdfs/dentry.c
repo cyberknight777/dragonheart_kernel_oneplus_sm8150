6d17fb1ad14a604865ba2580e07b07be2791535c
Daniel Rosenberg <drosen@google.com>
2017-11-20 19:48:40 -0800

    CHROMIUM: esdfs: drop dentries on d_delete
    There was a deadlock in the existing code to deal with
    multiple views, as the inode lock is already held when
    looking over the other superblocks. If this happens on
    the same inode on two different views, they will deadlock.
    
    Instead of cleaning up other views on rmdir/unlink, always
    drop the dentries when they aren't in active use.
    
    BUG=b:63876697
    TEST=compilation, mount esdfs
    
    Change-Id: If860b4d3bf8d4492c6f78c0564b6f4f2a7bb285d
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/887839
    Commit-Ready: Sarthak Kukreti <sarthakkukreti@chromium.org>
    Tested-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    (cherry picked from commit 72cd907dd189394d0433267d1ce29fc3e6dfa075)
    Reviewed-on: https://chromium-review.googlesource.com/963471

--- fs/esdfs/dentry.c
+++ fs/esdfs/dentry.c
@@ -90,6 +90,12 @@ out:
 	return err;
 }
 
+/* 1 = delete, 0 = cache */
+int esdfs_d_delete(const struct dentry *d)
+{
+	return 0;
+}
+
 /* directly from fs/fat/namei_vfat.c */
 static unsigned int __vfat_striptail_len(unsigned int len, const char *name)
 {
@@ -156,6 +162,7 @@ static void esdfs_canonical_path(const struct path *path,
 
 const struct dentry_operations esdfs_dops = {
 	.d_revalidate	= esdfs_d_revalidate,
+	.d_delete	= esdfs_d_delete,
 	.d_hash		= esdfs_d_hash,
 	.d_compare	= esdfs_d_compare,
 	.d_release	= esdfs_d_release,
