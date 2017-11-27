bff124c54ce471a5619e5e37c5e9619e65c63204
Christian König <christian.koenig@amd.com>
2017-11-27 13:12:35 +0100

    UPSTREAM: drm/ttm: fix populate_and_map() functions once more
    This reverts "drm/ttm: Fix configuration error around populate_and_map()
    functions".
    
    This fix has gone into the wrong direction. Those helpers should be
    available even when neither CONFIG_INTEL_IOMMU nor CONFIG_SWIOTLB are
    set.
    
    BUG=b:72048470
    TEST=boot Kahlee, run graphics_SanAngeles
    
    Signed-off-by: Christian Knig <christian.koenig@amd.com>
    Reviewed-by: Michel Dnzer <michel.daenzer@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 1569d651f152870663fabd8f1c80af353f967ad5)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    
    Change-Id: I8208ff490fbba057ac3c8b432a196b0a4b3d982a
    Reviewed-on: https://chromium-review.googlesource.com/1012671
    Commit-Ready: Dominik Behr <dbehr@google.com>
    Tested-by: Dominik Behr <dbehr@google.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/ttm/ttm_page_alloc.c
+++ drivers/gpu/drm/ttm/ttm_page_alloc.c
@@ -1064,7 +1064,6 @@ void ttm_pool_unpopulate(struct ttm_tt *ttm)
 }
 EXPORT_SYMBOL(ttm_pool_unpopulate);
 
-#if defined(CONFIG_SWIOTLB) || defined(CONFIG_INTEL_IOMMU)
 int ttm_populate_and_map_pages(struct device *dev, struct ttm_dma_tt *tt)
 {
 	unsigned i, j;
@@ -1135,7 +1134,6 @@ void ttm_unmap_and_unpopulate_pages(struct device *dev, struct ttm_dma_tt *tt)
 	ttm_pool_unpopulate(&tt->ttm);
 }
 EXPORT_SYMBOL(ttm_unmap_and_unpopulate_pages);
-#endif
 
 int ttm_page_alloc_debugfs(struct seq_file *m, void *data)
 {
