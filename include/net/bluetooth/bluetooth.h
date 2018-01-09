5d024743f429a62d82174c511c6f28088b31f329
Joseph Hwang <josephsih@chromium.org>
2018-01-09 15:26:37 +0800

    CHROMIUM: bluetooth: include file and function names in logs
    BUG=none
    TEST=Examine the logs in /var/log/messages. The bluetooth kernel logs
    should include the file names and function names as follows:
    
    DEBUG kernel: [66660.783602] hci_request.c:req_run() length 3
    
    Change-Id: I987f2cc6e9a2a8d85f2ff26a0dd527b07c103f77
    Signed-off-by: Joseph Hwang <josephsih@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/856383
    Commit-Ready: Shyh-In Hwang <josephsih@chromium.org>
    Tested-by: Shyh-In Hwang <josephsih@chromium.org>
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    (cherry picked from commit 376193db7931fbb248edad3cd0306c00abd6fbb1)
    Reviewed-on: https://chromium-review.googlesource.com/859658

--- include/net/bluetooth/bluetooth.h
+++ include/net/bluetooth/bluetooth.h
@@ -131,10 +131,21 @@ void bt_err(const char *fmt, ...);
 __printf(1, 2)
 void bt_err_ratelimited(const char *fmt, ...);
 
-#define BT_INFO(fmt, ...)	bt_info(fmt "\n", ##__VA_ARGS__)
-#define BT_WARN(fmt, ...)	bt_warn(fmt "\n", ##__VA_ARGS__)
-#define BT_ERR(fmt, ...)	bt_err(fmt "\n", ##__VA_ARGS__)
-#define BT_DBG(fmt, ...)	pr_debug(fmt "\n", ##__VA_ARGS__)
+static inline const char *basename(const char *path)
+{
+	const char *str = strrchr(path, '/');
+
+	return str ? (str + 1) : path;
+}
+
+#define BT_INFO(fmt, ...)	bt_info("%s:%s() " fmt "\n",		\
+				basename(__FILE__), __func__, ##__VA_ARGS__)
+#define BT_WARN(fmt, ...)	bt_warn("%s:%s() " fmt "\n",		\
+				basename(__FILE__), __func__, ##__VA_ARGS__)
+#define BT_ERR(fmt, ...)	bt_err("%s:%s() " fmt "\n",		\
+				basename(__FILE__), __func__, ##__VA_ARGS__)
+#define BT_DBG(fmt, ...)	pr_debug("%s:%s() " fmt "\n",		\
+				basename(__FILE__), __func__, ##__VA_ARGS__)
 
 #define BT_ERR_RATELIMITED(fmt, ...) bt_err_ratelimited(fmt "\n", ##__VA_ARGS__)
 
