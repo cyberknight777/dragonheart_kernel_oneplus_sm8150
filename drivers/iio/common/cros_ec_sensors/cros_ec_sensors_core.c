ba757906ab6c869f7cb30d6f7a9975dc115b1791
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2018-01-11 12:19:19 +0100

    Revert "CHROMIUM: iio: cros_ec: Fix a kernel panic when processing first ring message"
    This reverts commit 37a68af97ca63b98e529a60a2d955e71a5ab4d57.
    
    BUG=chromium:785506
    TEST=Using the whole suite, check on eve,cyan,glimmer AIDA64 works.
    Check on pixel 2 usb-pd-charger exits.
    
    Change-Id: I130b45689b5a95a66e09bacfd36fcba0398c439b
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- drivers/iio/common/cros_ec_sensors/cros_ec_sensors_core.c
+++ drivers/iio/common/cros_ec_sensors/cros_ec_sensors_core.c
@@ -120,7 +120,7 @@ int cros_ec_sensors_core_init(struct platform_device *pdev,
 	platform_set_drvdata(pdev, indio_dev);
 
 	state->ec = ec_dev;
-	state->indio_dev = indio_dev;
+
 	state->msg = devm_kzalloc(&pdev->dev,
 				max((u16)sizeof(struct ec_params_motion_sense),
 				state->ec->max_response), GFP_KERNEL);
