375ba551d446af83937acc6acc260d9d559f5b3f
Joseph Hwang <josephsih@chromium.org>
2018-01-18 15:56:11 +0800

    CHROMIUM: bluetooth: clean up connection in hci_cs_disconnect
    In bluetooth core specification 4.2,
    Vol 2, Part E, 7.8.9 LE Set Advertise Enable Command, it says
    
        The Controller shall continue advertising until ...
        or until a connection is created or ...
        In these cases, advertising is then disabled.
    
    Hence, advertising would be disabled before a connection is
    established. In current kernel implementation, advertising would
    be re-enabled when all connections are terminated.
    
    The correct disconnection flow looks like
    
      < HCI Command: Disconnect
    
      > HCI Event: Command Status
          Status: Success
    
      > HCI Event: Disconnect Complete
          Status: Success
    
    Specifically, the last Disconnect Complete Event would trigger a
    callback function hci_event.c:hci_disconn_complete_evt() to
    cleanup the connection and re-enable advertising when proper.
    
    However, sometimes, there might occur an exception in the controller
    when disconnection is being executed. The disconnection flow might
    then look like
    
      < HCI Command: Disconnect
    
      > HCI Event: Command Status
          Status: Unknown Connection Identifier
    
      Note that "> HCI Event: Disconnect Complete" is missing when such an
    exception occurs. This would result in advertising staying disabled
    forever since the connection in question is not cleaned up correctly.
    
    To fix the controller exception issue, we need to do some connection
    cleanup when the disconnect command status indicates an error.
    
    BUG=b:70903583
    TEST=Run magic tethering without a SIM card.
    Sometimes, an "Unknown Connection Identifier" controller error would
    show up in the disconnect command status event in the btsnoop log.
    This would end up with the missing of Disconnect Complete Event
    which looks like
    
    < HCI Command: Disconnect (0x01|0x0006) plen 3 [hci0] 17:23:55.037679
            Handle: 3585
            Reason: Remote User Terminated Connection (0x13)
    > HCI Event: Command Status (0x0f) plen 4      [hci0] 17:23:55.038612
          Disconnect (0x01|0x0006) ncmd 1
            Status: Unknown Connection Identifier (0x02)
    @ MGMT Event: Command Compl.. (0x0001) plen 10 [hci0] 17:23:55.038663
          Disconnect (0x0014) plen 7
            Status: Not Connected (0x02)
            LE Address: 76:1D:6E:C2:5B:AC (Resolvable)
    
    However, observe that the advertising would still be re-enabled
    as follows
    
    < HCI Command: LE Set Advertise... (0x08|0x000a) [hci0] 17:23:55.141659
          Advertising: Enabled (0x01)
    
    Change-Id: Id25cec6532ade569a87e7c6f24061219855e08a6
    Signed-off-by: Joseph Hwang <josephsih@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1053041
    Commit-Ready: Shyh-In Hwang <josephsih@chromium.org>
    Tested-by: Shyh-In Hwang <josephsih@chromium.org>
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1648863
    Commit-Ready: Yoni Shavit <yshavit@chromium.org>
    Tested-by: Yoni Shavit <yshavit@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -1915,6 +1915,7 @@ static void hci_cs_disconnect(struct hci_dev *hdev, u8 status)
 {
 	struct hci_cp_disconnect *cp;
 	struct hci_conn *conn;
+	u8 type;
 
 	if (!status)
 		return;
@@ -1926,9 +1927,29 @@ static void hci_cs_disconnect(struct hci_dev *hdev, u8 status)
 	hci_dev_lock(hdev);
 
 	conn = hci_conn_hash_lookup_handle(hdev, __le16_to_cpu(cp->handle));
-	if (conn)
+	if (conn) {
 		mgmt_disconnect_failed(hdev, &conn->dst, conn->type,
 				       conn->dst_type, status);
+
+		/* If the disconnection failed for any reason, the upper layer
+		 * does not retry to disconnect in current implementation.
+		 * Hence, we need to do some basic cleanup here.
+		 * TODO(b/72355862): Intel to fix the controller firmware.
+		 * The disconnect failure occurs sometimes on Intel 7265
+		 * controller as follows:
+		 *     > HCI Event: Command Status (0x0f) plen 4
+		 *         Disconnect (0x01|0x0006) ncmd 1
+		 *           Status: Unknown Connection Identifier (0x02)
+		 */
+		BT_DBG("Do some disconnect cleanup.");
+
+		type = conn->type;
+		hci_conn_del(conn);
+		if (type == LE_LINK)
+			hci_req_reenable_advertising(hdev);
+	} else {
+		BT_DBG("The connection handle cannot be found.");
+	}
 
 	hci_dev_unlock(hdev);
 }
