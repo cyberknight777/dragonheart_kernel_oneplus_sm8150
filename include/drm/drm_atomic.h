5a4b1f408af2037e26ab4a2e138e8ea1320d0506
Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
2018-02-02 21:12:59 -0800

    UPSTREAM: drm/atomic: Handle 64-bit return from drm_crtc_vblank_count()
    570e86963a51 ("drm: Widen vblank count to 64-bits [v3]") changed the
    return type for drm_crtc_vblank_count() to u64.
    
    The flip ioctl receives a 32-bit target sequence from user space and is
    compared against the current sequence from drm_crtc_vblank_count(). So,
    typecast return from drm_crtc_vblank_count() explicitly to add clarity.
    
    __drm_crtcs_state.last_vblank_count however only ever stores the value from
    drm_crtc_vblank_count() and can be upgraded to u64.
    
    BUG=b:112542653
    TEST=boot Grunt, run graphics_SanAngeles and login_LoginSuccess
    
    Cc: Keith Packard <keithp@keithp.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    Reviewed-by: Keith Packard <keithp@keithp.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180203051302.9974-7-dhinakaran.pandiyan@intel.com
    (cherry picked from commit f4c0468e4bd64b7cd00e2db309925728fc72bf8b)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    
    Change-Id: Id6cdc416f084412d45036b95e4a14202791c9acf

--- include/drm/drm_atomic.h
+++ include/drm/drm_atomic.h
@@ -165,7 +165,7 @@ struct __drm_crtcs_state {
 	struct drm_crtc_commit *commit;
 
 	s32 __user *out_fence_ptr;
-	unsigned last_vblank_count;
+	u64 last_vblank_count;
 };
 
 struct __drm_connnectors_state {
