be3d5a1fc216a170ea6e35a423c802c1c07a2c26
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-02-08 14:56:39 +0900

    UPSTREAM: kconfig: remove check_stdin()
    Except silentoldconfig, valid_stdin is 1, so check_stdin() is no-op.
    
    oldconfig and silentoldconfig work almost in the same way except that
    the latter generates additional files under include/.  Both ask users
    for input for new symbols.
    
    I do not know why only silentoldconfig requires stdio be tty.
    
      $ rm -f .config; touch .config
      $ yes "" | make oldconfig > stdout
      $ rm -f .config; touch .config
      $ yes "" | make silentoldconfig > stdout
      make[1]: *** [silentoldconfig] Error 1
      make: *** [silentoldconfig] Error 2
      $ tail -n 4 stdout
      Console input/output is redirected. Run 'make oldconfig' to update configuration.
    
      scripts/kconfig/Makefile:40: recipe for target 'silentoldconfig' failed
      Makefile:507: recipe for target 'silentoldconfig' failed
    
    Redirection is useful, for example, for testing where we want to give
    particular key inputs from a test file, then check the result.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Ulf Magnusson <ulfalizer@gmail.com>
    (cherry picked from commit d2a04648a5dbc3d1d043b35257364f0197d4d868)
    
    BUG=chromium:947697
    TEST=build; cd to portage cache; cp cros-old-config cros-old-defconfig;
         build
    
    Change-Id: Ib2ffe4b63e3eceacc64f523b571a66ebf570d189
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1548069
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -38,7 +38,6 @@ enum input_mode {
 
 static int indent = 1;
 static int tty_stdio;
-static int valid_stdin = 1;
 static int sync_kconfig;
 static int conf_cnt;
 static char line[PATH_MAX];
@@ -69,16 +68,6 @@ static void strip(char *str)
 	p = str + l - 1;
 	while ((isspace(*p)))
 		*p-- = 0;
-}
-
-static void check_stdin(void)
-{
-	if (!valid_stdin) {
-		printf(_("aborted!\n\n"));
-		printf(_("Console input/output is redirected. "));
-		printf(_("Run 'make oldconfig' to update configuration.\n\n"));
-		exit(1);
-	}
 }
 
 /* Helper function to facilitate fgets() by Jean Sacren. */
@@ -112,7 +101,6 @@ static int conf_askvalue(struct symbol *sym, const char *def)
 			printf("%s\n", def);
 			return 0;
 		}
-		check_stdin();
 		/* fall through */
 	case oldaskconfig:
 		fflush(stdout);
@@ -314,7 +302,6 @@ static int conf_choice(struct menu *menu)
 				printf("%d\n", cnt);
 				break;
 			}
-			check_stdin();
 			/* fall through */
 		case oldaskconfig:
 			fflush(stdout);
@@ -648,7 +635,6 @@ int main(int ac, char **av)
 				return 1;
 			}
 		}
-		valid_stdin = tty_stdio;
 	}
 
 	switch (input_mode) {
