7712d7f449e8038a2ed4d50b94092f569f5bab85
Golan Ben Ami <golan.ben.ami@intel.com>
2018-02-14 18:25:20 +0200

    CHROMIUM: iwl7000: stop device's busmaster DMA activity correctly
    Fix the register addresses and values for stopping the busmaster
    DMA activity.
    
    Signed-off-by: Golan Ben Ami <golan.ben.ami@intel.com>
    automatic-review: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 5171edac89779c9abb3de292cc49a4b5ea0e57d5

--- drivers/net/wireless/iwl7000/iwlwifi/pcie/trans.c
+++ drivers/net/wireless/iwl7000/iwlwifi/pcie/trans.c
@@ -552,11 +552,12 @@ void iwl_pcie_apm_stop_master(struct iwl_trans *trans)
 	int ret;
 
 	/* stop device's busmaster DMA activity */
-	iwl_set_bit(trans, CSR_RESET, trans->cfg->csr->flag_stop_master);
+	iwl_set_bit(trans, trans->cfg->csr->addr_sw_reset,
+		    BIT(trans->cfg->csr->flag_stop_master));
 
 	ret = iwl_poll_bit(trans, trans->cfg->csr->addr_sw_reset,
-			   trans->cfg->csr->flag_master_dis,
-			   trans->cfg->csr->flag_master_dis, 100);
+			   BIT(trans->cfg->csr->flag_master_dis),
+			   BIT(trans->cfg->csr->flag_master_dis), 100);
 	if (ret < 0)
 		IWL_WARN(trans, "Master Disable Timed Out, 100 usec\n");
 
