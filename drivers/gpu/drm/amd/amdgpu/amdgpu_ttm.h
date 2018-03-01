fcd65ff37c8bd414c9e6b5b1092700069df3d6a3
Christian König <christian.koenig@amd.com>
2018-03-01 10:41:46 +0100

    BACKPORT: drm/amdgpu: move some functions into amdgpu_ttm.h
    Those belong to the TTM handling.
    
    Backport: context changes.
    
    BUG=b:112542653
    TEST=boot Grunt, run graphics_SanAngeles and login_LoginSuccess
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Chunming Zhou <david1.zhou@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit c396ef9bf3172fd57e86df4529b11d563f9ff801)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    
    Change-Id: Iae7dbe24e11096b527c304d5aca9d53eb82e3380

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.h
@@ -76,6 +76,9 @@ u64 amdgpu_vram_mgr_bo_invisible_size(struct amdgpu_bo *bo);
 uint64_t amdgpu_vram_mgr_usage(struct ttm_mem_type_manager *man);
 uint64_t amdgpu_vram_mgr_vis_usage(struct ttm_mem_type_manager *man);
 
+int amdgpu_ttm_init(struct amdgpu_device *adev);
+void amdgpu_ttm_fini(struct amdgpu_device *adev);
+void amdgpu_ttm_set_active_vram_size(struct amdgpu_device *adev, u64 size);
 void amdgpu_ttm_late_init(struct amdgpu_device *adev);
 int amdgpu_copy_buffer(struct amdgpu_ring *ring, uint64_t src_offset,
 		       uint64_t dst_offset, uint32_t byte_count,
