2f48c0e4ba6385d1acf5d17d70fab2532d81b7fa
Dylan Reid <dgreid@chromium.org>
2018-03-13 15:45:08 -0700

    CHROMIUM: alt-syscall: Block AF_VSOCK from android
    We've enabled VSOCK, but there is no reason for android to access it.
    Return EPERM if it ever tries.
    
    BUG=817548
    TEST=allmodconfig builds, android still starts on scarlet.
    
    Change-Id: I559dd4b15d6fb2c57980b7eb7c39857f19586297
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/963679
    (cherry picked from commit 1fa51dfc47835a8e5eed5f5ea9f36ba4dd65600d)
    Signed-off-by: Micah Morton <mortonm@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1162826

--- security/chromiumos/alt-syscall.c
+++ security/chromiumos/alt-syscall.c
@@ -16,6 +16,7 @@
 #include <linux/prctl.h>
 #include <linux/sched/types.h>
 #include <linux/slab.h>
+#include <linux/socket.h>
 #include <linux/syscalls.h>
 #include <linux/timex.h>
 
@@ -578,6 +579,14 @@ do_android_sched_setscheduler(pid_t pid, int policy,
 	return retval;
 }
 
+static asmlinkage int __maybe_unused
+android_socket(int domain, int type, int socket)
+{
+	if (domain == AF_VSOCK)
+	       return -EACCES;
+	return sys_socket(domain, type, socket);
+}
+
 static asmlinkage long
 android_sched_setscheduler(pid_t pid, int policy,
 			   struct sched_param __user *param)
@@ -884,7 +893,7 @@ static struct syscall_whitelist_entry android_whitelist[] = {
 	SYSCALL_ENTRY(sendto),
 	SYSCALL_ENTRY(setsockopt),
 	SYSCALL_ENTRY(shutdown),
-	SYSCALL_ENTRY(socket),
+	SYSCALL_ENTRY_ALT(socket, android_socket),
 	SYSCALL_ENTRY(socketpair),
 	/*
 	 * recv(2)/send(2) are officially deprecated, but their entry-points
