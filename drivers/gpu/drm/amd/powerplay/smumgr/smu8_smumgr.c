d3db40df4baea5c460a921bf6cb6e9b6c33e2411
Rex Zhu <Rex.Zhu@amd.com>
2018-03-15 14:45:04 +0800

    UPSTREAM: drm/amd/pp: Remove the cgs wrapper for notify smu version on APU
    Refine commit f49e9bac191b ("drm/amd/pp: Get and save Rv smu version")
    
    BUG=b:112542653
    TEST=boot Grunt, run graphics_SanAngeles and login_LoginSuccess
    
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Rex Zhu <Rex.Zhu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 59156faf810e05f5c5241f9a90e6d715d1185cd1)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    
    Change-Id: Iaeb6b5f461d501e2518995d21fc81bcfbb86c844

--- drivers/gpu/drm/amd/powerplay/smumgr/smu8_smumgr.c
+++ drivers/gpu/drm/amd/powerplay/smumgr/smu8_smumgr.c
@@ -699,7 +699,8 @@ static int smu8_start_smu(struct pp_hwmgr *hwmgr)
 {
 	int ret = 0;
 	uint32_t fw_to_check = 0;
-	struct cgs_firmware_info info = {0};
+	struct amdgpu_device *adev = hwmgr->adev;
+
 	uint32_t index = SMN_MP1_SRAM_START_ADDR +
 			 SMU8_FIRMWARE_HEADER_LOCATION +
 			 offsetof(struct SMU8_Firmware_Header, Version);
@@ -710,12 +711,11 @@ static int smu8_start_smu(struct pp_hwmgr *hwmgr)
 
 	cgs_write_register(hwmgr->device, mmMP0PUB_IND_INDEX, index);
 	hwmgr->smu_version = cgs_read_register(hwmgr->device, mmMP0PUB_IND_DATA);
-	info.version = hwmgr->smu_version >> 8;
+	adev->pm.fw_version = hwmgr->smu_version >> 8;
 	pr_info("smu version %02d.%02d.%02d\n",
 		((hwmgr->smu_version >> 16) & 0xFF),
 		((hwmgr->smu_version >> 8) & 0xFF),
 		(hwmgr->smu_version & 0xFF));
-	cgs_get_firmware_info(hwmgr->device, CGS_UCODE_ID_SMU, &info);
 
 	fw_to_check = UCODE_ID_RLC_G_MASK |
 			UCODE_ID_SDMA0_MASK |
