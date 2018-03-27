4c3fcc5686d96660638be4a1e271aebed6822a68
Luca Coelho <luciano.coelho@intel.com>
2018-03-27 10:48:54 +0300

    CHROMIUM: iwl7000: mac80211: remove check for local->wolan in gtk_rekey_add()
    We call ieee80211_gtk_rekey_add() when "resuming" from the d3_test in
    iwlmvm, if there was a rekeying while pseudo-suspended.  The
    ieee80211_gtk_rekey_add() in mac80211 warns if local->wowlan is not
    set (which is the case with d3_test), so waking up after rekeying
    doesn't work.
    
    We could officially remove the check in mac80211, but it could cause
    problems if gtk_rekey_notify() is not called immediately after
    gtk_rekey_add() was called.  So the right solution would be to
    refactor these two functions so that they are together to prevent such
    issues.  For now (since I don't have much spare time :P) just hack the
    check out and mark it as NOUPSTREAM.
    
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: ee76106ce442cbae1063ed7620e8a719a87b50a2

--- drivers/net/wireless/iwl7000/mac80211/key.c
+++ drivers/net/wireless/iwl7000/mac80211/key.c
@@ -5,6 +5,7 @@
  * Copyright 2007-2008	Johannes Berg <johannes@sipsolutions.net>
  * Copyright 2013-2014  Intel Mobile Communications GmbH
  * Copyright 2015-2017	Intel Deutschland GmbH
+ * Copyright 2018	Intel Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -1124,12 +1125,15 @@ ieee80211_gtk_rekey_add(struct ieee80211_vif *vif,
 			struct ieee80211_key_conf *keyconf)
 {
 	struct ieee80211_sub_if_data *sdata = vif_to_sdata(vif);
-	struct ieee80211_local *local = sdata->local;
 	struct ieee80211_key *key;
 	int err;
 
-	if (WARN_ON(!local->wowlan))
-		return ERR_PTR(-EINVAL);
+	/*
+	 * TODO: we should check for local->wowlan here, because
+	 * calling gtk_rekey_add() without being in wowlan can cause
+	 * problems, unless we refactor the code to unify
+	 * gtk_rekey_add() with gtk_rekey_notify().
+	 */
 
 	if (WARN_ON(vif->type != NL80211_IFTYPE_STATION))
 		return ERR_PTR(-EINVAL);
