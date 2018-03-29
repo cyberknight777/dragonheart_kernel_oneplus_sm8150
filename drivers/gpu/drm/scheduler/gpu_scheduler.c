57ccf9a45407d710afdda1438d2fcb03ad55b2b0
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2018-03-29 22:36:32 +0530

    BACKPORT: drm/scheduler: remove unused parameter
    this patch also effect the amdgpu and etnaviv drivers which
    use the function drm_sched_entity_init
    
    Backport: context change, fixed different comment.
    
    BUG=b:112542653
    TEST=boot Grunt, run graphics_SanAngeles and login_LoginSuccess
    
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>
    Suggested-by: Christian König <christian.koenig@amd.com>
    Acked-by: Lucas Stach <l.stach@pengutronix.de>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 8344c53f57057b42a5da87e9557c40fcda18fb7a)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    
    Change-Id: I2c7817c69f89c93241e182a6f8c7665a71f0561c
    Signed-off-by: Dominik Behr <dbehr@chromium.org>

--- drivers/gpu/drm/scheduler/gpu_scheduler.c
+++ drivers/gpu/drm/scheduler/gpu_scheduler.c
@@ -117,15 +117,15 @@ drm_sched_rq_select_entity(struct drm_sched_rq *rq)
  * @sched	The pointer to the scheduler
  * @entity	The pointer to a valid drm_sched_entity
  * @rq		The run queue this entity belongs
- * @kernel	If this is an entity for the kernel
- * @jobs	The max number of jobs in the job queue
+ * @guilty      atomic_t set to 1 when a job on this queue
+ *              is found to be guilty causing a timeout
  *
  * return 0 if succeed. negative error code on failure
 */
 int drm_sched_entity_init(struct drm_gpu_scheduler *sched,
 			  struct drm_sched_entity *entity,
 			  struct drm_sched_rq *rq,
-			  uint32_t jobs, atomic_t *guilty)
+			  atomic_t *guilty)
 {
 	if (!(sched && entity && rq))
 		return -EINVAL;
