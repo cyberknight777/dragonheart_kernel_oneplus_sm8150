d28f32767b1f39f8ba283f350d35e9eb2e32d83e
Evan Quan <evan.quan@amd.com>
2018-04-10 12:30:59 +0800

    UPSTREAM: drm/amdgpu: add MP1 and THM hw ip base reg offset
    BUG=b:112542653
    TEST=boot Grunt, run graphics_SanAngeles and login_LoginSuccess
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Rex Zhu <Rex.Zhu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 1b59fb036a3e2a5bc4ce3d02c3fb473e595400c9)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    
    Change-Id: I45d6b766e93a83d882a99d9b129b3e4b574f5806

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -1380,6 +1380,7 @@ enum amd_hw_ip_block_type {
 	ATHUB_HWIP,
 	NBIO_HWIP,
 	MP0_HWIP,
+	MP1_HWIP,
 	UVD_HWIP,
 	VCN_HWIP = UVD_HWIP,
 	VCE_HWIP,
@@ -1389,6 +1390,7 @@ enum amd_hw_ip_block_type {
 	SMUIO_HWIP,
 	PWR_HWIP,
 	NBIF_HWIP,
+	THM_HWIP,
 	MAX_HWIP
 };
 
