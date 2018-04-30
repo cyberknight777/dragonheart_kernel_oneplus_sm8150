ea9e5fc6e463d0d266a180cbf654036b8751558e
Peter Zijlstra <peterz@infradead.org>
2018-04-30 12:00:09 +0200

    UPSTREAM: x86/tsc: Always unregister clocksource_tsc_early
    Don't leave the tsc-early clocksource registered if it errors out
    early.
    
    This was reported by Diego, who on his Core2 era machine got TSC
    invalidated while it was running with tsc-early (due to C-states).
    This results in keeping tsc-early with very bad effects.
    
    Reported-and-Tested-by: Diego Viola <diego.viola@gmail.com>
    Fixes: aa83c45762a2 ("x86/tsc: Introduce early tsc clocksource")
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: len.brown@intel.com
    Cc: rjw@rjwysocki.net
    Cc: diego.viola@gmail.com
    Cc: rui.zhang@intel.com
    Cc: stable@vger.kernel.org
    Link: https://lkml.kernel.org/r/20180430100344.350507853@infradead.org
    (cherry picked from commit e9088adda13cd23249d4b0abb97ff8a81bf5573a)
    
    BUG=b:112103715, b:112159884
    TEST=Verified that no hang is observed on yorp for 5000+ warm reboots
    
    Change-Id: Ib66fe4ff4ccd5a92eff2e970bc64872646cde67c
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1214028
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Yu Zhao <yuzhao@chromium.org>

--- arch/x86/kernel/tsc.c
+++ arch/x86/kernel/tsc.c
@@ -1201,7 +1201,7 @@ static void tsc_refine_calibration_work(struct work_struct *work)
 
 	/* Don't bother refining TSC on unstable systems */
 	if (tsc_unstable)
-		return;
+		goto unreg;
 
 	/*
 	 * Since the work is started early in boot, we may be
@@ -1254,11 +1254,12 @@ static void tsc_refine_calibration_work(struct work_struct *work)
 
 out:
 	if (tsc_unstable)
-		return;
+		goto unreg;
 
 	if (boot_cpu_has(X86_FEATURE_ART))
 		art_related_clocksource = &clocksource_tsc;
 	clocksource_register_khz(&clocksource_tsc, tsc_khz);
+unreg:
 	clocksource_unregister(&clocksource_tsc_early);
 }
 
@@ -1268,8 +1269,8 @@ static int __init init_tsc_clocksource(void)
 	if (!boot_cpu_has(X86_FEATURE_TSC) || tsc_disabled > 0 || !tsc_khz)
 		return 0;
 
-	if (check_tsc_unstable())
-		return 0;
+	if (tsc_unstable)
+		goto unreg;
 
 	if (tsc_clocksource_reliable)
 		clocksource_tsc.flags &= ~CLOCK_SOURCE_MUST_VERIFY;
@@ -1285,6 +1286,7 @@ static int __init init_tsc_clocksource(void)
 		if (boot_cpu_has(X86_FEATURE_ART))
 			art_related_clocksource = &clocksource_tsc;
 		clocksource_register_khz(&clocksource_tsc, tsc_khz);
+unreg:
 		clocksource_unregister(&clocksource_tsc_early);
 		return 0;
 	}
