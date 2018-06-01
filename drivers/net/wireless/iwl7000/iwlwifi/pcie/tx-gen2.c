d713fb8df58f9d7de664e2769330c82dc67dfc8a
Johannes Berg <johannes.berg@intel.com>
2018-06-01 10:32:55 +0200

    CHROMIUM: iwl7000: pcie: support transmitting SKBs with fraglist
    We want to be able to build A-MSDUs in higher layers, e.g. by
    xmit_more, so support transmitting SKBs with fraglist to use
    it for such.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    automatic-review: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: f8a39076f9eba86182ac9bd269bb23db630bcb7f

--- drivers/net/wireless/iwl7000/iwlwifi/pcie/tx-gen2.c
+++ drivers/net/wireless/iwl7000/iwlwifi/pcie/tx-gen2.c
@@ -461,6 +461,7 @@ iwl_tfh_tfd *iwl_pcie_gen2_build_tx(struct iwl_trans *trans,
 	dma_addr_t tb_phys;
 	int len, tb1_len, tb2_len;
 	void *tb1_addr;
+	struct sk_buff *frag;
 
 	tb_phys = iwl_pcie_get_first_tb_dma(txq, idx);
 
@@ -500,6 +501,16 @@ iwl_tfh_tfd *iwl_pcie_gen2_build_tx(struct iwl_trans *trans,
 
 	if (iwl_pcie_gen2_tx_add_frags(trans, skb, tfd, out_meta))
 		goto out_err;
+
+	skb_walk_frags(skb, frag) {
+		tb_phys = dma_map_single(trans->dev, frag->data,
+					 skb_headlen(frag), DMA_TO_DEVICE);
+		if (unlikely(dma_mapping_error(trans->dev, tb_phys)))
+			goto out_err;
+		iwl_pcie_gen2_set_tb(trans, tfd, tb_phys, frag->len);
+		if (iwl_pcie_gen2_tx_add_frags(trans, frag, tfd, out_meta))
+			goto out_err;
+	}
 
 	trace_iwlwifi_dev_tx(trans->dev, skb, tfd, sizeof(*tfd), &dev_cmd->hdr,
 			     IWL_FIRST_TB_SIZE + tb1_len, hdr_len);
