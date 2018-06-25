22b2c04f2bf3ebd6266ac4c8974e0125e793f37b
Junwei Zhang <Jerry.Zhang@amd.com>
2018-06-25 12:51:14 +0800

    BACKPORT: drm/amdgpu: separate gpu address from bo pin
    It could be got by amdgpu_bo_gpu_offset() if need
    
    Backport: context changes in amdgpu_device.c.
    
    BUG=b:112542653
    TEST=boot grunt, run login_LoginSuccess, graphics_SanAngeles, cheets_StartAndroid
    
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Michel Dänzer <michel.daenzer@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 7b7c6c81b3a370b46b0c48f4ab7ac3be83237a12)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    
    Change-Id: I472d34fa4fc472db6a6bb8d91bb409af71f18b52

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -3095,7 +3095,7 @@ static int dm_plane_helper_prepare_fb(struct drm_plane *plane,
 	else
 		domain = AMDGPU_GEM_DOMAIN_VRAM;
 
-	r = amdgpu_bo_pin(rbo, domain, &afb->address);
+	r = amdgpu_bo_pin(rbo, domain);
 	amdgpu_bo_unreserve(rbo);
 
 	if (unlikely(r != 0)) {
@@ -3103,6 +3103,7 @@ static int dm_plane_helper_prepare_fb(struct drm_plane *plane,
 			DRM_ERROR("Failed to pin framebuffer with error %d\n", r);
 		return r;
 	}
+	afb->address = amdgpu_bo_gpu_offset(rbo);
 
 	amdgpu_bo_ref(rbo);
 
