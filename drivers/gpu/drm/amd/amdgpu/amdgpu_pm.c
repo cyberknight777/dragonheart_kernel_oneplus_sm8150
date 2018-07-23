4dc897cf02f34b0971cda69a54c303fb2d16a171
Gustavo A. R. Silva <gustavo@embeddedor.com>
2018-07-23 11:32:32 -0500

    BACKPORT: drm/amdgpu/pm: Fix potential Spectre v1
    commit ddf74e79a54070f277ae520722d3bab7f7a6c67a upstream.
    
    idx can be indirectly controlled by user-space, hence leading to a
    potential exploitation of the Spectre variant 1 vulnerability.
    
    This issue was detected with the help of Smatch:
    
    drivers/gpu/drm/amd/amdgpu/amdgpu_pm.c:408 amdgpu_set_pp_force_state()
    warn: potential spectre issue 'data.states'
    
    Fix this by sanitizing idx before using it to index data.states
    
    Notice that given that speculation windows are large, the policy is
    to kill the speculation on the first load and not worry if it can be
    completed with a dependent load/store [1].
    
    [1] https://marc.info/?l=linux-kernel&m=152449131114778&w=2
    
    Backport: context change.
    
    BUG=b:112542653
    TEST=boot Grunt, run graphics_SanAngeles and login_LoginSuccess
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit 3df731e0ccfe0d5210158b9104c9ed6015fb853b)
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    
    Change-Id: I4d448fec70e9467743eaa3a3740b214585b31635

--- drivers/gpu/drm/amd/amdgpu/amdgpu_pm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_pm.c
@@ -31,6 +31,7 @@
 #include <linux/power_supply.h>
 #include <linux/hwmon.h>
 #include <linux/hwmon-sysfs.h>
+#include <linux/nospec.h>
 #include "hwmgr.h"
 #define WIDTH_4K 3840
 
@@ -395,6 +396,7 @@ static ssize_t amdgpu_set_pp_force_state(struct device *dev,
 			count = -EINVAL;
 			goto fail;
 		}
+		idx = array_index_nospec(idx, ARRAY_SIZE(data.states));
 
 		amdgpu_dpm_get_pp_num_states(adev, &data);
 		state = data.states[idx];
