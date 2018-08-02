e1d71b601eb1d274bf2a393279ba8b24bbd87c7b
Luca Coelho <luciano.coelho@intel.com>
2018-08-02 16:24:53 +0300

    CHROMIUM: iwl7000: pcie: set iwl_trans->cfg later in the probe function
    Instead of setting the cfg to iwl_trans already during allocation, set
    it only later when we have had the time to decide which cfg to use.
    This is part of the effort to be able to decide the cfg based on HW
    revision and RF ID after iwl_trans_alloc() has been called.
    
    For now, since we still have a bunch of code checking the HW revision
    and the RF ID, we set iwl_trans->cfg early, even before we decided the
    real cfg to use.  We only use the trans configuration at this point,
    so this is fine for now.  In the future, the trans configuration will
    be completely independent from the rest of the config structure, so
    we'll be able to avoid this.
    
    Additionally, we can't access the PRPH registers in iwl_trans_alloc()
    anymore, so move the HW REV C-step check for family 8000 code later to
    the probe function as well.  This step is probably not necessary, but
    if that's the case it should be removed separately later on.
    
    Change-Id: Ib4145067133e54d73af9b1a1b83781f7e9475efb
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 09e7430b951a0f278955f36d8b3297222a6e2cb7

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-trans.c
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-trans.c
@@ -68,7 +68,6 @@
 
 struct iwl_trans *iwl_trans_alloc(unsigned int priv_size,
 				  struct device *dev,
-				  const struct iwl_cfg *cfg,
 				  const struct iwl_trans_ops *ops)
 {
 	struct iwl_trans *trans;
@@ -86,7 +85,6 @@ struct iwl_trans *iwl_trans_alloc(unsigned int priv_size,
 #endif
 
 	trans->dev = dev;
-	trans->cfg = cfg;
 	trans->ops = ops;
 	trans->num_rx_queues = 1;
 
