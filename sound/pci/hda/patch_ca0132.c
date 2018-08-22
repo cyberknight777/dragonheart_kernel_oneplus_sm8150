82f2338c25d8be18653ab1dd5ed2478266ab06a8
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2018-08-22 15:24:57 -0500

    FROMGIT: ALSA: hda: move hda_codec.h to include/sound
    As suggested by Takashi, move this header file to make it easier
    to include from e.g. the Intel Skylake driver in follow-up patches
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit be57bfffb7b5ba72e1293643053f8861fcaa5163)
    Signed-off-by: Samaga Prasanna Krishna <samaga.krishna@intel.corp-partner.google.com>
    
    BUG=b:110025789
    TEST=Test Audio use cases after full SOF series patches are applied.
    
    Change-Id: Iab0a89b5c72da8f2092c185667d1cd614123a144
    Reviewed-on: https://chromium-review.googlesource.com/1219217
    Commit-Ready: Curtis Malainey <cujomalainey@chromium.org>
    Tested-by: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>

--- sound/pci/hda/patch_ca0132.c
+++ sound/pci/hda/patch_ca0132.c
@@ -28,7 +28,7 @@
 #include <linux/module.h>
 #include <linux/firmware.h>
 #include <sound/core.h>
-#include "hda_codec.h"
+#include <sound/hda_codec.h>
 #include "hda_local.h"
 #include "hda_auto_parser.h"
 #include "hda_jack.h"
