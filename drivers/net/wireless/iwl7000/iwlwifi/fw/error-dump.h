5c76ff5dc21e1f7cd7cd464d4f0612995c4581f5
Shahar S Matityahu <shahar.s.matityahu@intel.com>
2018-08-28 16:15:55 +0300

    CHROMIUM: iwl7000: wrt: add rt status and num of rx/tx fifos to dump
    Add the rt status of the last assert or 0 if the dump collection
    was not initiated by an assert.
    Add number of rx and tx fifos in use.
    the fields are added to dump info lst file.
    
    Change-Id: I9b47afb97ee97f53795c25e6eafccd3783ad2b85
    Signed-off-by: Shahar S Matityahu <shahar.s.matityahu@intel.com>
    iwl7000-tree: 6d774007e67c65fde4526b28b5d5af03f5e0e714

--- drivers/net/wireless/iwl7000/iwlwifi/fw/error-dump.h
+++ drivers/net/wireless/iwl7000/iwlwifi/fw/error-dump.h
@@ -187,6 +187,10 @@ enum iwl_fw_error_dump_family {
  * @fw_human_readable: human readable FW version
  * @dev_human_readable: name of the device
  * @bus_human_readable: name of the bus used
+ * @rt_status: the error_id/rt_status that that triggered the latest dump
+ *	if the dump collection was not initiated by an assert, the value is 0
+ * @num_of_txfifos: number of rx fifos
+ * @num_of_rxfifos: number of tx fifos
  */
 struct iwl_fw_error_dump_info {
 	__le32 device_family;
@@ -194,6 +198,9 @@ struct iwl_fw_error_dump_info {
 	u8 fw_human_readable[FW_VER_HUMAN_READABLE_SZ];
 	u8 dev_human_readable[64];
 	u8 bus_human_readable[8];
+	__le32 rt_status;
+	__le16 num_of_txfifos;
+	__le16 num_of_rxfifos;
 } __packed;
 
 /**
