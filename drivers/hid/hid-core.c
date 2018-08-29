020e85e885fe14687137d1e988da29ef99f9040d
Gustavo A. R. Silva <gustavo@embeddedor.com>
2018-08-29 10:22:09 -0500

    UPSTREAM: HID: core: fix NULL pointer dereference
    There is a NULL pointer dereference in case memory resources
    for *parse* are not successfully allocated.
    
    Fix this by adding a new goto label and make the execution
    path jump to it in case vzalloc() fails.
    
    Addresses-Coverity-ID: 1473081 ("Dereference after null check")
    Fixes: b2dd9f2e5a8a ("HID: core: fix memory leak on probe")
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Reviewed-by: Stefan Agner <stefan@agner.ch>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    (cherry picked from commit b034ed50a2bb5)
    
    BUG=chromium:888172
    TEST=With the rest of the high-resolution scrolling series, checked that
         mice worked.
    
    Change-Id: Ic38cf71821dd8532fc0a7805d75f5c945db43764
    Signed-off-by: Harry Cutts <hcutts@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1476147
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -1021,7 +1021,7 @@ int hid_open_report(struct hid_device *device)
 	parser = vzalloc(sizeof(struct hid_parser));
 	if (!parser) {
 		ret = -ENOMEM;
-		goto err;
+		goto alloc_err;
 	}
 
 	parser->device = device;
@@ -1070,6 +1070,7 @@ int hid_open_report(struct hid_device *device)
 	hid_err(device, "item fetching failed at offset %d\n", (int)(end - start));
 err:
 	kfree(parser->collection_stack);
+alloc_err:
 	vfree(parser);
 	hid_close_report(device);
 	return ret;
