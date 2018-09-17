eff932559f5aa4b86b73d123394e263e99931d84
Shahar S Matityahu <shahar.s.matityahu@intel.com>
2018-09-17 12:26:01 +0300

    CHROMIUM: iwl7000: mvm: set STATUS_FW_ERROR bit in case of assert during D3
    The driver should set STATUS_FW_ERROR if there was an assert during D3
    state, but it currently doesn't.  Fix that.
    
    Change-Id: I02da36172163bca066e957e0d4bf50fe2de2c141
    Signed-off-by: Shahar S Matityahu <shahar.s.matityahu@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 9623d6a19f65072705c196bd50fc9a3b07721892

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/d3.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/d3.c
@@ -1957,6 +1957,7 @@ static int __iwl_mvm_resume(struct iwl_mvm *mvm, bool test)
 	iwl_mvm_read_d3_sram(mvm);
 
 	if (iwl_mvm_check_rt_status(mvm, vif)) {
+		set_bit(STATUS_FW_ERROR, &mvm->trans->status);
 		iwl_mvm_dump_nic_error_log(mvm);
 		iwl_fw_dbg_collect_desc(&mvm->fwrt, &iwl_dump_desc_assert,
 					false, 0);
