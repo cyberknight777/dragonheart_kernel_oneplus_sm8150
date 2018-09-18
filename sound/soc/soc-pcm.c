675e25344364f320af814d81b2db0d84d759d70f
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2018-09-18 01:30:54 +0000

    UPSTREAM: ASoC: add for_each_dpcm_fe() macro
    To be more readable code, this patch adds
    new for_each_dpcm_fe() macro, and replace existing code to it.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit d2e24d64652bf9d272e5496ae8a562bc64facff3)
    Signed-off-by: Samaga Prasanna Krishna <samaga.krishna@intel.corp-partner.google.com>
    
    BUG=b:110025789
    TEST=Test Audio use cases with full SOF 0.6 rebase release series applied.
    
    Change-Id: Idb42ac770c29adf338172c8d7f66ba8ed338dac6
    Reviewed-on: https://chromium-review.googlesource.com/1353749
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-by: Sathyanarayana Nujella <sathyanarayana.nujella@intel.com>
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-by: Sudarshan S <sudarshan.s@intel.com>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -1253,7 +1253,7 @@ static void dpcm_be_reparent(struct snd_soc_pcm_runtime *fe,
 
 	be_substream = snd_soc_dpcm_get_substream(be, stream);
 
-	list_for_each_entry(dpcm, &be->dpcm[stream].fe_clients, list_fe) {
+	for_each_dpcm_fe(be, stream, dpcm) {
 		if (dpcm->fe == fe)
 			continue;
 
@@ -3220,7 +3220,7 @@ int snd_soc_dpcm_can_be_free_stop(struct snd_soc_pcm_runtime *fe,
 	struct snd_soc_dpcm *dpcm;
 	int state;
 
-	list_for_each_entry(dpcm, &be->dpcm[stream].fe_clients, list_fe) {
+	for_each_dpcm_fe(be, stream, dpcm) {
 
 		if (dpcm->fe == fe)
 			continue;
@@ -3247,7 +3247,7 @@ int snd_soc_dpcm_can_be_params(struct snd_soc_pcm_runtime *fe,
 	struct snd_soc_dpcm *dpcm;
 	int state;
 
-	list_for_each_entry(dpcm, &be->dpcm[stream].fe_clients, list_fe) {
+	for_each_dpcm_fe(be, stream, dpcm) {
 
 		if (dpcm->fe == fe)
 			continue;
