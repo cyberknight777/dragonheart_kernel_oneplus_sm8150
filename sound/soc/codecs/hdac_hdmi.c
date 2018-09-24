25fef5e566f22ae1d093ffa4c299773c81949030
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2018-09-24 13:12:51 -0700

    FROMLIST: Revert "FROMLIST: SoC: Intel: Power down links before tuning off display audio power"
    This reverts commit f931c90bac5d8836eabc7e5225cfee8506220661.
    
    (cherry picked from commit de655b2c326997c05271a1ee39fb3924bd4a0ea4)
    Link:https://github.com/thesofproject/linux/commit/de655b2c326997c05271a1ee39fb3924bd4a0ea4
    
    BUG=b:110025789
    TEST=Test Audio use cases after full SOF series patches are applied.
    
    Signed-off-by: Naveen Manohar <naveen.m@intel.com>
    Signed-off-by: Samaga Prasanna Krishna <samaga.krishna@intel.corp-partner.google.com>
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>
    
    conflicts:
    	sound/soc/intel/skylake/skl.c
    
    Change-Id: I6baa9e3acb9d6fb859c4d9666e30dfe948754d4e
    Reviewed-on: https://chromium-review.googlesource.com/1242639
    Commit-Ready: Curtis Malainey <cujomalainey@chromium.org>
    Tested-by: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- sound/soc/codecs/hdac_hdmi.c
+++ sound/soc/codecs/hdac_hdmi.c
@@ -2129,11 +2129,7 @@ static int hdac_hdmi_runtime_suspend(struct device *dev)
 
 	snd_hdac_ext_bus_link_put(bus, hlink);
 
-	err = snd_hdac_display_power(bus, false);
-	if (err < 0)
-		dev_err(bus->dev, "Cannot turn off display power on i915\n");
-
-	return err;
+	return 0;
 }
 
 static int hdac_hdmi_runtime_resume(struct device *dev)
