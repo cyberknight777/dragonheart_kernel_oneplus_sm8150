6d501bceddff3397107047c36bd57882ed3581eb
Vivek Gautam <vivek.gautam@codeaurora.org>
2018-09-27 14:39:45 +0530

    CHROMIUM: Revert "FROMLIST: iommu/arm-smmu: Add support for qcom,smmu-v2 variant"
    This reverts commit 735d0fc8491702c0d9812fc227410ea78460e58d.
    
    We are moving to latest set of patches for arm-smmu pm runtime
    support, that are ready for merge in upstream.
    
    CQ-DEPEND=CL:1249169
    BUG=b:110260554, b:74036927, b:112610860
    TEST=Build and boot on Cheza
    
    Change-Id: Id6927810c39057789ae462209fd93100b6ef4406
    Signed-off-by: Vivek Gautam <vivek.gautam@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/1249161
    Commit-Ready: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -119,7 +119,6 @@ enum arm_smmu_implementation {
 	GENERIC_SMMU,
 	ARM_MMU500,
 	CAVIUM_SMMUV2,
-	QCOM_SMMUV2,
 };
 
 struct arm_smmu_s2cr {
@@ -1981,17 +1980,6 @@ ARM_SMMU_MATCH_DATA(arm_mmu401, ARM_SMMU_V1_64K, GENERIC_SMMU);
 ARM_SMMU_MATCH_DATA(arm_mmu500, ARM_SMMU_V2, ARM_MMU500);
 ARM_SMMU_MATCH_DATA(cavium_smmuv2, ARM_SMMU_V2, CAVIUM_SMMUV2);
 
-static const char * const qcom_smmuv2_clks[] = {
-	"bus", "iface",
-};
-
-static const struct arm_smmu_match_data qcom_smmuv2 = {
-	.version = ARM_SMMU_V2,
-	.model = QCOM_SMMUV2,
-	.clks = qcom_smmuv2_clks,
-	.num_clks = ARRAY_SIZE(qcom_smmuv2_clks),
-};
-
 static const struct of_device_id arm_smmu_of_match[] = {
 	{ .compatible = "arm,smmu-v1", .data = &smmu_generic_v1 },
 	{ .compatible = "arm,smmu-v2", .data = &smmu_generic_v2 },
@@ -1999,7 +1987,6 @@ static const struct of_device_id arm_smmu_of_match[] = {
 	{ .compatible = "arm,mmu-401", .data = &arm_mmu401 },
 	{ .compatible = "arm,mmu-500", .data = &arm_mmu500 },
 	{ .compatible = "cavium,smmu-v2", .data = &cavium_smmuv2 },
-	{ .compatible = "qcom,smmu-v2", .data = &qcom_smmuv2 },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, arm_smmu_of_match);
@@ -2378,7 +2365,6 @@ IOMMU_OF_DECLARE(arm_mmu400, "arm,mmu-400");
 IOMMU_OF_DECLARE(arm_mmu401, "arm,mmu-401");
 IOMMU_OF_DECLARE(arm_mmu500, "arm,mmu-500");
 IOMMU_OF_DECLARE(cavium_smmuv2, "cavium,smmu-v2");
-IOMMU_OF_DECLARE(qcom_smmuv2, "qcom,smmu-v2");
 
 MODULE_DESCRIPTION("IOMMU API for ARM architected SMMU implementations");
 MODULE_AUTHOR("Will Deacon <will.deacon@arm.com>");
