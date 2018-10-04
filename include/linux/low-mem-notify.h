5b3b8c3957d845b449d14ab72b67170278524b2e
Kuo-Hsin Yang <vovoy@chromium.org>
2018-10-04 09:55:27 +0800

    CHROMIUM: low_mem: exclude totalreserve_pages from available memory
    totalreserve_pages is the reserve of pages that are not available to
    userspace allocations. totalreserve_pages includes lowmem_reserve which
    is higher on boards with highmem zones. Exclude totalreserve_pages
    instead of min_free from available memory.
    
    BUG=chromium:890335
    TEST=run platform_LowMemoryTest
    
    Change-Id: I5568a5d438de334b22c62aa54cd82e90c868392e
    Signed-off-by: Kuo-Hsin Yang <vovoy@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1260526
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Vovo Yang <vovoy@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    (cherry picked from commit 6cf6c12eaec831a8ea45197f161c1b6746cf00ba)
    Reviewed-on: https://chromium-review.googlesource.com/1654770
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>

--- include/linux/low-mem-notify.h
+++ include/linux/low-mem-notify.h
@@ -59,17 +59,14 @@ static inline unsigned long get_available_anon_mem(void)
  */
 static inline unsigned long get_available_mem_adj(void)
 {
-	/* min_free_kbytes is reserved for emergency allocation like when
-	 * PF_MEMALLOC is set. In general it's not usable in normal page
-	 * allocation process.
-	 */
-	unsigned long min_free_pages = min_free_kbytes >> (PAGE_SHIFT - 10);
 	/* free_mem is completely unallocated; clean file-backed memory
 	 * (file_mem - dirty_mem) is easy to reclaim, except for the last
-	 * min_filelist_kbytes.
+	 * min_filelist_kbytes. totalreserve_pages is the reserve of pages that
+	 * are not available to user space.
 	 */
-	unsigned long free_mem =
-			global_zone_page_state(NR_FREE_PAGES) - min_free_pages;
+	unsigned long raw_free_mem = global_zone_page_state(NR_FREE_PAGES);
+	unsigned long free_mem = (raw_free_mem > totalreserve_pages) ?
+			raw_free_mem - totalreserve_pages : 0;
 	unsigned long available_mem = free_mem +
 			get_available_file_mem();
 	unsigned long swappable_pages = min_t(unsigned long,
