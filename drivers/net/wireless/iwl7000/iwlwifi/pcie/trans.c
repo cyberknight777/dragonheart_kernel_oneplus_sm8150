d4cb7d11d8cd08aa32cb11e4736d8d43f8956e87
Aloni, Adiel <adiel.aloni@intel.com>
2018-11-13 07:38:33 +0200

    CHROMIUM: iwl7000: pcie: recognize NICs with hw_rev 0x364 correctly
    some devices are using subsystem device id that is common
    to other devices (0x0074), differentiate between them using hw_rev.
    
    Change-Id: I41a2f5c87e508e783048ad5f84a897d5af787273
    Signed-off-by: Aloni, Adiel <adiel.aloni@intel.com>
    iwl7000-tree: ac617fefa14e9fdaa09aa06bdb552d0190ad1223

--- drivers/net/wireless/iwl7000/iwlwifi/pcie/trans.c
+++ drivers/net/wireless/iwl7000/iwlwifi/pcie/trans.c
@@ -3618,7 +3618,11 @@ struct iwl_trans *iwl_trans_pcie_alloc(struct pci_dev *pdev,
 
 	if (cfg == &iwl22560_2ax_cfg_hr) {
 		if (CSR_HW_RF_ID_TYPE_CHIP_ID(trans->hw_rf_id) ==
-		    CSR_HW_RF_ID_TYPE_CHIP_ID(CSR_HW_RF_ID_TYPE_HR)) {
+		    CSR_HW_RF_ID_TYPE_CHIP_ID(CSR_HW_RF_ID_TYPE_HR) &&
+		    trans->hw_rev == CSR_HW_REV_TYPE_QNJ_B0) {
+			trans->cfg = &iwl22000_2ax_cfg_qnj_hr_b0;
+		} else if (CSR_HW_RF_ID_TYPE_CHIP_ID(trans->hw_rf_id) ==
+			   CSR_HW_RF_ID_TYPE_CHIP_ID(CSR_HW_RF_ID_TYPE_HR)) {
 			trans->cfg = &iwl22560_2ax_cfg_hr;
 		} else if (CSR_HW_RF_ID_TYPE_CHIP_ID(trans->hw_rf_id) ==
 			   CSR_HW_RF_ID_TYPE_CHIP_ID(CSR_HW_RF_ID_TYPE_JF)) {
