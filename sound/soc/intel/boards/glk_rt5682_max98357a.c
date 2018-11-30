84a9a705dc6c2d4360fb74d26a2181ed3cb90ca9
Mac Chiang <mac.chiang@intel.com>
2018-11-30 17:32:46 +0800

    CHROMIUM: ASoC: Intel: boards: fix errors during suspend_resume
    FW return error results in suspend resume stress test failures.
    
    "FW error code: 1015", remove 'Echoref' dai link
    "Headset Playback trigger FE failed: -32", add back front-end
    hw_params supported format.
    
    BUG=b:118363920
    TEST=run suspend_stress_test 50 times
    
    Signed-off-by: Mac Chiang <mac.chiang@intel.com>
    Change-Id: I1d943985973c3da09e4feb77d6340df446b603ce
    Reviewed-on: https://chromium-review.googlesource.com/c/1356759
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>

--- sound/soc/intel/boards/glk_rt5682_max98357a.c
+++ sound/soc/intel/boards/glk_rt5682_max98357a.c
@@ -48,7 +48,6 @@ enum {
 	GLK_DPCM_AUDIO_PB = 0,
 	GLK_DPCM_AUDIO_CP,
 	GLK_DPCM_AUDIO_HS_PB,
-	GLK_DPCM_AUDIO_ECHO_REF_CP,
 	GLK_DPCM_AUDIO_REF_CP,
 	GLK_DPCM_AUDIO_DMIC_CP,
 	GLK_DPCM_AUDIO_HDMI1_PB,
@@ -267,6 +266,40 @@ static struct snd_pcm_hw_constraint_list constraints_channels_quad = {
 	.mask = 0,
 };
 
+static int glk_fe_startup(struct snd_pcm_substream *substream)
+{
+	struct snd_pcm_runtime *runtime = substream->runtime;
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+
+	/*
+	 * On this platform for PCM device we support,
+	 * 48Khz
+	 * stereo
+	 * 16 bit audio
+	 */
+	if (!strstr(rtd->cpu_dai->name, "HDMI")) {
+		runtime->hw.channels_max = DUAL_CHANNEL;
+		snd_pcm_hw_constraint_list(runtime, 0,
+					   SNDRV_PCM_HW_PARAM_CHANNELS,
+					   &constraints_channels);
+
+		runtime->hw.formats = SNDRV_PCM_FMTBIT_S16_LE;
+		snd_pcm_hw_constraint_msbits(runtime, 0, 16, 16);
+	}
+	snd_pcm_hw_constraint_list(runtime, 0,
+				SNDRV_PCM_HW_PARAM_RATE, &constraints_rates);
+
+	return 0;
+}
+
+static const struct snd_soc_ops geminilake_rt5682_fe_ops = {
+	.startup = glk_fe_startup,
+};
+
+static const struct snd_soc_ops geminilake_hdmi_fe_ops = {
+	.startup = glk_fe_startup,
+};
+
 static int geminilake_dmic_fixup(struct snd_soc_pcm_runtime *rtd,
 		struct snd_pcm_hw_params *params)
 {
@@ -333,6 +366,7 @@ static struct snd_soc_dai_link geminilake_dais[] = {
 		.trigger = {
 			SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
 		.dpcm_playback = 1,
+		.ops = &geminilake_rt5682_fe_ops,
 	},
 	[GLK_DPCM_AUDIO_CP] = {
 		.name = "Glk Audio Capture Port",
@@ -346,10 +380,11 @@ static struct snd_soc_dai_link geminilake_dais[] = {
 		.trigger = {
 			SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
 		.dpcm_capture = 1,
+		.ops = &geminilake_rt5682_fe_ops,
 	},
 	[GLK_DPCM_AUDIO_HS_PB] = {
 		.name = "Glk Audio Headset Playback",
-		.stream_name = "Headset Audio",
+		.stream_name = "Headset Playback",
 		.cpu_dai_name = "System Pin2",
 		.platform_name = "0000:00:0e.0",
 		.codec_name = "snd-soc-dummy",
@@ -357,17 +392,7 @@ static struct snd_soc_dai_link geminilake_dais[] = {
 		.dpcm_playback = 1,
 		.nonatomic = 1,
 		.dynamic = 1,
-	},
-	[GLK_DPCM_AUDIO_ECHO_REF_CP] = {
-		.name = "Glk Audio Echo Reference cap",
-		.stream_name = "Echoreference Capture",
-		.cpu_dai_name = "Echoref Pin",
-		.codec_name = "snd-soc-dummy",
-		.platform_name = "0000:00:0e.0",
-		.codec_dai_name = "snd-soc-dummy-dai",
-		.init = NULL,
-		.capture_only = 1,
-		.nonatomic = 1,
+		.ops = &geminilake_rt5682_fe_ops,
 	},
 	[GLK_DPCM_AUDIO_REF_CP] = {
 		.name = "Glk Audio Reference cap",
@@ -408,6 +433,7 @@ static struct snd_soc_dai_link geminilake_dais[] = {
 			SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
 		.nonatomic = 1,
 		.dynamic = 1,
+		.ops = &geminilake_hdmi_fe_ops,
 	},
 	[GLK_DPCM_AUDIO_HDMI2_PB] =	{
 		.name = "Glk HDMI Port2",
@@ -422,6 +448,7 @@ static struct snd_soc_dai_link geminilake_dais[] = {
 			SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
 		.nonatomic = 1,
 		.dynamic = 1,
+		.ops = &geminilake_hdmi_fe_ops,
 	},
 	[GLK_DPCM_AUDIO_HDMI3_PB] =	{
 		.name = "Glk HDMI Port3",
@@ -436,6 +463,7 @@ static struct snd_soc_dai_link geminilake_dais[] = {
 		.init = NULL,
 		.nonatomic = 1,
 		.dynamic = 1,
+		.ops = &geminilake_hdmi_fe_ops,
 	},
 	/* Back End DAI links */
 	{
