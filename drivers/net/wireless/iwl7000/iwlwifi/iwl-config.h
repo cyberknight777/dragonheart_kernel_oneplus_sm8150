91995edb3556ac7ddfbcd5989a375a5a35a899f2
Luca Coelho <luciano.coelho@intel.com>
2019-01-14 14:56:02 +0200

    CHROMIUM: iwl7000: start using B-step for some 9000 devices
    We don't support A-step for some device combinations anymore.  So
    change them to use B-step, renaming and reorganizing the config
    structures.  Additionally, fix one device that was using the wrong
    configuration.
    
    Change-Id: I899ce313be4eb7388c7cdfb76cf663654509759f
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 9df37bf48fdaf46affe76a4598747f82aa20a8b6

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-config.h
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-config.h
@@ -543,7 +543,7 @@ extern const struct iwl_cfg iwl22000_2ax_cfg_jf;
 extern const struct iwl_cfg iwl22000_2ax_cfg_qnj_hr_a0_f0;
 extern const struct iwl_cfg iwl22000_2ax_cfg_qnj_hr_b0_f0;
 extern const struct iwl_cfg iwl22000_2ax_cfg_qnj_hr_b0;
-extern const struct iwl_cfg iwl22000_2ax_cfg_qnj_jf_b0;
+extern const struct iwl_cfg iwl9560_2ac_cfg_qnj_jf_b0;
 extern const struct iwl_cfg iwl22000_2ax_cfg_qnj_hr_a0;
 extern const struct iwl_cfg iwl22560_2ax_cfg_su_cdb;
 #endif /* CPTCFG_IWLMVM || CPTCFG_IWLFMAC */
