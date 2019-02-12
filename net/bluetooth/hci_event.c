f5e998d6d893a369a0c5f66a585d848c6f8fb1a8
Sonny Sasaka <sonnysasaka@chromium.org>
2019-02-12 11:05:02 -0800

    CHROMIUM: Drop Bluetooth connection with weak encryption key
    This aborts any connection with encryption key shorter than 7 octets.
    
    BUG=chromium:929763
    TEST=Tested with MIN_ENC_KEY_LEN=17 and check that every connection
    should be immediately dropped. With MIN_ENC_KEY_LEN=16 check that
    connection with devices that support 16 bytes (most devices) are not
    dropped.
    
    Change-Id: I0e6578132e116f010d7c703194c00878d29bda58
    Signed-off-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1492007
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Xiaoyong Zhou <xzhou@chromium.org>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -39,6 +39,9 @@
 #define ZERO_KEY "\x00\x00\x00\x00\x00\x00\x00\x00" \
 		 "\x00\x00\x00\x00\x00\x00\x00\x00"
 
+/* Minimum encryption key length, value adopted from BLE (7 bytes) */
+#define MIN_ENC_KEY_LEN 7
+
 /* Handle HCI Event packets */
 
 static void hci_cc_inquiry_cancel(struct hci_dev *hdev, struct sk_buff *skb)
@@ -2590,6 +2593,13 @@ static void read_enc_key_size_complete(struct hci_dev *hdev, u8 status,
 		conn->enc_key_size = HCI_LINK_KEY_SIZE;
 	} else {
 		conn->enc_key_size = rp->key_size;
+	}
+
+	if (conn->enc_key_size < MIN_ENC_KEY_LEN) {
+		BT_DBG("Dropping connection with weak encryption key length");
+		hci_disconnect(conn, HCI_ERROR_REMOTE_USER_TERM);
+		hci_conn_drop(conn);
+		goto unlock;
 	}
 
 	if (conn->state == BT_CONFIG) {
