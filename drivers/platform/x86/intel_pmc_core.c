1b54cb6c21104fd80909bee90cbaa9c0a9f3ba02
Rajat Jain <rajatja@google.com>
2019-02-15 16:19:20 -0800

    FROMGIT: platform/x86: intel_pmc_core: Avoid a u32 overflow
    The register (SLP_S0_RES) at offset slp_s0_offset is a 32 bit register.
    The pmc_core_adjust_slp_s0_step() could overflow the u32 value while
    returning it after adjusting the step. Thus change to u64, this is
    already accounted for in debugfs attribute (that wants to output a
    64 bit value).
    
    Acked-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@linux.intel.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>
    (cherry picked from commit 4a5861f714293767980e4948c9a7c9e5e09c9b94
     git://git.infradead.org/linux-platform-drivers-x86.git for-next)
    
    BUG=b:125391455,b:123540469
    TEST=Boot hatch and verify no regressions
    
    Signed-off-by: Rajat Jain <rajatja@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1484362
    Reviewed-by: Evan Green <evgreen@chromium.org>
    (cherry picked from commit 17d46ae9f526897e9c260cb6c9cfe283f738caf8)
    Signed-off-by: Ravi Chandra Sadineni <ravisadineni@chromium.org>
    Change-Id: I4ac2baacd4d6998d103d8cb72c130ec2f060abe5
    Reviewed-on: https://chromium-review.googlesource.com/1551501
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/platform/x86/intel_pmc_core.c
+++ drivers/platform/x86/intel_pmc_core.c
@@ -141,9 +141,9 @@ static inline void pmc_core_reg_write(struct pmc_dev *pmcdev, int
 	writel(val, pmcdev->regbase + reg_offset);
 }
 
-static inline u32 pmc_core_adjust_slp_s0_step(u32 value)
+static inline u64 pmc_core_adjust_slp_s0_step(u32 value)
 {
-	return value * SPT_PMC_SLP_S0_RES_COUNTER_STEP;
+	return (u64)value * SPT_PMC_SLP_S0_RES_COUNTER_STEP;
 }
 
 /**
