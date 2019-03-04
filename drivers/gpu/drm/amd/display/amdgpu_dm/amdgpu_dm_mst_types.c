35d786420e89bc8437dd091818cfd41f761364c2
Drew Davenport <ddavenport@chromium.org>
2019-03-04 14:40:12 -0700

    Revert "FROMGIT: drm/amd/display: Check msg->size before starting aux transfer"
    This reverts commit 538551f26019be44ad0a2181df910a2bb2d79a36.
    
    The same commit was added in as a part of the drm+amdgpu forklift as well, so
    the check is duplicated.
    
    BUG=none
    TEST=build+deploy kernel to grunt device
    
    Change-Id: If0e2c428adc9d0613a3d15f6796f68454a70e3c4
    Signed-off-by: Drew Davenport <ddavenport@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1500858
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -88,9 +88,6 @@ static ssize_t dm_dp_aux_transfer(struct drm_dp_aux *aux,
 	if (WARN_ON(msg->size > 16))
 		return -E2BIG;
 
-	if (WARN_ON(msg->size > 16))
-		return -E2BIG;
-
 	switch (msg->request & ~DP_AUX_I2C_MOT) {
 	case DP_AUX_NATIVE_READ:
 		read_bytes = dal_ddc_service_read_dpcd_data(
