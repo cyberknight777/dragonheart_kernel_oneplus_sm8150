5e99d63a5df65400768f926fb11712f52d3dfc5c
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-03-27 08:47:21 -0700

    Revert "CHROMIUM: ASoC: SOF: reduce timeout for ROM init and modify FW load retries"
    This reverts commit 8ff7b53653c7c77cc1e5c65b9bdcf3e72f5b65fe.
    
    BUG=b:130538528
    TEST=Test Audio use cases for GLK with full SOF patch series applied.
    
    Signed-off-by: smit shah <smit.shah@intel.corp-partner.google.com>
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>

--- sound/soc/sof/intel/hda-loader.c
+++ sound/soc/sof/intel/hda-loader.c
@@ -175,7 +175,7 @@ step5:
 	ret = snd_sof_dsp_register_poll(sdev, HDA_DSP_BAR,
 					HDA_DSP_SRAM_REG_ROM_STATUS,
 					HDA_DSP_ROM_STS_MASK, HDA_DSP_ROM_INIT,
-					HDA_ROM_INIT_TIMEOUT);
+					HDA_DSP_INIT_TIMEOUT);
 	if (ret >= 0)
 		goto out;
 
@@ -325,7 +325,7 @@ int hda_dsp_cl_boot_firmware(struct snd_sof_dev *sdev)
 	init_waitqueue_head(&sdev->boot_wait);
 	sdev->boot_complete = false;
 
-	/* try attempting ROM init a few times before giving up */
+	/* try attempting fw boot a few times before giving up */
 	for (i = 0; i < HDA_FW_BOOT_ATTEMPTS; i++) {
 		tag = cl_dsp_init(sdev, stripped_firmware.data,
 				  stripped_firmware.size);
@@ -341,27 +341,28 @@ int hda_dsp_cl_boot_firmware(struct snd_sof_dev *sdev)
 			ret = tag;
 			continue;
 		}
-		break;
-	}
 
-	if (tag <= 0)
-		return tag;
+		/* at this point DSP ROM has been initialized and
+		 * should be ready for code loading and firmware boot
+		 */
+		ret = cl_copy_fw(sdev, tag);
+		if (ret < 0) {
+			dev_err(sdev->dev, "error: iteration %d of load fw failed err: %d\n",
+				i, ret);
+			continue;
+		}
 
-	/*
-	 * at this point DSP ROM has been initialized and
-	 * should be ready for code loading and firmware boot
-	 */
-	ret = cl_copy_fw(sdev, tag);
-	if (ret < 0) {
-		hda_dsp_dump(sdev, SOF_DBG_REGS | SOF_DBG_PCI | SOF_DBG_MBOX);
 
-		/* disable DSP */
-		snd_sof_dsp_update_bits(sdev, HDA_DSP_PP_BAR, SOF_HDA_REG_PP_PPCTL,
-				SOF_HDA_PPCTL_GPROCEN, 0);
-		dev_err(sdev->dev, "error: load fw failed after with err: %d\n", ret);
+		dev_dbg(sdev->dev, "Firmware download successful, booting...\n");
 		return ret;
 	}
 
-	dev_dbg(sdev->dev, "Firmware download successful, booting...\n");
+	hda_dsp_dump(sdev, SOF_DBG_REGS | SOF_DBG_PCI | SOF_DBG_MBOX);
+
+	/* disable DSP */
+	snd_sof_dsp_update_bits(sdev, HDA_DSP_PP_BAR, SOF_HDA_REG_PP_PPCTL,
+				SOF_HDA_PPCTL_GPROCEN, 0);
+	dev_err(sdev->dev, "error: load fw failed after %d attempts with err: %d\n",
+		HDA_FW_BOOT_ATTEMPTS, ret);
 	return ret;
 }
