33b59903f9bc4844760a45405e35e190a1839555
Kirtika Ruchandani <kirtika@google.com>
2019-04-18 18:22:36 -0700

    Revert "CHROMIUM: iwl7000: add sync_nmi to trans ops"
    This reverts commit a7c7a73a366c.
    Reason for revert: b/130651313
    iwl_trans_sync_nmi assumes that MSI-X is enabled and tries to read a
    register that is not part of iomem (CSR_MSIX_HW_INT_CAUSES_AD).
    MSI-X is not enabled on StP2 devices.
    
    BUG=b:130651313, chromium:951645
    TEST=Sanity check on octopus. No real test as JfP2/ThP2 support MSI-X
    so we cannot recreate $BUG there.
    
    Change-Id: I2c82d8c2c058b54e38a10b3c26e74abce7ebc567
    Signed-off-by: Kirtika Ruchandani <kirtika@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1583138
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Kirtika Ruchandani <kirtika@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- drivers/net/wireless/iwl7000/iwlwifi/pcie/trans.c
+++ drivers/net/wireless/iwl7000/iwlwifi/pcie/trans.c
@@ -3375,8 +3375,7 @@ static void iwl_trans_pcie_resume(struct iwl_trans *trans)
 	.unref = iwl_trans_pcie_unref,					\
 	.dump_data = iwl_trans_pcie_dump_data,				\
 	.d3_suspend = iwl_trans_pcie_d3_suspend,			\
-	.d3_resume = iwl_trans_pcie_d3_resume,				\
-	.sync_nmi = iwl_trans_pcie_sync_nmi
+	.d3_resume = iwl_trans_pcie_d3_resume
 
 #ifdef CONFIG_PM_SLEEP
 #define IWL_TRANS_PM_OPS						\
@@ -3733,7 +3732,7 @@ out_no_pci:
 	return ERR_PTR(ret);
 }
 
-void iwl_trans_pcie_sync_nmi(struct iwl_trans *trans)
+void iwl_trans_sync_nmi(struct iwl_trans *trans)
 {
 	unsigned long timeout = jiffies + IWL_TRANS_NMI_TIMEOUT;
 
