90641090c8fcd8f6eaf8558fcc8259e82699ae51
Ap, Kamal <kamal.ap@intel.corp-partner.google.com>
2019-06-14 09:51:02 +0530

    Revert "CHROMIUM: ASoC: SOF: check mach before using it"
    This reverts commit 69de77c899d9569f5a727d453201bb043932dd34.
    
    BUG=b:135490563
    TEST=Test Audio use cases for GLK with full SOF patch series applied.
    
    Signed-off-by: Ap, Kamal <kamal.ap@intel.corp-partner.google.com>
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>

--- sound/soc/sof/sof-acpi-dev.c
+++ sound/soc/sof/sof-acpi-dev.c
@@ -231,10 +231,8 @@ static int sof_acpi_probe(struct platform_device *pdev)
 	}
 #endif
 
-	if (mach) {
-		mach->mach_params.platform = dev_name(dev);
-		mach->mach_params.acpi_ipc_irq_index = desc->irqindex_host_ipc;
-	}
+	mach->mach_params.platform = dev_name(dev);
+	mach->mach_params.acpi_ipc_irq_index = desc->irqindex_host_ipc;
 
 	sof_pdata->machine = mach;
 	sof_pdata->desc = desc;
