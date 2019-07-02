752fb328598c6f9b6cb277b21a845f72736a0c1a
Hsin-Yu Chao <hychao@google.com>
2019-07-02 15:26:30 +0800

    CHROMIUM: bluetooth/sco - Parse correct conn handle
    According to Core spec 5.4.3 HCI Synchronous Data Packets the
    Connection_Handle uses the first 12 bits of header.
    
    This change fixes the connection handle value. Otherwise if
    the succeeding 2 bits of packet status flag is set, it could
    result in connection look up failure and drop one SCO packet.
    
    BUG=chromium:980454
    TEST=Test wideband speech headset, verify there's no error
    printed for 'SCO packet for unknown connection handle'
    
    Change-Id: Idab09b69de768fc4113476409c9435af730dae7b
    Signed-off-by: Hsin-Yu Chao <hychao@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1718627
    Tested-by: Hsinyu Chao <hychao@chromium.org>
    Commit-Ready: Hsinyu Chao <hychao@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Shyh-In Hwang <josephsih@chromium.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -4238,7 +4238,7 @@ static void hci_scodata_packet(struct hci_dev *hdev, struct sk_buff *skb)
 
 	skb_pull(skb, HCI_SCO_HDR_SIZE);
 
-	handle = __le16_to_cpu(hdr->handle);
+	handle = __le16_to_cpu(hdr->handle) & 0xfff;
 
 	BT_DBG("%s len %d handle 0x%4.4x", hdev->name, skb->len, handle);
 
