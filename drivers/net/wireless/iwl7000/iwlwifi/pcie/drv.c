580e4b8a2bbb167f844d38fed9c83133999ee91d
Luca Coelho <luciano.coelho@intel.com>
2019-08-16 09:29:40 +0300

    CHROMIUM: iwl7000: pcie: fix recognition of QuZ devices
    If the HW revision of Qu devices we found is QuZ, then we need to
    switch the configuration accordingly in order to use the correct FW.
    
    Add a block of ifs in order do that.
    
    Change-Id: I161c87bc1562de7b0cb159094dc4a0a39f1e459d
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 00703ff2204ca7bc3806d93a44b723080dbd2640

--- drivers/net/wireless/iwl7000/iwlwifi/pcie/drv.c
+++ drivers/net/wireless/iwl7000/iwlwifi/pcie/drv.c
@@ -979,6 +979,23 @@ static int iwl_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 		else if (cfg == &iwl9560_2ac_160_cfg_qu_b0_jf_b0)
 			cfg = &iwl9560_2ac_160_cfg_qu_c0_jf_b0;
 	}
+
+	/* same thing for QuZ... */
+	if (iwl_trans->hw_rev == CSR_HW_REV_TYPE_QUZ) {
+		if (cfg == &iwl_ax101_cfg_qu_hr)
+			cfg = &iwl_ax101_cfg_quz_hr;
+		else if (cfg == &iwl_ax201_cfg_qu_hr)
+			cfg = &iwl_ax201_cfg_quz_hr;
+		else if (cfg == &iwl9461_2ac_cfg_qu_b0_jf_b0)
+			cfg = &iwl9461_2ac_cfg_quz_a0_jf_b0_soc;
+		else if (cfg == &iwl9462_2ac_cfg_qu_b0_jf_b0)
+			cfg = &iwl9462_2ac_cfg_quz_a0_jf_b0_soc;
+		else if (cfg == &iwl9560_2ac_cfg_qu_b0_jf_b0)
+			cfg = &iwl9560_2ac_cfg_quz_a0_jf_b0_soc;
+		else if (cfg == &iwl9560_2ac_160_cfg_qu_b0_jf_b0)
+			cfg = &iwl9560_2ac_160_cfg_quz_a0_jf_b0_soc;
+	}
+
 #endif
 	/* now set the real cfg we decided to use */
 	iwl_trans->cfg = cfg;
