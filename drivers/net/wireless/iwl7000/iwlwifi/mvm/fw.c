66780c828db407e301beba55bcea64a6a4ebb9df
Luca Coelho <luciano.coelho@intel.com>
2019-08-28 15:20:34 +0300

    CHROMIUM: iwl7000: mvm: fix compilation error in ppag code without CONFIG_ACPI
    When CONFIG_ACPI is not enabled, compilation fails due to
    redeclaration and warns on static vs. non-static function
    declarations.
    
    Fix that.
    
    Change-Id: I259d8e12ed4b3b1c5b47e1f9616953e89e34cb6e
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: a072e79c298643fb04b2da7048477d8b71450870

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/fw.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/fw.c
@@ -1318,12 +1318,7 @@ int iwl_mvm_get_sar_geo_profile(struct iwl_mvm *mvm)
 	return -ENOENT;
 }
 
-static int iwl_mvm_get_ppag_table(struct iwl_mvm *mvm)
-{
-	return -ENOENT;
-}
-
-static int iwl_mvm_ppag_send_cmd(struct iwl_mvm *mvm)
+int iwl_mvm_ppag_send_cmd(struct iwl_mvm *mvm)
 {
 	return -ENOENT;
 }
