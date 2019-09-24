a26f24d8b976682877fce0ffa4a001d92d19be65
Luca Coelho <luciano.coelho@intel.com>
2019-09-24 12:12:27 +0300

    CHROMIUM: iwl7000: fw: don't send GEO_TX_POWER_LIMIT command to FW version 36
    The intention was to have the GEO_TX_POWER_LIMIT command in FW version
    36 as well, but not all 8000 family got this feature enabled.  The
    8000 family is the only one using version 36, so skip this version
    entirely.  If we try to send this command to the firmwares that do not
    support it, we get a BAD_COMMAND response from the firmware.
    
    This fixes https://bugzilla.kernel.org/show_bug.cgi?id=204151.
    
    Change-Id: I11ea8229111ad21266ce5850078a67bf4d2f1a3d
    Cc: stable@vger.kernel.org # 4.19+
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 5da8c517edbe37dfc3b738f24b02067ffc6a5684

--- drivers/net/wireless/iwl7000/iwlwifi/fw/acpi.c
+++ drivers/net/wireless/iwl7000/iwlwifi/fw/acpi.c
@@ -464,11 +464,13 @@ bool iwl_sar_geo_support(struct iwl_fw_runtime *fwrt)
 	 * firmware versions.  Unfortunately, we don't have a TLV API
 	 * flag to rely on, so rely on the major version which is in
 	 * the first byte of ucode_ver.  This was implemented
-	 * initially on version 38 and then backported to 36, 29 and
-	 * 17.
+	 * initially on version 38 and then backported to 29 and 17.
+	 * The intention was to have it in 36 as well, but not all
+	 * 8000 family got this feature enabled.  The 8000 family is
+	 * the only one using version 36, so skip this version
+	 * entirely.
 	 */
 	return IWL_UCODE_SERIAL(fwrt->fw->ucode_ver) >= 38 ||
-	       IWL_UCODE_SERIAL(fwrt->fw->ucode_ver) == 36 ||
 	       IWL_UCODE_SERIAL(fwrt->fw->ucode_ver) == 29 ||
 	       IWL_UCODE_SERIAL(fwrt->fw->ucode_ver) == 17;
 }
