89a2af4284b4c0f95b398b4e6406fe50bac479e0
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2019-10-01 14:45:48 -0700

    CHROMIUM: Bluetooth: btusb: configure remote_wakeup for realtek chips
    Realtek devices will enter global suspend and lose firmware if
    SET_FEATURE(DEVICE_REMOTE_WAKEUP) is not set. Tie ->do_remote_wakeup to
    whether the device is configured for wakeup and do a reset on resume
    otherwise (to make sure the firmware is restored). Also add
    non-persistent setup quirk to HCI.
    
    BUG=chromium:941827
    TEST=Verified reset and wakeup both work correctly via sysfs.
         First, found USB parent with power control by doing:
         udevadm info -a -p /sys/class/bluetooth/hci0
         The USB parent has idVendor=0bda, idProduct=b00c and SUBSYSTEM==usb
         With /sys/$usb_devpath/power/wakeup = disabled, the Realtek chip
         resets in suspend (observe in dmesg a disconnect and reconnect of
         usb1-3 on Blooglet; udevadm monitor should also show hci0 device
         being re-added).
         With /sys/$usb_devpath/power/wakeup = enabled, the Realtek chip
         does not reset and a connected bluetooth keyboard was able to wake
         it from suspend (powerd_dbus_suspend and then press key on
            bluetooth keyboard to confirm it wakes).
    
    Change-Id: I8fa86229e8202c11d56d60c7c2074b1021c04bc8
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1834459
    Reviewed-by: Shyh-In Hwang <josephsih@chromium.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -440,6 +440,7 @@ static const struct dmi_system_id btusb_needs_reset_resume_table[] = {
 #define BTUSB_DIAG_RUNNING	10
 #define BTUSB_OOB_WAKE_ENABLED	11
 #define BTUSB_HW_RESET_ACTIVE	12
+#define BTUSB_SUSPEND_REMOTE_WAKE	13
 
 struct btusb_data {
 	struct hci_dev       *hdev;
@@ -3387,11 +3388,15 @@ static int btusb_probe(struct usb_interface *intf,
 		hdev->shutdown = btrtl_shutdown_realtek;
 		hdev->cmd_timeout = btusb_rtl_cmd_timeout;
 
-		/* Realtek devices lose their updated firmware over suspend,
-		 * but the USB hub doesn't notice any status change.
-		 * Explicitly request a device reset on resume.
+		/* Realtek devices lose their updated firmware over global
+		 * suspend, which happens when host doesn't send SET_FEATURE
+		 * (DEVICE_REMOTE_WAKEUP). Set the remote wake flag so suspend
+		 * will set the REMOTE_WAKEUP correctly (or reset_resume). Set
+		 * the HCI quirk so setup will be called every time open is
+		 * called (firmware is restored or no-op if unnecessary).
 		 */
-		interface_to_usbdev(intf)->quirks |= USB_QUIRK_RESET_RESUME;
+		set_bit(BTUSB_SUSPEND_REMOTE_WAKE, &data->flags);
+		set_bit(HCI_QUIRK_NON_PERSISTENT_SETUP, &hdev->quirks);
 	}
 #endif
 
@@ -3565,6 +3570,17 @@ static int btusb_suspend(struct usb_interface *intf, pm_message_t message)
 		set_bit(BTUSB_OOB_WAKE_ENABLED, &data->flags);
 		enable_irq_wake(data->oob_wake_irq);
 		enable_irq(data->oob_wake_irq);
+	}
+
+	/* This flag indicates that the bluetooth firmware requires remote-wake
+	 * on suspend or it will lose firmware. Set the remote_wakeup feature
+	 * only if the device is configured for wakeup.
+	 */
+	if (test_bit(BTUSB_SUSPEND_REMOTE_WAKE, &data->flags)) {
+		if (device_may_wakeup(&data->udev->dev))
+			data->udev->do_remote_wakeup = 1;
+		else
+			data->udev->reset_resume = 1;
 	}
 
 	return 0;
