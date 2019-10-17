7ede16fe00cd93a60b2515615bc180ed822fc797
Chris Down <chris@chrisdown.name>
2019-10-17 10:48:41 -0400

    CHROMIUM: iwl7000: Don't IWL_WARN on FW reconfiguration
    IWL_WARN seems excessive here since this can happen during normal
    operation. Every time I connect to a new network with 8086:24fd I get
    this as KERN_WARNING on the console, which mildly distracts from other
    more pressing messages. For example:
    
        % sudo journalctl _TRANSPORT=kernel | grep -c 'FW already configured'
        403
    
    Change-Id: I7dcc28adec12d79432dff75360b42ab171e09f0e
    Signed-off-by: Chris Down <chris@chrisdown.name>
    Cc: Shahar S Matityahu <shahar.s.matityahu@intel.com>
    Cc: Luca Coelho <luciano.coelho@intel.com>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-wireless@vger.kernel.org
    Link: https://lore.kernel.org/r/20191017144841.GA16393@chrisdown.name
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 2a063fe7db8a15d0f3c1ded3428f8e0a381c3d68

--- drivers/net/wireless/iwl7000/iwlwifi/fw/dbg.c
+++ drivers/net/wireless/iwl7000/iwlwifi/fw/dbg.c
@@ -2655,7 +2655,7 @@ int iwl_fw_start_dbg_conf(struct iwl_fw_runtime *fwrt, u8 conf_id)
 		return -EINVAL;
 
 	if (fwrt->dump.conf != FW_DBG_INVALID)
-		IWL_WARN(fwrt, "FW already configured (%d) - re-configuring\n",
+		IWL_INFO(fwrt, "FW already configured (%d) - re-configuring\n",
 			 fwrt->dump.conf);
 
 #if IS_ENABLED(CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDES) && \
