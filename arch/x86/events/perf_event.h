7e0b83e231a1b0741f90ba5dee3097ea28008f7a
Alexey Budankov <alexey.budankov@linux.intel.com>
2019-10-23 10:12:54 +0300

    UPSTREAM: perf/x86/intel: Implement LBR callstack context synchronization
    Implement intel_pmu_lbr_swap_task_ctx() method updating counters
    of the events that requested LBR callstack data on a sample.
    
    The counter can be zero for the case when task context belongs to
    a thread that has just come from a block on a futex and the context
    contains saved (lbr_stack_state == LBR_VALID) LBR register values.
    
    For the values to be restored at LBR registers on the next thread's
    switch-in event it swaps the counter value with the one that is
    expected to be non zero at the previous equivalent task perf event
    context.
    
    Swap operation type ensures the previous task perf event context
    stays consistent with the amount of events that requested LBR
    callstack data on a sample.
    
    BUG=b:170558957
    TEST=build/boot
    
    Signed-off-by: Alexey Budankov <alexey.budankov@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Kan Liang <kan.liang@linux.intel.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Song Liu <songliubraving@fb.com>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vince Weaver <vincent.weaver@maine.edu>
    Link: https://lkml.kernel.org/r/261ac742-9022-c3f4-5885-1eae7415b091@linux.intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 421ca868ea3b7c1ca1a541ed6dff3c101a563b95)
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    Change-Id: I26c12534442c494fe3fb5a7f1b200cf723ea7867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2744630
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -957,6 +957,9 @@ void intel_pmu_pebs_sched_task(struct perf_event_context *ctx, bool sched_in);
 
 void intel_ds_init(void);
 
+void intel_pmu_lbr_swap_task_ctx(struct perf_event_context *prev,
+				 struct perf_event_context *next);
+
 void intel_pmu_lbr_sched_task(struct perf_event_context *ctx, bool sched_in);
 
 u64 lbr_from_signext_quirk_wr(u64 val);
