2d2c5c7347456fa4684bc9caa41d6685524d5d3c
0ctobot <return.of.octobot@gmail.com>
2019-11-05 05:07:03 -0500

    coretech/houston: Disable fps_boost/ddrfreq_hispeed_enable by default
    Yeah, how about no. I'm tempted to lock permissions on these nodes,
    but hopefully this will be sufficient as it doesn't appear that
    userspace makes any attempts to re-engage this.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- coretech/houston/houston.c
+++ coretech/houston/houston.c
@@ -192,7 +192,7 @@ module_param_named(bat_update_period_us, bat_update_period_us, ulong, 0664);
 extern void bq27541_force_update_current(void);
 
 /* fps boost switch */
-static bool fps_boost_enable = true;
+static bool fps_boost_enable = false;
 module_param_named(fps_boost_enable, fps_boost_enable, bool, 0664);
 
 static bool fps_boost_force_enable;
@@ -209,7 +209,7 @@ module_param_named(cpufreq_hispeed_enable, cpufreq_hispeed_enable, bool, 0664);
 static unsigned int cpufreq_hispeed[HT_CLUSTERS] = { 1209600, 1612800, 1612800 };
 module_param_array_named(cpufreq_hispeed, cpufreq_hispeed, uint, NULL, 0664);
 
-static bool ddrfreq_hispeed_enable = true;
+static bool ddrfreq_hispeed_enable = false;
 module_param_named(ddrfreq_hispeed_enable, ddrfreq_hispeed_enable, bool, 0664);
 
 static unsigned int ddrfreq_hispeed = 1017;
