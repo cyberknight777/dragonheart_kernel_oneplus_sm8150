71097fb03940708ca66f1714ef47ec3a926402cf
Luca Coelho <luciano.coelho@intel.com>
2019-11-12 14:23:37 +0200

    CHROMIUM: iwl7000: pci: don't consider QnJ integrated
    The QnJ devices are not integrated, so we should not use the
    integrated flag and the xtal latency value when setting it up.
    
    Change-Id: I4557b4fed17bceb70521976769e35e606a5adea5
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 108e6471dbb93e0ddfff30e7288ebe668f3478e7

--- drivers/net/wireless/iwl7000/iwlwifi/pcie/drv.c
+++ drivers/net/wireless/iwl7000/iwlwifi/pcie/drv.c
@@ -345,7 +345,7 @@ static const struct pci_device_id iwl_hw_card_ids[] = {
 	{IWL_PCI_DEVICE(0x43F0, PCI_ANY_ID, iwl_qu_long_latency_trans_cfg)},
 	{IWL_PCI_DEVICE(0xA0F0, PCI_ANY_ID, iwl_qu_long_latency_trans_cfg)},
 
-	{IWL_PCI_DEVICE(0x2720, PCI_ANY_ID, iwl_qu_trans_cfg)},
+	{IWL_PCI_DEVICE(0x2720, PCI_ANY_ID, iwl_qnj_trans_cfg)},
 
 	{IWL_PCI_DEVICE(0x2723, PCI_ANY_ID, iwl_ax200_trans_cfg)},
 
