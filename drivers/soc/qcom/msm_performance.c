8f38e7dd4b324bcbe1501ae60f521b28b6aaae60
Zachariah Kennedy <zkennedy87@gmail.com>
2019-12-12 16:49:58 +0000

    core_ctl.c: Stub CAF's Core Control to prepare for !CONFIG_SCHED_WALT
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    (cherry picked from commit a6e8335352697f3fa128588398404d1a2823e601)
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -383,67 +383,6 @@ static int init_events_group(void)
 	return 0;
 }
 
-static void nr_notify_userspace(struct work_struct *work)
-{
-	sysfs_notify(notify_kobj, NULL, "aggr_top_load");
-	sysfs_notify(notify_kobj, NULL, "aggr_big_nr");
-}
-
-static int msm_perf_core_ctl_notify(struct notifier_block *nb,
-				    unsigned long unused,
-				    void *data)
-{
-	static unsigned int tld, nrb, i;
-	static DECLARE_WORK(sysfs_notify_work, nr_notify_userspace);
-	struct core_ctl_notif_data *d = data;
-
-
-	nrb += d->nr_big;
-	tld += d->coloc_load_pct;
-	i++;
-	if (i == POLL_INT) {
-		aggr_big_nr = ((nrb%POLL_INT) ? 1 : 0) + nrb/POLL_INT;
-		aggr_top_load = tld/POLL_INT;
-		tld = 0;
-		nrb = 0;
-		i = 0;
-		schedule_work(&sysfs_notify_work);
-	}
-	return NOTIFY_OK;
-}
-
-static struct notifier_block msm_perf_nb = {
-	.notifier_call = msm_perf_core_ctl_notify
-};
-
-static bool core_ctl_register;
-static int set_core_ctl_register(const char *buf, const struct kernel_param *kp)
-{
-	int ret;
-	bool old_val = core_ctl_register;
-
-	ret = param_set_bool(buf, kp);
-	if (ret < 0)
-		return ret;
-
-	if (core_ctl_register == old_val)
-		return 0;
-
-	if (core_ctl_register)
-		core_ctl_notifier_register(&msm_perf_nb);
-	else
-		core_ctl_notifier_unregister(&msm_perf_nb);
-
-	return 0;
-}
-
-static const struct kernel_param_ops param_ops_cc_register = {
-	.set = set_core_ctl_register,
-	.get = param_get_bool,
-};
-module_param_cb(core_ctl_register, &param_ops_cc_register,
-		&core_ctl_register, 0644);
-
 static int __init msm_performance_init(void)
 {
 	unsigned int cpu;
