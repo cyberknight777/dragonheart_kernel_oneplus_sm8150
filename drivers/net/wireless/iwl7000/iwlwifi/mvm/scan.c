1ce94d6e6e704949894385fc7a9bc6ac4301aed6
Tova Mussai <tova.mussai@intel.com>
2020-01-12 11:48:31 +0200

    CHROMIUM: iwl7000: mvm: use cfg80211_is_psc() in sched scan flow
    Use cfg80211_is_psc() to check for PSC channel is sched scan flow
    
    Change-Id: I24b6597a7addddc16b57e3146361f3b2e059c782
    Signed-off-by: Tova Mussai <tova.mussai@intel.com>
    iwl7000-tree: 95939a7569130a831344a594e8c82b7719f7b77c

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/scan.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/scan.c
@@ -5,10 +5,9 @@
  *
  * GPL LICENSE SUMMARY
  *
- * Copyright(c) 2012 - 2014 Intel Corporation. All rights reserved.
  * Copyright(c) 2013 - 2015 Intel Mobile Communications GmbH
  * Copyright(c) 2016 - 2017 Intel Deutschland GmbH
- * Copyright(c) 2018 - 2019 Intel Corporation
+ * Copyright(c) 2012 - 2014, 2018 - 2020 Intel Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License as
@@ -28,10 +27,9 @@
  *
  * BSD LICENSE
  *
- * Copyright(c) 2012 - 2014 Intel Corporation. All rights reserved.
  * Copyright(c) 2013 - 2015 Intel Mobile Communications GmbH
  * Copyright(c) 2016 - 2017 Intel Deutschland GmbH
- * Copyright(c) 2018 - 2019 Intel Corporation
+ * Copyright(c) 2012 - 2014, 2018 - 2019 Intel Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
