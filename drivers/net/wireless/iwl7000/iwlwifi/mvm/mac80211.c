acdefaa3a61251bb24d46859d23e72ceb12a141b
Shaul Triebitz <shaul.triebitz@intel.com>
2020-01-19 22:05:53 +0200

    CHROMIUM: iwl7000: mvm: set PROTECTED_TWT feature if supported by firmware
    Change-Id: I86ba17db16fc99bb094bef4d0cc40ee99d5ba233
    Signed-off-by: Shaul Triebitz <shaul.triebitz@intel.com>
    iwl7000-tree: 63a596426c61029196d31d511b45f92bce387be0

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
@@ -866,6 +866,10 @@ int iwl_mvm_mac_setup_register(struct iwl_mvm *mvm)
 	iwl_mvm_vendor_cmds_register(mvm);
 #endif
 
+	if (fw_has_capa(&mvm->fw->ucode_capa, IWL_UCODE_TLV_CAPA_PROTECTED_TWT))
+		wiphy_ext_feature_set(hw->wiphy,
+				      NL80211_EXT_FEATURE_PROTECTED_TWT);
+
 	hw->wiphy->available_antennas_tx = iwl_mvm_get_valid_tx_ant(mvm);
 	hw->wiphy->available_antennas_rx = iwl_mvm_get_valid_rx_ant(mvm);
 
