e4144de1a470f102dac5d7a071213e3edccbff29
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-05 11:49:31 -0500

    [FORWARDPORT/ADAPT] drm/msm: keep dim_layer and HBM more in sync
     - Enable/disable HBM asynchronously when dim_layer state changes
    
    [RealJohnGalt]:
     - Adapt for new onpress FOD implementation.
     - Do not attempt to disable HBM if HBM_flag is set (hdb issue).
     - Original commit had post wait skip. However on qhd this affects
       sync negatively (dim occurs before hbm resulting in a "black flash").
       In reality only fhd panels have improper reported post_wait duration
       reported for sync. Tune them to still protect against flashbangs.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -35,6 +35,7 @@
 #include "dsi_drm.h"
 #include "dsi_display.h"
 #include "sde_crtc.h"
+#include "sde_hw_mdss.h"
 #include "sde_rm.h"
 #include "sde_trace.h"
 /**
@@ -737,10 +738,11 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 	return rc;
 }
 
-int dsi_panel_tx_cmd_set(struct dsi_panel *panel,
-				enum dsi_cmd_set_type type)
+int __dsi_panel_tx_cmd_set(struct dsi_panel *panel,
+				enum dsi_cmd_set_type type,
+				bool fod_usage)
 {
-	int rc = 0, i = 0;
+	int rc = 0, i = 0, wait_multi = 1000;
 	ssize_t len;
 	struct dsi_cmd_desc *cmds;
 	u32 count;
@@ -776,14 +778,29 @@ int dsi_panel_tx_cmd_set(struct dsi_panel *panel,
 			pr_err("failed to set cmds(%d), rc=%d\n", type, rc);
 			goto error;
 		}
+
+		if (fod_usage) {
+			if (panel->hw_type == DSI_PANEL_SAMSUNG_SOFEF03F_M)
+				wait_multi = 700;
+			else if (panel->hw_type == DSI_PANEL_SAMSUNG_S6E3FC2X01)
+				wait_multi = 500;
+		}
+
 		if (cmds->post_wait_ms)
-			usleep_range(cmds->post_wait_ms*1000,
-					((cmds->post_wait_ms*1000)+10));
+			usleep_range(cmds->post_wait_ms*wait_multi,
+					((cmds->post_wait_ms*wait_multi)+10));
 		cmds++;
 	}
 error:
 	return rc;
 }
+
+int dsi_panel_tx_cmd_set(struct dsi_panel *panel,
+				enum dsi_cmd_set_type type)
+{
+	return __dsi_panel_tx_cmd_set(panel, type, false);
+}
+
 
 static int dsi_panel_pinctrl_deinit(struct dsi_panel *panel)
 {
@@ -951,56 +968,47 @@ error:
 	return rc;
 }
 
-int dsi_panel_op_set_hbm_mode(struct dsi_panel *panel, int level)
+bool hbm_active;
+int hbm_level;
+static void set_hbm_mode(struct work_struct *work)
 {
-	int rc = 0;
-	u32 count;
-    struct dsi_display_mode *mode;
+    struct dsi_panel *panel = get_main_display()->panel;
+    int level = hbm_level;
 
-	if (!panel || !panel->cur_mode) {
-		pr_err("Invalid params\n");
-		return -EINVAL;
-	}
+    mutex_lock(&panel->panel_lock);
 
-	mutex_lock(&panel->panel_lock);
-
-    mode = panel->cur_mode;
     switch (level) {
     case 0:
-        count = mode->priv_info->cmd_sets[DSI_CMD_SET_HBM_OFF].count;
-        if (!count) {
-            pr_err("This panel does not support HBM mode off.\n");
-            goto error;
-        } else {
-            rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_HBM_OFF);
-			printk(KERN_ERR"When HBM OFF -->hbm_backight = %d panel->bl_config.bl_level =%d\n",panel->hbm_backlight,panel->bl_config.bl_level);
-			rc= dsi_panel_update_backlight(panel,panel->hbm_backlight);
+      if (!HBM_flag) {
+	dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_HBM_OFF);
+	pr_err(
+		 "When HBM OFF -->hbm_backight = %d panel->bl_config.bl_level =%d\n",
+		 panel->hbm_backlight, panel->bl_config.bl_level);
+	dsi_panel_update_backlight(panel, panel->hbm_backlight);
         }
     break;
-
     case 1:
-        count = mode->priv_info->cmd_sets[DSI_CMD_SET_HBM_ON_5].count;
-        if (!count) {
-            pr_err("This panel does not support HBM mode.\n");
-            goto error;
-        } else {
-            rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_HBM_ON_5);
-        }
+        __dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_HBM_ON_5, true);
     break;
-    default:
-    break;
-
     }
+    mutex_unlock(&panel->panel_lock);
     pr_err("Set HBM Mode = %d\n", level);
-	if(level==5)
-	{
-		pr_err("HBM == 5 for fingerprint\n");
-	}
+}
 
-error:
-	mutex_unlock(&panel->panel_lock);
+DECLARE_WORK(hbm_work, set_hbm_mode);
+/*
+ * This function is used only for "op_friginer_print_hbm".
+ *
+ * As we are triggering hbm_work after dim layer is committed,
+ * remove the call here.
+ */
+int dsi_panel_op_set_hbm_mode(struct dsi_panel *panel, int level)
+{
+	hbm_active = !!level;
 
-	return rc;
+	// queue_work(system_highpri_wq, &hbm_work);
+
+	return 0;
 }
 
 static int dsi_panel_update_pwm_backlight(struct dsi_panel *panel,
@@ -5460,7 +5468,7 @@ int dsi_panel_set_hbm_mode(struct dsi_panel *panel, int level)
 			}
 			else {
 				HBM_flag = true;
-				rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_HBM_ON_5);
+				__dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_HBM_ON_5, true);
 				pr_debug("Send DSI_CMD_SET_HBM_ON_5 cmds.\n");
 			}
 			break;
@@ -6061,6 +6069,7 @@ int dsi_panel_tx_gamma_cmd_set(struct dsi_panel *panel,
 			pr_err("failed to set cmds(%d), rc=%d\n", type, rc);
 			goto error;
 		}
+
 		if (cmds->post_wait_ms)
 			usleep_range(cmds->post_wait_ms*1000,
 					((cmds->post_wait_ms*1000)+10));
--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -546,6 +546,11 @@ int sde_connector_update_backlight(struct drm_connector *connector)
 
 extern int dsi_panel_tx_cmd_set(struct dsi_panel *panel,
 				enum dsi_cmd_set_type type);
+
+extern int __dsi_panel_tx_cmd_set(struct dsi_panel *panel,
+				enum dsi_cmd_set_type type,
+				bool fod_usage);
+
 static int _sde_connector_update_hbm(struct sde_connector *c_conn)
 {
 	struct drm_connector *connector = &c_conn->base;
@@ -614,29 +619,25 @@ static int _sde_connector_update_hbm(struct sde_connector *c_conn)
 			mutex_lock(&dsi_display->panel->panel_lock);
 			if (dsi_display->panel->aod_status==1 && !finger_type) {
 				if (dsi_display->panel->aod_mode == 2) {
-					rc = dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_AOD_OFF_HBM_ON_SETTING);
+					dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_AOD_OFF_HBM_ON_SETTING);
 					pr_err("Send DSI_CMD_AOD_OFF_HBM_ON_SETTING cmds\n");
 				} else {
-					rc = dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_REAL_AOD_OFF_HBM_ON_SETTING);
+					dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_REAL_AOD_OFF_HBM_ON_SETTING);
 					pr_err("Send DSI_CMD_REAL_AOD_OFF_HBM_ON_SETTING cmds\n");
 				}
 				aod_fod_flag = true;
 			}
 			else if (dsi_display->panel->aod_status == 1 && finger_type) {
-				rc = dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_SET_AOD_OFF_NEW);
+				dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_SET_AOD_OFF_NEW);
 				pr_err("qdt aod off\n");
 			}
 			else {
 				//sde_encoder_poll_line_counts(drm_enc);
-				rc = dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_SET_HBM_ON_5);
+				__dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_SET_HBM_ON_5, true);
 				pr_err("Send DSI_CMD_SET_HBM_ON_5 cmds\n");
 			}
 			SDE_ATRACE_END("set_hbm_on");
 			mutex_unlock(&dsi_display->panel->panel_lock);
-			if (rc) {
-				pr_err("failed to send DSI_GAMMA_CMD_SET_HBM_ON cmds, rc=%d\n", rc);
-				return rc;
-			}
 		}
 		else {
 			SDE_ATRACE_BEGIN("set_hbm_off");
@@ -645,7 +646,7 @@ static int _sde_connector_update_hbm(struct sde_connector *c_conn)
 			mutex_lock(&dsi_display->panel->panel_lock);
 			if (dsi_display->panel->aod_status == 1 && !finger_type) {
 				if(oneplus_dim_status == 5){
-					rc = dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_SET_HBM_OFF);
+					dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_SET_HBM_OFF);
 					pr_err("Send DSI_CMD_SET_HBM_OFF cmds\n");
 					aod_fod_flag = true;
 					dsi_display->panel->aod_status = 0;
@@ -665,7 +666,7 @@ static int _sde_connector_update_hbm(struct sde_connector *c_conn)
 							pr_err("Send DSI_CMD_SET_AOD_ON_3 cmds\n");
 						}
 					} else if (oneplus_onscreenfp_status == 0) {
-						rc = dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_HBM_OFF_AOD_ON_SETTING);
+						dsi_panel_tx_cmd_set(dsi_display->panel, DSI_CMD_HBM_OFF_AOD_ON_SETTING);
 						aod_layer_hide = 1;
 						pr_err("Send DSI_CMD_HBM_OFF_AOD_ON_SETTING  cmds\n");
 					}
@@ -688,10 +689,6 @@ static int _sde_connector_update_hbm(struct sde_connector *c_conn)
 			SDE_ATRACE_END("set_hbm_off");
 			mutex_unlock(&dsi_display->panel->panel_lock);
 			_sde_connector_update_bl_scale(c_conn);
-			if (rc) {
-				pr_err("failed to send DSI_CMD_HBM_OFF cmds, rc=%d\n", rc);
-				return rc;
-			}
 		}
 	}
 	return 0;
