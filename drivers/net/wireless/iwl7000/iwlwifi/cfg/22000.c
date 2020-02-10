43e5c2f6574db9761a259666fba7224ec2a4e504
Golan Ben Ami <golan.ben.ami@intel.com>
2020-02-10 19:59:26 +0200

    CHROMIUM: iwl7000: Revert "iwlwifi: pcie: move external device strings to local consts"
    This patch causes a naming problem, so we look for iwlwifi-Qu-b0-hr-b0-*
    ucode instead of looking for iwlwifi-QuZ-a0-hr-b0-* ucode.
    
    Revert for now and fix this later.
    
    This reverts commit 9a588ed5e59bd22fb77ff9292b8987f70d66e04e.
    
    Change-Id: I1ce5161e7ec9af4d4df2a5ff02f8017328b2af2c
    Signed-off-by: Golan Ben Ami <golan.ben.ami@intel.com>
    iwl7000-tree: 0bffd8b8bad5e10e3863d4dad9d3984af3adbd52

--- drivers/net/wireless/iwl7000/iwlwifi/cfg/22000.c
+++ drivers/net/wireless/iwl7000/iwlwifi/cfg/22000.c
@@ -336,6 +336,15 @@ const struct iwl_cfg_trans_params iwl_ax200_trans_cfg = {
 	.bisr_workaround = 1,
 };
 
+const char iwl_ax200_name[] = "Intel(R) Wi-Fi 6 AX200 160MHz";
+const char iwl_ax201_name[] = "Intel(R) Wi-Fi 6 AX201 160MHz";
+const char iwl_ax101_name[] = "Intel(R) Wi-Fi 6 AX101";
+
+const char iwl_ax200_killer_1650w_name[] =
+	"Killer(R) Wi-Fi 6 AX1650w 160MHz Wireless Network Adapter (200D2W)";
+const char iwl_ax200_killer_1650x_name[] =
+	"Killer(R) Wi-Fi 6 AX1650x 160MHz Wireless Network Adapter (200NGW)";
+
 const struct iwl_cfg iwl_qu_b0_hr1_b0 = {
 	.fw_name_pre = IWL_QU_B_HR_B_FW_PRE,
 	IWL_DEVICE_22500,
