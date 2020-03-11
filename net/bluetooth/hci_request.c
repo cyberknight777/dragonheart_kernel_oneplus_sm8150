f7d5097b8dc97e633c3a43336ff424ae110a0a48
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2020-03-11 08:54:00 -0700

    BACKPORT: FROMGIT: Bluetooth: Handle PM_SUSPEND_PREPARE and PM_POST_SUSPEND
    Register for PM_SUSPEND_PREPARE and PM_POST_SUSPEND to make sure the
    Bluetooth controller is prepared correctly for suspend/resume. Implement
    the registration, scheduling and task handling portions only in this
    patch.
    
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    (cherry picked from commit 9952d90ea2885d7cbf80cd233f694f09a9c0eaec
    git://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git master)
    
    BUG=b:146927113
    TEST=None
    
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Change-Id: Ia2897746488fe630e71b8f51ddbf06e8153802e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2119447
    Reviewed-by: Alain Michaud <alainm@chromium.org>

--- net/bluetooth/hci_request.c
+++ net/bluetooth/hci_request.c
@@ -844,6 +844,21 @@ void hci_req_add_le_passive_scan(struct hci_request *req)
 		    &enable_cp);
 }
 
+/* Call with hci_dev_lock */
+void hci_req_prepare_suspend(struct hci_dev *hdev, enum suspended_state next)
+{
+	if (next == hdev->suspend_state) {
+		bt_dev_dbg(hdev, "Same state before and after: %d", next);
+		goto done;
+	}
+
+	hdev->suspend_state = next;
+
+done:
+	clear_bit(SUSPEND_PREPARE_NOTIFIER, hdev->suspend_tasks);
+	wake_up(&hdev->suspend_wait_q);
+}
+
 static u8 get_cur_adv_instance_scan_rsp_len(struct hci_dev *hdev)
 {
 	u8 instance = hdev->cur_adv_instance;
