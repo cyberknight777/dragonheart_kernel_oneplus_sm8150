efbb25bbb39cded623d65ef001b7a1e57046cba9
Alain Michaud <alainmichaud@google.com>
2020-03-23 14:10:46 -0400

    Revert "CHROMIUM: bluetooth: determine wide band speech by controller id"
    This reverts commit f52720a21dc9e39f671e43f61f91e0955d50c1f1.
    
    Reason for revert: Aligning with upstream.
    
    BUG=b:146667535
    TEST=Built and tested WBS on Atlas
    
    Signed-off-by: Alain Michaud <alainm@chromium.org>
    Change-Id: I33f5d459d7315c9f1822d91769a29f0295eab075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2121101
    Commit-Queue: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -3484,9 +3484,6 @@ static int btusb_probe(struct usb_interface *intf,
 
 	usb_set_intfdata(intf, data);
 
-	hdev->controller_id.idVendor = id->idVendor;
-	hdev->controller_id.idProduct = id->idProduct;
-
 	return 0;
 
 out_free_dev:
