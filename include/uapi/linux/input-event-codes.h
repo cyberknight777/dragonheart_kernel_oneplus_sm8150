c52175a75d3ef6f3f6b270218167f983dfe6aaab
Meng Wang <mengw@codeaurora.org>
2020-04-23 08:39:56 +0800

    Revert "ALSA: jack: Add support to report second microphone"
    This reverts commit 63c5aec9697ab9faa0785c04bc52217a96f53b72.
    This change is not needed anymore as ANC is not supported.
    
    Change-Id: I522971a59945a84a71d243120d2b43ac67f2ae7f
    Signed-off-by: Meng Wang <mengw@codeaurora.org>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -803,8 +803,7 @@
 #define SW_PEN_INSERTED		0x0f  /* set = pen inserted */
 #define SW_HPHL_OVERCURRENT	0x10  /* set = over current on left hph */
 #define SW_HPHR_OVERCURRENT	0x11  /* set = over current on right hph */
-#define SW_MICROPHONE2_INSERT	0x12  /* set = inserted */
-#define SW_UNSUPPORT_INSERT	0x13  /* set = unsupported device inserted */
+#define SW_UNSUPPORT_INSERT	0x12  /* set = unsupported device inserted */
 #define SW_MACHINE_COVER	0x14  /* set = cover closed */
 #define SW_MAX			0x20
 #define SW_CNT			(SW_MAX+1)
