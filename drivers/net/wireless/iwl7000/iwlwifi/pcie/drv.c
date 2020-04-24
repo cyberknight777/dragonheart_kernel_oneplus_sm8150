bdd4147310618ad46f7178b17830f39570ba6976
Luca Coelho <luciano.coelho@intel.com>
2020-04-24 11:22:35 +0300

    CHROMIUM: iwl7000: pcie: handle QuZ configs with killer NICs as well
    The killer devices were left out of the checks that convert Qu-B0 to
    QuZ configurations.  Add them.
    
    Change-Id: I273a098064a22577e4fca767910fd9cf0013f5cb
    Cc: stable@vger.kernel.org # v5.3+
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 7f4551372557d84877c4c6d05cce1516d03935e3

--- drivers/net/wireless/iwl7000/iwlwifi/pcie/drv.c
+++ drivers/net/wireless/iwl7000/iwlwifi/pcie/drv.c
@@ -949,6 +949,10 @@ static int iwl_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	if (iwl_trans->hw_rev == CSR_HW_REV_TYPE_QUZ) {
 		if (iwl_trans->cfg == &iwl_ax201_cfg_qu_hr)
 			iwl_trans->cfg = &iwl_ax201_cfg_quz_hr;
+		else if (iwl_trans->cfg == &killer1650s_2ax_cfg_qu_b0_hr_b0)
+			iwl_trans->cfg = &iwl_ax1650s_cfg_quz_hr;
+		else if (iwl_trans->cfg == &killer1650i_2ax_cfg_qu_b0_hr_b0)
+			iwl_trans->cfg = &iwl_ax1650i_cfg_quz_hr;
 	}
 
 #endif
