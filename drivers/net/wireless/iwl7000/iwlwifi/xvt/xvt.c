0c38bf78abca255f82f2137eab25b2fde1471ebc
Luca Coelho <luciano.coelho@intel.com>
2020-05-11 10:26:03 +0300

    CHROMIUM: iwl7000: xvt: set n_subbands in iwl_xvt_sar_select_profile()
    When the REDUCE_TX_POWER command version 6 was implemented, the
    n_subbands variable was left unset for the older command versions.
    
    Set the values to IWL_NUM_SUB_BANDS in those cases.
    
    Change-Id: I640f194b849f8f1106f151b0823edfd1b5c38e31
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: eb4e63f741c61f664f75c8959ebb2ba4a76a8460

--- drivers/net/wireless/iwl7000/iwlwifi/xvt/xvt.c
+++ drivers/net/wireless/iwl7000/iwlwifi/xvt/xvt.c
@@ -873,13 +873,16 @@ iwl_xvt_sar_select_profile(struct iwl_xvt *xvt, int prof_a, int prof_b)
 	} else if (fw_has_api(&xvt->fw->ucode_capa,
 			      IWL_UCODE_TLV_API_REDUCE_TX_POWER)) {
 		len = sizeof(cmd.v5);
+		n_subbands = IWL_NUM_SUB_BANDS;
 		per_chain = cmd.v5.per_chain[0][0];
 	} else if (fw_has_capa(&xvt->fw->ucode_capa,
 			       IWL_UCODE_TLV_CAPA_TX_POWER_ACK)) {
 		len = sizeof(cmd.v4);
+		n_subbands = IWL_NUM_SUB_BANDS;
 		per_chain = cmd.v4.per_chain[0][0];
 	} else {
 		len = sizeof(cmd.v3);
+		n_subbands = IWL_NUM_SUB_BANDS;
 		per_chain = cmd.v3.per_chain[0][0];
 	}
 
