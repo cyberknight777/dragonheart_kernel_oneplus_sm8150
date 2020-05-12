0a1e287780d97dfa43c557f15549041b61890c02
Ping-Ke Shih <pkshih@realtek.com>
2020-05-12 18:26:17 +0800

    UPSTREAM: rtw88: 8723d: implement flush queue
    Flush queue is used to check if queue is empty, before doing something
    else. Since 8723D uses different registers and page number of
    availabl/reserved occupy 8 bits instead of 16 bits, so use a 'wsize' field
    to discriminate which rtw_read{8,16} is adopted.
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Yan-Hsuan Chuang <yhchuang@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200512102621.5148-6-yhchuang@realtek.com
    (cherry picked from commit 7d754f974ac79640c8efda86d35d766704ebaa8e)
    
    BUG=none
    TEST=none
    
    Signed-off-by: Yan-Hsuan Chuang <yhchuang@realtek.com>
    Change-Id: I2fab17e4cda870d372c35f59ef0e245807f3a818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2355764
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Tested-by: Brian Norris <briannorris@chromium.org>
    Commit-Queue: Brian Norris <briannorris@chromium.org>

--- drivers/net/wireless/realtek/rtw88/main.h
+++ drivers/net/wireless/realtek/rtw88/main.h
@@ -963,6 +963,16 @@ struct rtw_rqpn {
 	enum rtw_dma_mapping dma_map_hi;
 };
 
+struct rtw_prioq_addr {
+	u32 rsvd;
+	u32 avail;
+};
+
+struct rtw_prioq_addrs {
+	struct rtw_prioq_addr prio[RTW_DMA_MAPPING_MAX];
+	bool wsize;
+};
+
 struct rtw_page_table {
 	u16 hq_num;
 	u16 nq_num;
@@ -1119,6 +1129,7 @@ struct rtw_chip_info {
 	const struct rtw_pwr_seq_cmd **pwr_on_seq;
 	const struct rtw_pwr_seq_cmd **pwr_off_seq;
 	const struct rtw_rqpn *rqpn_table;
+	const struct rtw_prioq_addrs *prioq_addrs;
 	const struct rtw_page_table *page_table;
 	const struct rtw_intf_phy_para_table *intf_table;
 
