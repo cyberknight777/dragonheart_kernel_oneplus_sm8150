cee3afeca3a0272d3f56146accdf91659e3ca412
Alain Michaud <alainm@chromium.org>
2020-07-07 20:13:17 +0000

    Revert "CHROMIUM: bluetooth: include file and function names in logs"
    This reverts commit 8a342032df79ccbd1c89e8541eafd4e496fbbb41.
    
    Reason for revert: Aligning to upstream
    
    BUG=b:148227743
    TEST=Built
    
    Signed-off-by: Alain Michaud <alainm@chromium.org>
    Change-Id: I1c0f99b9de35b1c16180e547edc19fcf970809b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2285368
    Reviewed-by: Manish Mandlik <mmandlik@chromium.org>

--- include/net/bluetooth/bluetooth.h
+++ include/net/bluetooth/bluetooth.h
@@ -143,27 +143,10 @@ void bt_warn_ratelimited(const char *fmt, ...);
 __printf(1, 2)
 void bt_err_ratelimited(const char *fmt, ...);
 
-static inline const char *basename(const char *path)
-{
-	const char *str = strrchr(path, '/');
-
-	return str ? (str + 1) : path;
-}
-
-#define BT_INFO(fmt, ...)	bt_info("%s:%s() " fmt "\n",		\
-				basename(__FILE__), __func__, ##__VA_ARGS__)
-#define BT_WARN(fmt, ...)	bt_warn("%s:%s() " fmt "\n",		\
-				basename(__FILE__), __func__, ##__VA_ARGS__)
-#define BT_ERR(fmt, ...)	bt_err("%s:%s() " fmt "\n",		\
-				basename(__FILE__), __func__, ##__VA_ARGS__)
-
-#if IS_ENABLED(CONFIG_BT_FEATURE_DEBUG)
-#define BT_DBG(fmt, ...)	bt_dbg("%s:%s() " fmt "\n",		\
-				basename(__FILE__), __func__, ##__VA_ARGS__)
-#else
-#define BT_DBG(fmt, ...)	pr_debug("%s:%s() " fmt "\n",		\
-				basename(__FILE__), __func__, ##__VA_ARGS__)
-#endif
+#define BT_INFO(fmt, ...)	bt_info(fmt "\n", ##__VA_ARGS__)
+#define BT_WARN(fmt, ...)	bt_warn(fmt "\n", ##__VA_ARGS__)
+#define BT_ERR(fmt, ...)	bt_err(fmt "\n", ##__VA_ARGS__)
+#define BT_DBG(fmt, ...)	pr_debug(fmt "\n", ##__VA_ARGS__)
 
 #define BT_ERR_RATELIMITED(fmt, ...) bt_err_ratelimited(fmt "\n", ##__VA_ARGS__)
 
