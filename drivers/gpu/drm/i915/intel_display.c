fcicq/chromiumos-third_party-kernel

CHROMIUM: drm/i915: Don't modeset to toggle hdcp
This patch moves the hdcp enable/disable to atomic commit instead of
requiring a modeset. The result is a much better user experience since
the monitor doesn't flash everytime playback is initiated/stopped.

Upstream didn't like the added complexity of adding yet another thing in
commit_tail, so we'll keep this local for now. Hopefully once we
upstream the rockchip implementation (which shares some of this code),
we can move i915 along too.

BUG=804309
TEST=Tested on fizz, played protected content observed no flashes

Change-Id: Ia25119652b2cd9bc666a3670d04649e1d5fbc28a
Signed-off-by: Sean Paul <seanpaul@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/874852
Commit-Ready: Sean Paul <seanpaul@google.com>
Tested-by: Sean Paul <seanpaul@google.com>
Reviewed-by: Kristian H. Kristensen <hoegsberg@chromium.org>

drivers/gpu/drm/i915/intel_display.c
@@ -13058,6 +13058,54 @@ static void intel_atomic_helper_free_state_worker(struct work_struct *work)
 	intel_atomic_helper_free_state(dev_priv);
 }
 
+static void
+intel_atomic_disable_hdcp(struct drm_atomic_state *old_state)
+{
+	struct drm_connector *conn;
+	struct drm_connector_state *old_conn_state;
+	int i;
+
+	for_each_connector_in_state(old_state, conn, old_conn_state, i) {
+		struct intel_connector *intel_conn = to_intel_connector(conn);
+		uint64_t new, old;
+
+		if (!intel_conn->hdcp_shim)
+			continue;
+
+		old = old_conn_state->content_protection;
+		new = conn->state->content_protection;
+		if ((new != old &&
+		     new == DRM_MODE_CONTENT_PROTECTION_UNDESIRED) ||
+		    (!conn->state->crtc && old_conn_state->crtc))
+			intel_hdcp_disable(intel_conn);
+	}
+}
+
+static void
+intel_atomic_enable_hdcp(struct drm_atomic_state *old_state)
+{
+	struct drm_connector *conn;
+	struct drm_connector_state *old_conn_state;
+	int i;
+
+	for_each_connector_in_state(old_state, conn, old_conn_state, i) {
+		struct intel_connector *intel_conn = to_intel_connector(conn);
+		uint64_t new, old;
+
+		if (!intel_conn->hdcp_shim)
+			continue;
+
+		old = old_conn_state->content_protection;
+		new = conn->state->content_protection;
+		if (!conn->state->crtc ||
+		    new != DRM_MODE_CONTENT_PROTECTION_DESIRED ||
+		    (new == old && old_conn_state->crtc == conn->state->crtc))
+			continue;
+
+		intel_hdcp_enable(intel_conn);
+	}
+}
+
 static void intel_atomic_commit_tail(struct drm_atomic_state *state)
 {
 	struct drm_device *dev = state->dev;
@@ -13157,6 +13205,8 @@ static void intel_atomic_commit_tail(struct drm_atomic_state *state)
 		}
 	}
 
+	intel_atomic_disable_hdcp(state);
+
 	/* Now enable the clocks, plane, pipe, and connectors that we set up. */
 	dev_priv->display.update_crtcs(state, &crtc_vblank_mask);
 
@@ -13198,6 +13248,8 @@ static void intel_atomic_commit_tail(struct drm_atomic_state *state)
 
 	if (intel_state->modeset && intel_can_enable_sagv(state))
 		intel_enable_sagv(dev_priv);
+
+	intel_atomic_enable_hdcp(state);
 
 	drm_atomic_helper_commit_hw_done(state);
 

cyberknight777/dragonheart_kernel_oneplus_sm8150

BUG=b:161143639
TEST=Tested on dorp, no flicker when enabling/disabling HDCP
This is a forward port of https://chromium-review.googlesource.com/874852
which didn't get brought along from 4.4. I've posted a different
solution upstream which accomplishes the same thing using the update_pipe
callback, but the supporting code isn't present in 4.14.
Some minor tweaks since 4.4, the most notable using
for_each_oldnew_connector_in_state instead of
for_each_connector_in_state to avoid peeking directly into the
connector's current state.
Change-Id: I5b09a2a96d5eb7c2e3cf91bee5572c28bf57b4f2
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2300341
Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
Tested-by: Sean Paul <seanpaul@chromium.org>
Commit-Queue: Sean Paul <seanpaul@chromium.org>

drivers/gpu/drm/i915/intel_display.c
@@ -12446,6 +12446,58 @@ static void intel_atomic_commit_fence_wait(struct intel_atomic_state *intel_stat
 	finish_wait(&dev_priv->gpu_error.wait_queue, &wait_reset);
 }
 
+static void
+intel_atomic_disable_hdcp(struct drm_atomic_state *state)
+{
+	struct drm_connector *conn;
+	struct drm_connector_state *old_conn_state, *new_conn_state;
+	int i;
+
+	for_each_oldnew_connector_in_state(state, conn, old_conn_state,
+					   new_conn_state, i) {
+		struct intel_connector *intel_conn = to_intel_connector(conn);
+		uint64_t new, old;
+
+		if (!intel_conn->hdcp_shim)
+			continue;
+
+		old = old_conn_state->content_protection;
+		new = new_conn_state->content_protection;
+		if ((new != old &&
+		     new == DRM_MODE_CONTENT_PROTECTION_UNDESIRED) ||
+		    (!new_conn_state->crtc && old_conn_state->crtc))
+			intel_hdcp_disable(intel_conn);
+	}
+}
+
+static void
+intel_atomic_enable_hdcp(struct drm_atomic_state *state)
+{
+	struct drm_connector *conn;
+	struct drm_connector_state *old_conn_state, *new_conn_state;
+	int i;
+
+	for_each_oldnew_connector_in_state(state, conn, old_conn_state,
+					   new_conn_state, i) {
+		struct intel_connector *intel_conn = to_intel_connector(conn);
+		uint64_t new, old;
+
+		if (!intel_conn->hdcp_shim)
+			continue;
+
+		old = old_conn_state->content_protection;
+		new = new_conn_state->content_protection;
+		if (!new_conn_state->crtc ||
+		    new != DRM_MODE_CONTENT_PROTECTION_DESIRED ||
+		    (old == DRM_MODE_CONTENT_PROTECTION_ENABLED &&
+		     new == DRM_MODE_CONTENT_PROTECTION_DESIRED) ||
+		    (new == old && old_conn_state->crtc == new_conn_state->crtc))
+			continue;
+
+		intel_hdcp_enable(intel_conn);
+	}
+}
+
 static void intel_atomic_commit_tail(struct drm_atomic_state *state)
 {
 	struct drm_device *dev = state->dev;
@@ -12542,6 +12594,8 @@ static void intel_atomic_commit_tail(struct drm_atomic_state *state)
 		}
 	}
 
+	intel_atomic_disable_hdcp(state);
+
 	/* Now enable the clocks, plane, pipe, and connectors that we set up. */
 	dev_priv->display.update_crtcs(state);
 
@@ -12582,6 +12636,8 @@ static void intel_atomic_commit_tail(struct drm_atomic_state *state)
 
 	if (intel_state->modeset && intel_can_enable_sagv(state))
 		intel_enable_sagv(dev_priv);
+
+	intel_atomic_enable_hdcp(state);
 
 	drm_atomic_helper_commit_hw_done(state);
 