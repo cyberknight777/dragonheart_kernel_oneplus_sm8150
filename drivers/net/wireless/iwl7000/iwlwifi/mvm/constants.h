db349343a78781a27d2c3509d40740167b1820cd
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2020-07-28 09:58:28 +0300

    CHROMIUM: iwl7000: remove sw_csum_tx
    This was a hack done to test the data path of devices that
    didn't support well CSUM offload in Tx. This is not needed
    anymore.
    
    Change-Id: I2aaebf90e6fe81860105d049a8d35746fa8d86c2
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    iwl7000-tree: 7727f85fa2345b63aca97a69f11416a2248a3dc2

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/constants.h
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/constants.h
@@ -106,7 +106,6 @@
 #define IWL_MVM_RS_RSSI_BASED_INIT_RATE         0
 #define IWL_MVM_RS_80_20_FAR_RANGE_TWEAK	1
 #define IWL_MVM_TOF_IS_RESPONDER		0
-#define IWL_MVM_SW_TX_CSUM_OFFLOAD		0
 #define IWL_MVM_HW_CSUM_DISABLE			0
 #define IWL_MVM_PARSE_NVM			0
 #define IWL_MVM_ADWELL_ENABLE			1
@@ -198,7 +197,6 @@
 #define IWL_MVM_FW_MCAST_FILTER_PASS_ALL	(mvm->trans->dbg_cfg.MVM_FW_MCAST_FILTER_PASS_ALL)
 #define IWL_MVM_FW_BCAST_FILTER_PASS_ALL	(mvm->trans->dbg_cfg.MVM_FW_BCAST_FILTER_PASS_ALL)
 #define IWL_MVM_TOF_IS_RESPONDER		(mvm->trans->dbg_cfg.MVM_TOF_IS_RESPONDER)
-#define IWL_MVM_SW_TX_CSUM_OFFLOAD		(mvm->trans->dbg_cfg.MVM_SW_TX_CSUM_OFFLOAD)
 #define IWL_MVM_HW_CSUM_DISABLE			(mvm->trans->dbg_cfg.MVM_HW_CSUM_DISABLE)
 #define IWL_MVM_PARSE_NVM			(mvm->trans->dbg_cfg.MVM_PARSE_NVM)
 #define IWL_MVM_ADWELL_ENABLE			(mvm->trans->dbg_cfg.MVM_ADWELL_ENABLE)
