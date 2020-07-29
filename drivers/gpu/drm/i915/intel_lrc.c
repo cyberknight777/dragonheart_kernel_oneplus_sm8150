b05cf206e3a8f77cfa8b6cdc3e8af05595216252
Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
2020-07-29 02:06:31 +0000

    UPSTREAM: drm/i915: Avoid context dereference inside execlists_submission_tasklet
    A lesson that has to be relearnt over and over again is that the request
    does not keep a reference to the context and so we cannot freely
    dereference the context from inside the execlists_submission_tasklet. In
    particular, we try to do so in the new GEM_TRACE() so convert those over
    to the port->context_id we keep for GEM debugging. This means the
    tracing now depends on DRM_I915_GEM_DEBUG.
    
    Fixes: bccd3b831185 ("drm/i915: Use trace_printk to provide a death rattle for GEM")
    References: https://bugs.freedesktop.org/show_bug.cgi?id=104066
    References: https://bugs.freedesktop.org/show_bug.cgi?id=104162
    References: https://bugs.freedesktop.org/show_bug.cgi?id=104242
    References: https://bugs.freedesktop.org/show_bug.cgi?id=104310
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Michel Thierry <michel.thierry@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171219220916.30882-1-chris@chris-wilson.co.uk
    (cherry picked from commit 16c8619a7c53fe05526c31d4758be0eeabd16364)
    
    BUG=chromium:783394
    TEST=CNL boots to graphics
    
    Signed-off-by: Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
    Change-Id: I26e7f71adf97fc6b30cd9626ec6f099d6f09d794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2325376
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Guenter Roeck <groeck@chromium.org>
    Tested-by: Guenter Roeck <groeck@chromium.org>

--- drivers/gpu/drm/i915/intel_lrc.c
+++ drivers/gpu/drm/i915/intel_lrc.c
@@ -481,7 +481,7 @@ static void execlists_submit_ports(struct intel_engine_cs *engine)
 
 			GEM_TRACE("%s in[%d]:  ctx=%d.%d, seqno=%x\n",
 				  engine->name, n,
-				  rq->ctx->hw_id, count,
+				  port[n].context_id, count,
 				  rq->global_seqno);
 		} else {
 			GEM_BUG_ON(!n);
@@ -898,7 +898,7 @@ static void intel_lrc_irq_handler(unsigned long data)
 			 */
 
 			status = READ_ONCE(buf[2 * head]); /* maybe mmio! */
-			GEM_TRACE("%s csb[%dd]: status=0x%08x:0x%08x\n",
+			GEM_TRACE("%s csb[%d]: status=0x%08x:0x%08x\n",
 				  engine->name, head,
 				  status, buf[2*head + 1]);
 
@@ -939,7 +939,7 @@ static void intel_lrc_irq_handler(unsigned long data)
 			rq = port_unpack(port, &count);
 			GEM_TRACE("%s out[0]: ctx=%d.%d, seqno=%x\n",
 				  engine->name,
-				  rq->ctx->hw_id, count,
+				  port->context_id, count,
 				  rq->global_seqno);
 			GEM_BUG_ON(count == 0);
 			if (--count == 0) {
