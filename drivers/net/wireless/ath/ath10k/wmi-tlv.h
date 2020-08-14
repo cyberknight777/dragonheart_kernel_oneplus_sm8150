ad79cd0ea98dad630aa311a2dbf7d095aa9c0ac8
Wen Gong <wgong@codeaurora.org>
2020-08-14 18:55:50 +0300

    FROMGIT: ath10k: add wmi service peer stat info for wmi tlv
    ath10k_sta_statistics is used to report info for iw wlan0 link,
    it check ath10k_peer_stats_enabled, and ath10k_peer_stats_enabled
    check WMI_SERVICE_PEER_STATS bit of ar->wmi.svc_map. SVCMAP() for
    WMI_SERVICE_PEER_STATS was defined only for wmi_10x_svc_map and
    wmi_10_4_svc_map interfaces, it missed in wmi_tlv_svc_map, so it is
    not usable for iw wlan0 link for wmi tlv interface.
    
    If firmware report WMI_TLV_SERVICE_PEER_STATS_INFO for wmi tlv, then
    enable the WMI_SERVICE_PEER_STATS bit in ath10k, and then it pass check
    in ath10k_peer_stats_enabled and ath10k_sta_statistics pass check.
    
    Tested-on: QCA6174 hw3.2 SDIO WLAN.RMH.4.4.1-00048
    Tested-on: QCA6174 hw3.2 PCI WLAN.RM.4.4.1-00110-QCARMSWP-1
    
    Signed-off-by: Wen Gong <wgong@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/1597312029-32348-2-git-send-email-wgong@codeaurora.org
    (cherry picked from commit e39f32afc6d2157efbd601911d872eb46f93e866
    git://git.kernel.org/pub/scm/linux/kernel/git/kvalo/ath.git ath-next)
    
    BUG=b:118512917
    TEST=test command success: iw wlan0 station dump
    
    Change-Id: Iee7f95f1804f8f4c528b1b0d45e1356b323f3352
    Signed-off-by: Wen Gong <wgong@codeaurora.org>
    Signed-off-by: Yen-lin Lai <yenlinlai@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2374578
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/net/wireless/ath/ath10k/wmi-tlv.h
+++ drivers/net/wireless/ath/ath10k/wmi-tlv.h
@@ -1569,6 +1569,8 @@ wmi_tlv_svc_map(const __le32 *in, unsigned long *out, size_t len)
 	       WMI_SERVICE_SAP_AUTH_OFFLOAD, len);
 	SVCMAP(WMI_TLV_SERVICE_MGMT_TX_WMI,
 	       WMI_SERVICE_MGMT_TX_WMI, len);
+	SVCMAP(WMI_TLV_SERVICE_PEER_STATS_INFO,
+	       WMI_SERVICE_PEER_STATS, len);
 }
 
 static inline void
