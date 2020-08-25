366933663a224ae629de880c5a71aaae97442957
Johannes Berg <johannes.berg@intel.com>
2020-08-25 11:59:43 +0200

    CHROMIUM: iwl7000: mvm: handle CCA-EXT delay firmware notification
    If there are frequent CCA delays due to the extension channel
    as detected by the firmware, and we're on 2.4 GHz, then handle
    this by disconnecting (with a reconnect hint).
    
    When we disconnect, we'll also update our capabilities to use
    only 20 MHz on the next connection (if it's on 2.4 GHz) as to
    avoid the use of the extension channel that has too much noise.
    
    Change-Id: I709b7e6f73a7537c53f22d7418927691259de8a8
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: f668de980027d08034898090fb777f4dd323e997

--- drivers/net/wireless/iwl7000/iwlwifi/fw/api/datapath.h
+++ drivers/net/wireless/iwl7000/iwlwifi/fw/api/datapath.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
 /*
- * Copyright (C) 2012-2014, 2018-2019 Intel Corporation
+ * Copyright (C) 2012-2014, 2018-2020 Intel Corporation
  * Copyright (C) 2013-2015 Intel Mobile Communications GmbH
  * Copyright (C) 2016-2017 Intel Deutschland GmbH
  */
@@ -57,6 +57,12 @@ enum iwl_data_path_subcmd_ids {
 	 *	or &struct iwl_channel_estimation_cfg
 	 */
 	CHEST_COLLECTOR_FILTER_CONFIG_CMD = 0x14,
+
+	/**
+	 * @MONITOR_NOTIF: Datapath monitoring notification, using
+	 *	&struct iwl_datapath_monitor_notif
+	 */
+	MONITOR_NOTIF = 0xF4,
 
 	/**
 	 * @RX_NO_DATA_NOTIF: &struct iwl_rx_no_data
@@ -218,5 +224,15 @@ struct iwl_channel_estimation_cfg {
 		__le16 reserved;
 	} filter_addrs[IWL_NUM_CHANNEL_ESTIMATION_FILTER_ADDRS];
 } __packed; /* CHEST_COLLECTOR_FILTER_CMD_API_S_VER_2 */
+
+enum iwl_datapath_monitor_notif_type {
+	IWL_DP_MON_NOTIF_TYPE_EXT_CCA,
+};
+
+struct iwl_datapath_monitor_notif {
+	__le32 type;
+	u8 mac_id;
+	u8 reserved[3];
+} __packed; /* MONITOR_NTF_API_S_VER_1 */
 
 #endif /* __iwl_fw_api_datapath_h__ */
