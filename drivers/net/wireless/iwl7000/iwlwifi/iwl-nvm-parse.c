65c9d427962d074b813f614deb57f3cfc7070a6d
Avraham Stern <avraham.stern@intel.com>
2020-09-03 02:46:46 -0400

    CHROMIUM: iwl7000: mvm: don't set default HE phy capabilities if set by file
    IEEE80211_HE_PHY_CAP9_TX_1024_QAM_LESS_THAN_242_TONE_RU and
    IEEE80211_HE_PHY_CAP9_RX_1024_QAM_LESS_THAN_242_TONE_RU are set even
    if they were not set in the iwl-dbg-cfg.ini config file. This prevents
    the user from configuring the HE phy capabilities.
    
    If the HE phy capabilities are set from a file, don't override them.
    
    Change-Id: Ic1d7f81e2bff686ce4b96201dbcbe4dbf63efc13
    Signed-off-by: Avraham Stern <avraham.stern@intel.com>
    iwl7000-tree: 21e31d1d0ffd70b7fe450ecf63cb83f8dc4d4b43

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-nvm-parse.c
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-nvm-parse.c
@@ -698,21 +698,14 @@ static void iwl_init_he_hw_capab(struct iwl_trans *trans,
 				 struct ieee80211_supported_band *sband,
 				 u8 tx_chains, u8 rx_chains)
 {
-	int i;
-
 	ieee80211_sband_set_iftypes_data(sband, iwl_he_capa);
 	ieee80211_sband_set_num_iftypes_data(sband, ARRAY_SIZE(iwl_he_capa));
 
-	for (i = 0; i < ieee80211_sband_get_num_iftypes_data(sband); i++) {
-		if (iwl_he_capa[i].types_mask == BIT(NL80211_IFTYPE_STATION) &&
-		    trans->trans_cfg->device_family >= IWL_DEVICE_FAMILY_AX210) {
-			iwl_he_capa[i].he_cap.he_cap_elem.phy_cap_info[9] |=
-				IEEE80211_HE_PHY_CAP9_TX_1024_QAM_LESS_THAN_242_TONE_RU |
-				IEEE80211_HE_PHY_CAP9_RX_1024_QAM_LESS_THAN_242_TONE_RU;
-		}
+	/* If not 2x2, we need to indicate 1x1 in the Midamble RX Max NSTS */
+	if ((tx_chains & rx_chains) != ANT_AB) {
+		int i;
 
-		/* If not 2x2, we need to indicate 1x1 in the Midamble RX Max NSTS */
-		if ((tx_chains & rx_chains) != ANT_AB) {
+		for (i = 0; i < ieee80211_sband_get_num_iftypes_data(sband); i++) {
 			iwl_he_capa[i].he_cap.he_cap_elem.phy_cap_info[1] &=
 				~IEEE80211_HE_PHY_CAP1_MIDAMBLE_RX_TX_MAX_NSTS;
 			iwl_he_capa[i].he_cap.he_cap_elem.phy_cap_info[2] &=
@@ -862,6 +855,9 @@ static void iwl_init_he_override(struct iwl_trans *trans,
 			iftype_data->he_cap.he_cap_elem.phy_cap_info[2] |=
 				IEEE80211_HE_PHY_CAP2_UL_MU_FULL_MU_MIMO |
 				IEEE80211_HE_PHY_CAP2_UL_MU_PARTIAL_MU_MIMO;
+			iftype_data->he_cap.he_cap_elem.phy_cap_info[9] |=
+				IEEE80211_HE_PHY_CAP9_TX_1024_QAM_LESS_THAN_242_TONE_RU |
+				IEEE80211_HE_PHY_CAP9_RX_1024_QAM_LESS_THAN_242_TONE_RU;
 		}
 
 		if (trans->dbg_cfg.smps_disabled)
