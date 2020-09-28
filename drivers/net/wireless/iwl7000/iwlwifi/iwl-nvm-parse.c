c25c7d6b23d661504448800da2845fa4a0f42963
Krishnanand Prabhu <krishnanand.prabhu@intel.com>
2020-09-28 21:07:48 +0530

    CHROMIUM: iwl7000: Update 6GHz support
    Support 6 GHz feature only from kernel 5.10 and above in chromeOS.
    
    Skip the 6 GHz channels read from NVM if UHB is not supported.
    
    Change-Id: I6885ba6c6ee2cdf71628e88c1375b2c22319d984
    Signed-off-by: Krishnanand Prabhu <krishnanand.prabhu@intel.com>
    iwl7000-tree: 22da78a1671c03f1f379f5a703064f2f99406e25

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-nvm-parse.c
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-nvm-parse.c
@@ -330,10 +330,16 @@ static u32 iwl_get_channel_flags(u8 ch_num, int ch_idx, enum nl80211_band band,
 	return flags;
 }
 
-static enum nl80211_band iwl_nl80211_band_from_channel_idx(int ch_idx)
+static int iwl_nl80211_band_from_channel_idx(int ch_idx)
 {
-	if (ch_idx >= NUM_2GHZ_CHANNELS + NUM_5GHZ_CHANNELS)
+	if (ch_idx >= NUM_2GHZ_CHANNELS + NUM_5GHZ_CHANNELS) {
+#ifndef CPTCFG_IWLWIFI_WIFI_6_SUPPORT
+		/* Skip 6 GHz band/channels if UHB is unsupported */
+		return -1;
+#else
 		return NL80211_BAND_6GHZ;
+#endif /* CPTCFG_IWLWIFI_WIFI_6_SUPPORT */
+	}
 
 	if (ch_idx >= NUM_2GHZ_CHANNELS)
 		return NL80211_BAND_5GHZ;
@@ -351,6 +357,7 @@ static int iwl_init_channel_map(struct device *dev, const struct iwl_cfg *cfg,
 	u32 ch_flags;
 	int num_of_ch;
 	const u16 *nvm_chan;
+	int band;
 
 	if (cfg->uhb_supported) {
 		num_of_ch = IWL_NVM_NUM_CHANNELS_UHB;
@@ -364,8 +371,9 @@ static int iwl_init_channel_map(struct device *dev, const struct iwl_cfg *cfg,
 	}
 
 	for (; ch_idx < num_of_ch; ch_idx++) {
-		enum nl80211_band band =
-			iwl_nl80211_band_from_channel_idx(ch_idx);
+		band = iwl_nl80211_band_from_channel_idx(ch_idx);
+		if (band == -1)
+			continue;
 
 		if (v4)
 			ch_flags =
@@ -1417,7 +1425,7 @@ iwl_parse_nvm_mcc_info(struct device *dev, const struct iwl_cfg *cfg,
 	const u16 *nvm_chan;
 	struct ieee80211_regdomain *regd, *copy_rd;
 	struct ieee80211_reg_rule *rule;
-	enum nl80211_band band;
+	int band;
 	int center_freq, prev_center_freq = 0;
 	int valid_rules = 0;
 	bool new_rule;
@@ -1457,8 +1465,11 @@ iwl_parse_nvm_mcc_info(struct device *dev, const struct iwl_cfg *cfg,
 	reg_capa = iwl_get_reg_capa(cap, resp_ver);
 
 	for (ch_idx = 0; ch_idx < num_of_ch; ch_idx++) {
-		ch_flags = (u16)__le32_to_cpup(channels + ch_idx);
 		band = iwl_nl80211_band_from_channel_idx(ch_idx);
+		if (band == -1)
+			continue;
+
+		ch_flags = (u16)__le32_to_cpup(channels + ch_idx);
 		center_freq = ieee80211_channel_to_frequency(nvm_chan[ch_idx],
 							     band);
 		new_rule = false;
