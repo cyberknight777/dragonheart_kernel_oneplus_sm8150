b7e857ff7afa61dbe84c2d4fdc96d8535a37ac0b
Luca Coelho <luciano.coelho@intel.com>
2020-10-26 15:32:40 +0200

    CHROMIUM: iwl7000: mvm: add Microsoft to the PPAG approved list
    Microsoft is now approved to use the PPAG feature.  Add it to the
    approved list.
    
    Change-Id: I661f14d84f864d3860db6fcb05b7f37ec804b6ef
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: cd23b0e3a6b806380083abc1dab687a48bb52a19

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/fw.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/fw.c
@@ -1163,6 +1163,11 @@ static const struct dmi_system_id dmi_ppag_approved_list[] = {
 			DMI_MATCH(DMI_SYS_VENDOR, "SAMSUNG ELECTRONICS CO., LTD"),
 		},
 	},
+	{ .ident = "MSFT",
+	  .matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Microsoft Corporation"),
+		},
+	},
 };
 
 static int iwl_mvm_ppag_init(struct iwl_mvm *mvm)
