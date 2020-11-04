a2327c71f387a6b5b86472c90ee6b2d8fcc51772
Haim Dreyfuss <haim.dreyfuss@intel.com>
2020-11-04 08:17:35 -0500

    CHROMIUM: iwl7000: acpi: don't return valid pointer as an ERR_PTR
    iwl_acpi_get_wifi_pkg() may return a valid pointer (meaning success),
    while `tbl_rev` is invalid (equel to 1).
    In this case, we will treat that as an error.
    Subsequent "users" of this "error code" may either check for nonzero
    (good; pointers are never zero) or negative
    (bad; pointers may be "positive") fix that by splitting the if statement.
    First check if IS_ERR(wifi_pkg) and then if tbl_rev != 0.
    
    Change-Id: I147373f6fd364606b0282af8d402c722eb917225
    Signed-off-by: Haim Dreyfuss <haim.dreyfuss@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 83d8f0171056885cedf7ca2b263150a2d0431c44

--- drivers/net/wireless/iwl7000/iwlwifi/fw/acpi.c
+++ drivers/net/wireless/iwl7000/iwlwifi/fw/acpi.c
@@ -447,8 +447,13 @@ int iwl_sar_get_wrds_table(struct iwl_fw_runtime *fwrt)
 
 	wifi_pkg = iwl_acpi_get_wifi_pkg(fwrt->dev, data,
 					 ACPI_WRDS_WIFI_DATA_SIZE, &tbl_rev);
-	if (IS_ERR(wifi_pkg) || tbl_rev != 0) {
+	if (IS_ERR(wifi_pkg)) {
 		ret = PTR_ERR(wifi_pkg);
+		goto out_free;
+	}
+
+	if (tbl_rev != 0) {
+		ret = -EINVAL;
 		goto out_free;
 	}
 
@@ -485,8 +490,13 @@ int iwl_sar_get_ewrd_table(struct iwl_fw_runtime *fwrt)
 
 	wifi_pkg = iwl_acpi_get_wifi_pkg(fwrt->dev, data,
 					 ACPI_EWRD_WIFI_DATA_SIZE, &tbl_rev);
-	if (IS_ERR(wifi_pkg) || tbl_rev != 0) {
+	if (IS_ERR(wifi_pkg)) {
 		ret = PTR_ERR(wifi_pkg);
+		goto out_free;
+	}
+
+	if (tbl_rev != 0) {
+		ret = -EINVAL;
 		goto out_free;
 	}
 
@@ -545,8 +555,14 @@ int iwl_sar_get_wgds_table(struct iwl_fw_runtime *fwrt)
 
 	wifi_pkg = iwl_acpi_get_wifi_pkg(fwrt->dev, data,
 					 ACPI_WGDS_WIFI_DATA_SIZE, &tbl_rev);
-	if (IS_ERR(wifi_pkg) || tbl_rev > 1) {
+
+	if (IS_ERR(wifi_pkg)) {
 		ret = PTR_ERR(wifi_pkg);
+		goto out_free;
+	}
+
+	if (tbl_rev > 1) {
+		ret = -EINVAL;
 		goto out_free;
 	}
 
