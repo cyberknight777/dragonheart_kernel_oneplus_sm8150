f1554aa62b2eaa3c57423dbdb8b8d593aa9cef66
Curtis Malainey <cujomalainey@chromium.org>
2020-11-11 12:53:47 -0800

    Revert "UPSTREAM: ASoC: Intel: bdw-rt5650: Mark FE DAIs as nonatomic"
    This reverts commit ea782f3acc5158718c0e8f24e538b7e490d826d7.
    
    BUG=b:159920187
    TEST=test on various broadwell platforms
    
    Change-Id: Ie358e2036760210663fb11c851253293399b8d8f
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2533387
    Reviewed-by: Ben Zhang <benzh@chromium.org>

--- sound/soc/intel/boards/bdw-rt5650.c
+++ sound/soc/intel/boards/bdw-rt5650.c
@@ -218,7 +218,6 @@ static struct snd_soc_dai_link bdw_rt5650_dais[] = {
 		.stream_name = "System Playback",
 		.cpu_dai_name = "System Pin",
 		.platform_name = "catpt-platform",
-		.nonatomic = 1,
 		.dynamic = 1,
 		.ops = &bdw_rt5650_fe_ops,
 		.trigger = {
