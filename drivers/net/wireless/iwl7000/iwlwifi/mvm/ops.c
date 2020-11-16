39bfff6529a442153c9572c198db005c6a30d147
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2020-11-16 17:44:55 +0200

    CHROMIUM: iwl7000: mvm: isolate the get nvm flow
    This will soon be a bit more complicated. Take it out
    to a function.
    
    Change-Id: I9db96647eb9c7142051e26cd5be3f52c5ffc5534
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    iwl7000-tree: f4aa34bdc524445518efdcd2e3fd526f42cf7300

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/ops.c
@@ -744,6 +744,28 @@ static int iwl_mvm_tm_send_hcmd(void *op_mode, struct iwl_host_cmd *host_cmd)
 }
 #endif
 
+static int iwl_mvm_start_get_nvm(struct iwl_mvm *mvm)
+{
+	int ret;
+
+	mutex_lock(&mvm->mutex);
+
+	ret = iwl_run_init_mvm_ucode(mvm);
+
+	if (ret && ret != -ERFKILL)
+		iwl_fw_dbg_error_collect(&mvm->fwrt, FW_DBG_TRIGGER_DRIVER);
+
+	if (!iwlmvm_mod_params.init_dbg || !ret)
+		iwl_mvm_stop_device(mvm);
+
+	mutex_unlock(&mvm->mutex);
+
+	if (ret < 0)
+		IWL_ERR(mvm, "Failed to run INIT ucode: %d\n", ret);
+
+	return ret;
+}
+
 static struct iwl_op_mode *
 iwl_op_mode_mvm_start(struct iwl_trans *trans, const struct iwl_cfg *cfg,
 		      const struct iwl_fw *fw, struct dentry *dbgfs_dir)
@@ -1005,19 +1027,10 @@ iwl_op_mode_mvm_start(struct iwl_trans *trans, const struct iwl_cfg *cfg,
 	if (err)
 		goto out_free;
 
-	mutex_lock(&mvm->mutex);
-	err = iwl_run_init_mvm_ucode(mvm);
-	if (err && err != -ERFKILL)
-		iwl_fw_dbg_error_collect(&mvm->fwrt, FW_DBG_TRIGGER_DRIVER);
-	if (!iwlmvm_mod_params.init_dbg || !err)
-		iwl_mvm_stop_device(mvm);
-	mutex_unlock(&mvm->mutex);
-	if (err < 0) {
-		IWL_ERR(mvm, "Failed to run INIT ucode: %d\n", err);
-		goto out_free;
-	}
-
 	scan_size = iwl_mvm_scan_size(mvm);
+
+	if (iwl_mvm_start_get_nvm(mvm))
+		goto out_free;
 
 	mvm->scan_cmd = kmalloc(scan_size, GFP_KERNEL);
 	if (!mvm->scan_cmd)
