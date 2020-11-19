cba9bd2f83ebc106e3e1b6dc78c8215da6cde7c0
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2020-11-18 20:57:29 -0800

    Revert "CHROMIUM: bluetooth: fix log spewing from is_filter_match()"
    This reverts commit bd454761f0c94e90216b95f8ea375cf06f1113f7.
    
    Reason for revert: Getting rid of unnecessary hacks.
    
    BUG=b:173799663
    TEST=Ran bluetooth_AdapterQuickHealth on Grunt
    
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Change-Id: I9ea30f144a8d1295b4bdb4bf7b42c68a1b59873b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2552751
    Reviewed-by: Alain Michaud <alainm@chromium.org>
    Reviewed-by: Manish Mandlik <mmandlik@chromium.org>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -8187,7 +8187,7 @@ static bool is_filter_match(struct hci_dev *hdev, s8 rssi, u8 *eir,
 	 * scanning to ensure updated result with updated RSSI values.
 	 */
 	if (test_bit(HCI_QUIRK_STRICT_DUPLICATE_FILTER, &hdev->quirks)) {
-		BT_DBG("BT_DBG_DG: queue le_scan_restart: mgmt:restart_le_scan:is_filter_match\n");
+		printk("BT_DBG_DG: queue le_scan_restart: mgmt:restart_le_scan:is_filter_match\n");
 		restart_le_scan(hdev);
 
 		/* Validate RSSI value against the RSSI threshold once more. */
