de161de2e5d0902268306bb282fd7a42dbd1d471
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2020-12-07 11:03:05 -0800

    CHROMIUM: Bluetooth: Remove hci_req_le_suspend_config
    Since suspend task bits are set directly when configuring event filter
    and passive scanning, there is no need for a separate function to do so.
    As a result, both the suspend disable and suspend enable bit can be set
    at the same time so the suspend_req_complete function should clear both.
    
    DO NOT UPSTREAM THIS PATCH as-is. The upstream code currently doesn't
    match our repo and this patch is missing pieces necessary for
    upstreaming.
    
    BUG=b:174184979, b:173799663
    TEST=Built and tested with bluetooth_AdapterSRHealth
    
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Change-Id: Icb1766bc30b91c7cf1d4d5043340f9a604eaf652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2576126
    Reviewed-by: Daniel Winkler <danielwinkler@google.com>

--- net/bluetooth/hci_request.c
+++ net/bluetooth/hci_request.c
@@ -1035,17 +1035,6 @@ static void hci_req_set_event_filter(struct hci_request *req)
 	hci_req_add(req, HCI_OP_WRITE_SCAN_ENABLE, 1, &scan);
 }
 
-static void hci_req_config_le_suspend_scan(struct hci_request *req)
-{
-	/* Before changing params disable scan if enabled */
-	if (hci_dev_test_flag(req->hdev, HCI_LE_SCAN))
-		hci_req_add_le_scan_disable(req);
-
-	/* Configure params and enable scanning */
-	__hci_update_background_scan(req);
-
-}
-
 static void cancel_adv_timeout(struct hci_dev *hdev)
 {
 	if (hdev->adv_instance_timeout) {
@@ -1096,8 +1085,10 @@ static void suspend_req_complete(struct hci_dev *hdev, u8 status, u16 opcode)
 {
 	bt_dev_dbg(hdev, "Request complete opcode=0x%x, status=0x%x", opcode,
 		   status);
-	if (test_and_clear_bit(SUSPEND_SCAN_ENABLE, hdev->suspend_tasks) ||
-	    test_and_clear_bit(SUSPEND_SCAN_DISABLE, hdev->suspend_tasks)) {
+	if (test_bit(SUSPEND_SCAN_ENABLE, hdev->suspend_tasks) ||
+	    test_bit(SUSPEND_SCAN_DISABLE, hdev->suspend_tasks)) {
+		clear_bit(SUSPEND_SCAN_ENABLE, hdev->suspend_tasks);
+		clear_bit(SUSPEND_SCAN_DISABLE, hdev->suspend_tasks);
 		wake_up(&hdev->suspend_wait_q);
 	}
 }
@@ -1188,7 +1179,7 @@ void hci_req_prepare_suspend(struct hci_dev *hdev, enum suspended_state next)
 		/* Enable event filter for paired devices */
 		hci_req_set_event_filter(&req);
 		/* Enable passive scan at lower duty cycle */
-		hci_req_config_le_suspend_scan(&req);
+		__hci_update_background_scan(&req);
 		/* Pause scan changes again. */
 		hdev->scanning_paused = true;
 		hci_req_run(&req, suspend_req_complete);
@@ -1198,7 +1189,7 @@ void hci_req_prepare_suspend(struct hci_dev *hdev, enum suspended_state next)
 
 		hci_req_clear_event_filter(&req);
 		/* Reset passive/background scanning to normal */
-		hci_req_config_le_suspend_scan(&req);
+		__hci_update_background_scan(&req);
 
 		/* Unpause directed advertising */
 		hdev->advertising_paused = false;
