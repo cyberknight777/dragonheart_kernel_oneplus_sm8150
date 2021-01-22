929a1c9a01c4bbad746fda0e2c13b434cf7352ee
Archie Pusaka <apusaka@chromium.org>
2021-01-22 19:49:05 +0800

    CHROMIUM: Bluetooth: updating controller reset handler
    Since there is a logic change in msft_do_open and msft_do_close,
    the handler to remove monitor mapping on controller reset needs
    to be modifed.
    
    BUG=b:153383808
    TEST=Do the following test.
    (1) Upon hci0 power on, all monitors would be registered to the
        controller. This could be checked by powering off the controller,
        then add some monitor, then power on the controller. The userspace
        should not be notified when this happens.
    (2) Upon hci0 power off, the currently registered monitor list is not
        discarded. This could be checked by powering the controller back
        on and see that the monitors would all be re-registered. The
        userspace should not be notified when this happens.
    (3) A monitor which is successfully added when the power is off might
        have problems when it is actually presented to the controller when
        the power is back on. These monitors should be removed on power on
        and the userspace should be notified for the deletion. This could
        be tested by adding monitors with invalid rssi when powered off,
        then power the controller on.
    
    Signed-off-by: Archie Pusaka <apusaka@chromium.org>
    
    Change-Id: Ifd445ff4a11d2f5bd6632bbb0964ff63a689de91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2658826
    Tested-by: Archie Pusaka <apusaka@chromium.org>
    Reviewed-by: Yun-Hao Chung <howardchung@google.com>
    Commit-Queue: Archie Pusaka <apusaka@chromium.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -1464,6 +1464,7 @@ static int hci_dev_do_open(struct hci_dev *hdev)
 		ret = hdev->set_diag(hdev, true);
 
 	msft_do_open(hdev);
+	msft_power_on(hdev);
 
 	clear_bit(HCI_INIT, &hdev->flags);
 
@@ -1661,6 +1662,8 @@ int hci_dev_do_close(struct hci_dev *hdev)
 	smp_unregister(hdev);
 
 	hci_sock_dev_event(hdev, HCI_DEV_DOWN);
+
+	msft_power_off(hdev);
 
 	if (hdev->flush)
 		hdev->flush(hdev);
