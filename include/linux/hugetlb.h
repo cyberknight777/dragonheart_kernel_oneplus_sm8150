37253f709bd661b96bf2348d207fdda096d64c4f
Peter Xu <peterx@redhat.com>
2021-03-18 17:01:47 +1100

    BACKPORT: FROMGIT: hugetlb/userfaultfd: unshare all pmds for hugetlbfs when register wp
    Huge pmd sharing for hugetlbfs is racy with userfaultfd-wp because
    userfaultfd-wp is always based on pgtable entries, so they cannot be
    shared.
    
    Walk the hugetlb range and unshare all such mappings if there is, right
    before UFFDIO_REGISTER will succeed and return to userspace.
    
    This will pair with want_pmd_share() in hugetlb code so that huge pmd
    sharing is completely disabled for userfaultfd-wp registered range.
    
    Link: https://lkml.kernel.org/r/20210218231206.15524-1-peterx@redhat.com
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Cc: Kirill A. Shutemov <kirill@shutemov.name>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Adam Ruprecht <ruprecht@google.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Cannon Matthews <cannonmatthews@google.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Chinwen Chang <chinwen.chang@mediatek.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: "Dr . David Alan Gilbert" <dgilbert@redhat.com>
    Cc: Huang Ying <ying.huang@intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Jerome Glisse <jglisse@redhat.com>
    Cc: Lokesh Gidra <lokeshgidra@google.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: "Michal Koutn" <mkoutny@suse.com>
    Cc: Michel Lespinasse <walken@google.com>
    Cc: Mina Almasry <almasrymina@google.com>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Oliver Upton <oupton@google.com>
    Cc: Shaohua Li <shli@fb.com>
    Cc: Shawn Anastasio <shawn@anastas.io>
    Cc: Steven Price <steven.price@arm.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    
    (cherry picked from commit 267bda5c9993856b86f91a998df632b29cf517e2
    https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git akpm)
    Link: https://lore.kernel.org/patchwork/patch/1382208/
    Conflicts:
    	mm/hugetlb.c
    
    (CONFIG_CMA not CP'ed in this kernel. Manual rebase)
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>
    Bug: 160737021
    Bug: 169683130
    Change-Id: I99d541ce45aaf924fa912f00dafa4caefe307755
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -160,6 +160,8 @@ unsigned long hugetlb_change_protection(struct vm_area_struct *vma,
 		unsigned long address, unsigned long end, pgprot_t newprot);
 
 bool is_hugetlb_entry_migration(pte_t pte);
+void hugetlb_unshare_all_pmds(struct vm_area_struct *vma);
+
 #else /* !CONFIG_HUGETLB_PAGE */
 
 static inline void reset_vma_resv_huge_pages(struct vm_area_struct *vma)
@@ -232,6 +234,8 @@ static inline void __unmap_hugepage_range(struct mmu_gather *tlb,
 {
 	BUG();
 }
+
+static inline void hugetlb_unshare_all_pmds(struct vm_area_struct *vma) { }
 
 #endif /* !CONFIG_HUGETLB_PAGE */
 /*
