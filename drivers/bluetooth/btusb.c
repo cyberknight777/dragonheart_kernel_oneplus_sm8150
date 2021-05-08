508cba2d87edcdbd29b8422d3620eef23cddfaf4
Joseph Hwang <josephsih@chromium.org>
2021-05-08 17:58:52 +0800

    CHROMIUM: Bluetooth: disable Intel quality report feature by default
    To avoid overhead, this quality report feature is disabled by default.
    
    (This CHROMIUM patch will be reverted by an UPSTREAM patch later.)
    
    BUG=b:170175384
    TEST=None
    
    Change-Id: I731a3535df9ec79d87e5bce0de5e18ad9fc57e0c
    Signed-off-by: Joseph Hwang <josephsih@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2949247
    Reviewed-by: Yun-Hao Chung <howardchung@chromium.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -2357,7 +2357,6 @@ static int btusb_setup_intel_new(struct hci_dev *hdev)
 	struct btusb_data *data = hci_get_drvdata(hdev);
 	struct intel_version ver;
 	struct intel_boot_params params;
-	struct intel_debug_features features;
 	const struct firmware *fw;
 	u32 boot_param;
 	char fwname[64];
@@ -2652,14 +2651,6 @@ done:
 	 * to load the file, no need to fail the setup.
 	 */
 	btintel_load_ddc_config(hdev, fwname);
-
-	/* Read the Intel supported features and if new exception formats
-	 * supported, need to load the additional DDC config to enable.
-	 */
-	btintel_read_debug_features(hdev, &features);
-
-	/* Set DDC mask for available debug features */
-	btintel_set_debug_features(hdev, &features);
 
 	/* Read the Intel version information after loading the FW  */
 	err = btintel_read_version(hdev, &ver);
