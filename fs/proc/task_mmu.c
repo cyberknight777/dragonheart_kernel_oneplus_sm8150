22daf0d8528357ee32368bf7e3ffe2a6588e2545
Yu Zhao <yuzhao@google.com>
2021-05-21 00:33:02 -0600

    Revert "CHROMIUM: mm: fix tlb flush race between page table and rmap walkers"
    This reverts commit 49fd2c593997f31333ce85d153d461001be77dd4.
    
    Upstream commit 07509e10dcc7 ("arm64: pgtable: Fix pte_accessible()")
    superseded the reverted commit.
    
    BUG=b:166311876
    TEST=Built
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Change-Id: I8487b6e8c87f52ea929d74d2203fdf26b334d144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2912149
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -2079,7 +2079,6 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 	struct vm_area_struct *vma;
 	enum reclaim_type type;
 	char *type_buf;
-	struct mmu_gather tlb;
 
 	memset(buffer, 0, sizeof(buffer));
 	if (count > sizeof(buffer) - 1)
@@ -2119,7 +2118,6 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 		};
 
 		down_read(&mm->mmap_sem);
-		tlb_gather_mmu(&tlb, mm, 0, -1);
 		for (vma = mm->mmap; vma; vma = vma->vm_next) {
 			if (is_vm_hugetlb_page(vma))
 				continue;
@@ -2155,7 +2153,7 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 			walk_page_range(vma->vm_start, vma->vm_end,
 					&reclaim_walk);
 		}
-		tlb_finish_mmu(&tlb, 0, -1);
+		flush_tlb_mm(mm);
 		up_read(&mm->mmap_sem);
 		mmput(mm);
 	}
