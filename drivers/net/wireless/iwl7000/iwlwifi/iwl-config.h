4ffed95a5bbc54dd4a0adfb5509e236ce4ed117d
Mike Golant <michael.golant@intel.com>
2021-06-21 12:09:06 +0300

    CHROMIUM: iwl7000: add support for Bz-Z HW
    Add support for Bz Z step HW with GF RF.
    
    Change-Id: Ibfeffc8bb071f9773ea189992ebef056f51be1be
    Signed-off-by: Mike Golant <michael.golant@intel.com>
    iwl7000-tree: 8bfeaee5dae1faf8c9431d9ffd56575336d84bed

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-config.h
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-config.h
@@ -592,6 +592,7 @@ extern const struct iwl_cfg iwl_cfg_bz_a0_gf4_a0;
 extern const struct iwl_cfg iwl_cfg_bz_a0_mr_a0;
 extern const struct iwl_cfg iwl_cfg_bz_a0_fm_a0;
 extern const struct iwl_cfg iwl_cfg_gl_a0_fm_a0;
+extern const struct iwl_cfg iwl_cfg_bz_z0_gf_a0;
 #endif /* CPTCFG_IWLMVM || CPTCFG_IWLFMAC */
 
 #endif /* __IWL_CONFIG_H__ */
