af3c49c84bb81a569196edf81c17db256274820b
Vaisakh Murali <mvaisakh@statixos.com>
2021-06-27 16:56:52 +0530

    drivers: input: oneplus_ts: Drop unneeded pm_qos requests
    * This wastes power and is unneededly heavy, since we affine all
      touchscreens to the big cluster CPUs, this makes no sense to have
      as an aditional overhead to the already boosted driver.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/input/oneplus_touchscreen/Samsung/S6SY761/sec_drivers_s6sy761.c
+++ drivers/input/oneplus_touchscreen/Samsung/S6SY761/sec_drivers_s6sy761.c
@@ -15,7 +15,6 @@
 #include <linux/pinctrl/pinctrl.h>
 #include <linux/pinctrl/machine.h>
 #include <linux/regulator/consumer.h>
-#include <linux/pm_qos.h>
 
 #ifdef CONFIG_FB
 #include <linux/fb.h>
@@ -26,8 +25,6 @@
 
 extern int tp_register_times;
 extern struct touchpanel_data *g_tp;
-#define PM_QOS_VALUE_TP 200
-struct pm_qos_request pm_qos_req_stp;
 
 /****************** Start of Log Tag Declear and level define*******************************/
 #define TPD_DEVICE "sec-s6sy761"
@@ -993,12 +990,10 @@ static u8 sec_trigger_reason(void *chip_data, int gesture_enable, int is_suspend
 	struct chip_data_s6sy761 *chip_info = (struct chip_data_s6sy761 *)chip_data;
 	struct fp_underscreen_info tp_info;
 
-	pm_qos_add_request(&pm_qos_req_stp, PM_QOS_CPU_DMA_LATENCY, PM_QOS_VALUE_TP);
 	memset(chip_info->first_event, 0, SEC_EVENT_BUFF_SIZE);
 	ret = touch_i2c_read_block(chip_info->client, SEC_READ_ONE_EVENT, SEC_EVENT_BUFF_SIZE, chip_info->first_event);
 	if (ret < 0) {
 		TPD_DETAIL("%s: read one event failed\n", __func__);
-		pm_qos_remove_request(&pm_qos_req_stp);
 		return IRQ_IGNORE;
 	}
 
@@ -1008,7 +1003,6 @@ static u8 sec_trigger_reason(void *chip_data, int gesture_enable, int is_suspend
 
 	if (chip_info->first_event[0] == 0) {
 		TPD_DETAIL("%s: event buffer is empty\n", __func__);
-		pm_qos_remove_request(&pm_qos_req_stp);
 		return IRQ_IGNORE;
 	}
 
@@ -1019,7 +1013,6 @@ static u8 sec_trigger_reason(void *chip_data, int gesture_enable, int is_suspend
 		if (ret < 0) {
 			TPD_INFO("%s: clear event buffer failed\n", __func__);
 		}
-		pm_qos_remove_request(&pm_qos_req_stp);
 		return IRQ_IGNORE;
 	}
 
@@ -1033,7 +1026,6 @@ static u8 sec_trigger_reason(void *chip_data, int gesture_enable, int is_suspend
 			if (ret < 0) {
 				TPD_INFO("%s: write sense on failed\n", __func__);
 			}
-			pm_qos_remove_request(&pm_qos_req_stp);
 			return IRQ_FW_AUTO_RESET;
 		}
 
@@ -1045,14 +1037,12 @@ static u8 sec_trigger_reason(void *chip_data, int gesture_enable, int is_suspend
 
 		if ((p_event_status->stype == TYPE_STATUS_EVENT_ERR) && (p_event_status->status_id == SEC_ERR_EVENT_ESD)) {
 			TPD_INFO("%s: ESD detected. run reset\n", __func__);
-			pm_qos_remove_request(&pm_qos_req_stp);
 			return IRQ_EXCEPTION;
 		}
 
 		if ((p_event_status->stype == TYPE_STATUS_EVENT_VENDOR_INFO) && (p_event_status->status_id == SEC_STATUS_EARDETECTED)) {
 			chip_info->proximity_status = p_event_status->status_data_1;
 			TPD_INFO("%s: face detect status %d\n",__func__, chip_info->proximity_status);
-			pm_qos_remove_request(&pm_qos_req_stp);
 			return IRQ_FACE_STATE;
 		}
 
@@ -1079,14 +1069,12 @@ static u8 sec_trigger_reason(void *chip_data, int gesture_enable, int is_suspend
 					gf_opticalfp_irq_handler(0);
 			}
 			TPD_INFO("%s: touch_hold status %d\n",__func__, p_event_status->status_data_1);
-			pm_qos_remove_request(&pm_qos_req_stp);
 			return IRQ_IGNORE;
 		}
 
 		if ((p_event_status->stype == TYPE_STATUS_EVENT_INFO) && (p_event_status->status_id == SEC_TS_ACK_WET_MODE)) {
 			chip_info->wet_mode = p_event_status->status_data_1;
 			TPD_INFO("%s: water wet mode %d\n",__func__, chip_info->wet_mode);
-			pm_qos_remove_request(&pm_qos_req_stp);
 			return IRQ_IGNORE;
 		}
 		if ((p_event_status->stype == TYPE_STATUS_EVENT_VENDOR_INFO) && (p_event_status->status_id == SEC_TS_VENDOR_ACK_NOISE_STATUS_NOTI)) {
@@ -1094,7 +1082,6 @@ static u8 sec_trigger_reason(void *chip_data, int gesture_enable, int is_suspend
 			TPD_INFO("%s: TSP NOISE MODE %s[%d]\n",
 					__func__,chip_info->touch_noise_status == 0 ? "OFF" : "ON",
 					p_event_status->status_data_1);
-			pm_qos_remove_request(&pm_qos_req_stp);
 			return IRQ_IGNORE;
 		}
 	} else if (event_id == SEC_COORDINATE_EVENT) {
@@ -1102,7 +1089,6 @@ static u8 sec_trigger_reason(void *chip_data, int gesture_enable, int is_suspend
 	} else if (event_id == SEC_GESTURE_EVENT) {
 		return IRQ_GESTURE;
 	}
-	pm_qos_remove_request(&pm_qos_req_stp);
 	return IRQ_IGNORE;
 }
 
@@ -1120,7 +1106,6 @@ static int sec_get_touch_points(void *chip_data, struct point_info *points, int 
 	event_buff = kzalloc(MAX_EVENT_COUNT*SEC_EVENT_BUFF_SIZE * (sizeof(uint8_t)), GFP_KERNEL);
 	if (!event_buff) {
 		TPD_INFO("event_buff kzalloc failed\n");
-		pm_qos_remove_request(&pm_qos_req_stp);
 		return -ENOMEM;
 	}
 	p_event_coord = (struct sec_event_coordinate *)chip_info->first_event;
@@ -1142,7 +1127,6 @@ static int sec_get_touch_points(void *chip_data, struct point_info *points, int 
 	left_event = chip_info->first_event[7] & 0x3F;
 	if (left_event == 0) {
 		kfree(event_buff);
-		pm_qos_remove_request(&pm_qos_req_stp);
 		return obj_attention;
 	} else if (left_event > max_num - 1) {
 		TPD_INFO("%s: read left event beyond max touch points\n", __func__);
@@ -1152,7 +1136,6 @@ static int sec_get_touch_points(void *chip_data, struct point_info *points, int 
 	if (ret < 0) {
 		TPD_INFO("%s: i2c read all event failed\n", __func__);
 		kfree(event_buff);
-		pm_qos_remove_request(&pm_qos_req_stp);
 		return obj_attention;
 	}
 
@@ -1174,7 +1157,6 @@ static int sec_get_touch_points(void *chip_data, struct point_info *points, int 
 		}
 	}
 	kfree(event_buff);
-	pm_qos_remove_request(&pm_qos_req_stp);
 	return obj_attention;
 }
 
@@ -1378,7 +1360,6 @@ static int sec_get_gesture_info(void *chip_data, struct gesture_info * gesture)
 			gesture->Point_2nd.x, gesture->Point_2nd.y, \
 			gesture->Point_3rd.x, gesture->Point_3rd.y, \
 			gesture->Point_4th.x, gesture->Point_4th.y);
-	pm_qos_remove_request(&pm_qos_req_stp);
 
 	return 0;
 }
