e9220cfcfa45e024fdea2b99f90a1a68a08ee3c8
Mike Golant <michael.golant@intel.com>
2021-07-27 10:37:48 +0300

    CHROMIUM: iwl7000: add support for BNJ HW
    Add support for BNJ HW with GF, GF4, HR1, FM and FM4 RF modules
    
    Change-Id: I8cba209b1366dc0636a04711fc6a85539ca878d0
    Signed-off-by: Mike Golant <michael.golant@intel.com>
    iwl7000-tree: 702bee75fb672b771f033c6b924f76987376bf07

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-config.h
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-config.h
@@ -596,6 +596,11 @@ extern const struct iwl_cfg iwl_cfg_bz_a0_mr_a0;
 extern const struct iwl_cfg iwl_cfg_bz_a0_fm_a0;
 extern const struct iwl_cfg iwl_cfg_gl_a0_fm_a0;
 extern const struct iwl_cfg iwl_cfg_bz_z0_gf_a0;
+extern const struct iwl_cfg iwl_cfg_bnj_a0_fm_a0;
+extern const struct iwl_cfg iwl_cfg_bnj_a0_fm4_a0;
+extern const struct iwl_cfg iwl_cfg_bnj_a0_gf_a0;
+extern const struct iwl_cfg iwl_cfg_bnj_a0_gf4_a0;
+extern const struct iwl_cfg iwl_cfg_bnj_a0_hr_b0;
 #endif /* CPTCFG_IWLMVM || CPTCFG_IWLFMAC */
 
 #endif /* __IWL_CONFIG_H__ */
