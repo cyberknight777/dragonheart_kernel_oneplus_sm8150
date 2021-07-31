ae4c4663669ca18fd647959dcb7046b5887eeb99
Vaisakh Murali <mvaisakh@statixos.com>
2021-07-31 13:48:16 +0530

    drivers/input: fp_detect: Cleanup and Simplify fp detection logic
    Cleanup debug bloat and reduce extra work done by the driver for
    the fingerprint detection and inline the driver as much as possible.
    
    All of the OnePlus 7 series devices use Goodix fingerprint with
    the exact same fingerprint sensor id, with hotdogg being the only
    exception with a different sensor id.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/input/fingerprint/fingerprint_detect/fingerprint_detect.h
+++ drivers/input/fingerprint/fingerprint_detect/fingerprint_detect.h
@@ -1,10 +1,8 @@
-#ifndef __FINGERPRINT_DETETC_H_
-#define __FINGERPRINT_DETETC_H_
+#ifndef __FINGERPRINT_DETECT_H_
+#define __FINGERPRINT_DETECT_H_
 
 struct fingerprint_detect_data {
 	struct device *dev;
-	int id0_gpio;
-	int id1_gpio;
 	struct pinctrl         *fp_pinctrl;
 	struct pinctrl_state   *id_state_init;
 	int sensor_version;
