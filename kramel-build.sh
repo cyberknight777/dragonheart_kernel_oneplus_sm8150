7dbc5f93665cf6f26b963bdf698f1aba55568028
Cyber Knight <cyberknight755@gmail.com>
2021-08-11 21:38:55 +0800

    kramel-build: fix version mismatch in message
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kramel-build.sh
+++ kramel-build.sh
@@ -168,7 +168,7 @@ DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%s")
 	echo " "
 	if [ $COMPILER = "gcc" ]
 	then
-		msg "|| Cloning GCC 9.3.0 baremetal ||"
+		msg "|| Cloning GCC 12.0.0 baremetal ||"
 	        wget -O 64.zip https://github.com/mvaisakh/gcc-arm64/archive/1a4410a4cf49c78ab83197fdad1d2621760bdc73.zip;unzip 64.zip;mv gcc-arm64-1a4410a4cf49c78ab83197fdad1d2621760bdc73 gcc64
 		wget -O 32.zip https://github.com/mvaisakh/gcc-arm/archive/c8b46a6ab60d998b5efa1d5fb6aa34af35a95bad.zip;unzip 32.zip;mv gcc-arm-c8b46a6ab60d998b5efa1d5fb6aa34af35a95bad gcc32
 		GCC64_DIR=$KERNEL_DIR/gcc64
