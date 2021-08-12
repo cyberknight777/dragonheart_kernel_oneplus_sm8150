b29d1872d2f9d24fd416c6711bc3668979036719
Johannes Berg <johannes.berg@intel.com>
2021-08-12 13:34:58 +0200

    CHROMIUM: iwl7000: mvm: add some missing command strings
    Some command strings in the system group weren't added
    for debug, add them now.
    
    Change-Id: I53cefd74547a745fd29261a795c94611e7ee8d1d
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: 684fb5b77f0f9a3ff7a2395595acd7351f3c9083

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/ops.c
@@ -559,6 +559,8 @@ static const struct iwl_hcmd_names iwl_mvm_system_names[] = {
 	HCMD_NAME(SOC_CONFIGURATION_CMD),
 	HCMD_NAME(INIT_EXTENDED_CFG_CMD),
 	HCMD_NAME(FW_ERROR_RECOVERY_CMD),
+	HCMD_NAME(RFI_CONFIG_CMD),
+	HCMD_NAME(RFI_GET_FREQ_TABLE_CMD),
 };
 
 /* Please keep this array *SORTED* by hex value.
