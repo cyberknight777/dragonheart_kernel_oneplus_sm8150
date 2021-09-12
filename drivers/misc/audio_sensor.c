27c7705eff7f2cc6dd6aecb16314e7aa33bebe3c
Cyber Knight <cyberknight755@gmail.com>
2021-09-12 20:36:35 +0800

    drivers/misc: Fix print format specifier warnings
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/misc/audio_sensor.c
+++ drivers/misc/audio_sensor.c
@@ -192,7 +192,7 @@ static long share_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	int ret = 0;
 	struct notify_data *data = file->private_data;
 
-	pr_debug("%s: arg is %#x\n", __func__, arg);
+	pr_debug("%s: arg is %#lu\n", __func__, arg);
 
 	switch (cmd) {
 	case IOCTL_AUDIO_SEND_ACK:
