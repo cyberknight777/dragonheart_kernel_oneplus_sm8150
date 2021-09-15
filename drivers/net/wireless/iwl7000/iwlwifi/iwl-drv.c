101aacfc5b11311814737ff3dd95e6a7d3279937
Johannes Berg <johannes.berg@intel.com>
2021-09-15 17:10:29 +0200

    CHROMIUM: iwl7000: fix leaks/bad data after failed firmware load
    If firmware load fails after having loaded some parts of the
    firmware, e.g. the IML image, then this would leak. For the
    host command list we'd end up running into a WARN on the next
    attempt to load another firmware image.
    
    Fix this by calling iwl_dealloc_ucode() on failures, and make
    that also clear the data so we start fresh on the next round.
    
    Change-Id: I1315f22f6aa632d94ae2069f85e1bca5e734dce0
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: 2f709ec1e7be26ac843271f68c19f27754ea7a62

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-drv.c
@@ -340,6 +340,9 @@ static void iwl_dealloc_ucode(struct iwl_drv *drv)
 
 	for (i = 0; i < IWL_UCODE_TYPE_MAX; i++)
 		iwl_free_fw_img(drv, drv->fw.img + i);
+
+	/* clear the data for the aborted load case */
+	memset(&drv->fw, 0, sizeof(drv->fw));
 }
 
 static int iwl_alloc_fw_desc(struct iwl_drv *drv, struct fw_desc *desc,
@@ -1700,6 +1703,7 @@ static void iwl_req_fw_callback(const struct firmware *ucode_raw, void *context)
 	int i;
 	bool load_module = false;
 	bool usniffer_images = false;
+	bool failure = true;
 
 #ifdef CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDES
 	const struct firmware *fw_dbg_config;
@@ -1996,6 +2000,7 @@ static void iwl_req_fw_callback(const struct firmware *ucode_raw, void *context)
 	 */
 	if (load_module)
 		request_module("%s", op->name);
+	failure = false;
 	goto free;
 
  try_again:
@@ -2011,6 +2016,9 @@ static void iwl_req_fw_callback(const struct firmware *ucode_raw, void *context)
 	complete(&drv->request_firmware_complete);
 	device_release_driver(drv->trans->dev);
  free:
+	if (failure)
+		iwl_dealloc_ucode(drv);
+
 	if (pieces) {
 		for (i = 0; i < ARRAY_SIZE(pieces->img); i++)
 			kfree(pieces->img[i].sec);
