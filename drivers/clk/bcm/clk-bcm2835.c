e3fb831db725beac0bd0051efcab09d9f14900d2
Cyber Knight <cyberknight755@gmail.com>
2021-09-26 00:41:52 +0800

    drivers/clk: Import OnePlus Changes
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/clk/bcm/clk-bcm2835.c
+++ drivers/clk/bcm/clk-bcm2835.c
@@ -1354,10 +1354,8 @@ static struct clk_hw *bcm2835_register_pll(struct bcm2835_cprman *cprman,
 	pll->hw.init = &init;
 
 	ret = devm_clk_hw_register(cprman->dev, &pll->hw);
-	if (ret) {
-		kfree(pll);
+	if (ret)
 		return NULL;
-	}
 	return &pll->hw;
 }
 
