2a123f9619281f576624dcf37445cba8c197932c
Cyber Knight <cyberknight755@gmail.com>
2021-09-26 01:48:53 +0800

    drivers/power: Import OnePlus Changes
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/power/supply/test_power.c
+++ drivers/power/supply/test_power.c
@@ -344,7 +344,6 @@ static int param_set_ac_online(const char *key, const struct kernel_param *kp)
 static int param_get_ac_online(char *buffer, const struct kernel_param *kp)
 {
 	strcpy(buffer, map_get_key(map_ac_online, ac_online, "unknown"));
-	strcat(buffer, "\n");
 	return strlen(buffer);
 }
 
@@ -358,7 +357,6 @@ static int param_set_usb_online(const char *key, const struct kernel_param *kp)
 static int param_get_usb_online(char *buffer, const struct kernel_param *kp)
 {
 	strcpy(buffer, map_get_key(map_ac_online, usb_online, "unknown"));
-	strcat(buffer, "\n");
 	return strlen(buffer);
 }
 
@@ -373,7 +371,6 @@ static int param_set_battery_status(const char *key,
 static int param_get_battery_status(char *buffer, const struct kernel_param *kp)
 {
 	strcpy(buffer, map_get_key(map_status, battery_status, "unknown"));
-	strcat(buffer, "\n");
 	return strlen(buffer);
 }
 
@@ -388,7 +385,6 @@ static int param_set_battery_health(const char *key,
 static int param_get_battery_health(char *buffer, const struct kernel_param *kp)
 {
 	strcpy(buffer, map_get_key(map_health, battery_health, "unknown"));
-	strcat(buffer, "\n");
 	return strlen(buffer);
 }
 
@@ -404,7 +400,6 @@ static int param_get_battery_present(char *buffer,
 					const struct kernel_param *kp)
 {
 	strcpy(buffer, map_get_key(map_present, battery_present, "unknown"));
-	strcat(buffer, "\n");
 	return strlen(buffer);
 }
 
@@ -422,7 +417,6 @@ static int param_get_battery_technology(char *buffer,
 {
 	strcpy(buffer,
 		map_get_key(map_technology, battery_technology, "unknown"));
-	strcat(buffer, "\n");
 	return strlen(buffer);
 }
 
