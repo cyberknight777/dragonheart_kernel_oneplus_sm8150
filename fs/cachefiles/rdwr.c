568b071a0ebe905a2b25a0fa1e2849fbdab655ba
Cyber Knight <cyberknight755@gmail.com>
2021-09-26 02:06:55 +0800

    fs/cachefiles: Import OnePlus Changes
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- fs/cachefiles/rdwr.c
+++ fs/cachefiles/rdwr.c
@@ -125,7 +125,7 @@ static int cachefiles_read_reissue(struct cachefiles_object *object,
 		_debug("reissue read");
 		ret = bmapping->a_ops->readpage(NULL, backpage);
 		if (ret < 0)
-			goto discard;
+			goto unlock_discard;
 	}
 
 	/* but the page may have been read before the monitor was installed, so
@@ -142,7 +142,6 @@ static int cachefiles_read_reissue(struct cachefiles_object *object,
 
 unlock_discard:
 	unlock_page(backpage);
-discard:
 	spin_lock_irq(&object->work_lock);
 	list_del(&monitor->op_link);
 	spin_unlock_irq(&object->work_lock);
