b311f1743afe85c94660fba09b944c8ec175691b
Cyber Knight <cyberknight755@gmail.com>
2021-09-30 18:48:37 +0800

    drivers: kprofiles: nuke logging
    It is better to implement logging in parts where kprofiles is used instead of the driver itself so reduce logspam.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -13,17 +13,14 @@ module_param(enabled, uint, 0664);
 
 unsigned int active_mode(void) {
 	if (enabled == 1) {
-		pr_info("changing active mode to level 1\n");
 		return 1;
 	}
 
 	if (enabled == 2) {
-		pr_info("changing active mode to level 2\n");
 		return 2;
 	}
 
 	if (enabled == 3) {
-		pr_info("changing active mode to level 3\n");
 		return 3;
 	}
 
