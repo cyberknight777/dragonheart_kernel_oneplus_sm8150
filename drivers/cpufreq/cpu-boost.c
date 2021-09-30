f942ee147b8a841a27786e7df36beebbcc276f6b
Dakkshesh <dakkshesh5@gmail.com>
2021-09-30 20:17:05 +0800

    drivers/cpufreq: cpu-boost: Skip boosting when profiles is on level 1
    [based-on]: https://github.com/dakkshesh07/parallax_kernel_realme_rmx1921/commit/dd701c912046d399066fb1d527b40c41f8fd0441
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -22,6 +22,7 @@
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/time.h>
+#include <linux/kprofiles.h>
 
 struct cpu_sync {
 	int cpu;
@@ -191,6 +192,11 @@ static void do_input_boost(struct work_struct *work)
 {
 	unsigned int i, ret;
 	struct cpu_sync *i_sync_info;
+
+	if (active_mode() == 1) {
+		pr_info("Skipping boost as battery profile is enabled\n");
+		return;
+	}
 
 	cancel_delayed_work_sync(&input_boost_rem);
 	if (sched_boost_active) {
