285f853d126c4bc79d353cc688de155478927511
Brent Lu <brent.lu@intel.com>
2021-10-28 22:09:09 +0800

    BACKPORT: ASoC: Intel: glk_rt5682_max98357a: support ALC5682I-VS codec
    Detect the codec variant in probe function and update DAI link
    accordingly. Also add an new entry in enumeration table for machine
    driver enumeration.
    
    Signed-off-by: Brent Lu <brent.lu@intel.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20211028140909.496022-1-brent.lu@intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    (cherry picked from commit 88b4d77d6035dcf1182c4bf05c743e30363f3078)
    
    Conflicts:
       sound/soc/intel/boards/Kconfig
       sound/soc/intel/boards/glk_rt5682_max98357a.c
    
    BUG=b:198716740
    TEST=ODM tests audio function on Chrome device
    
    Change-Id: I37c1152a72905461acb0c91fd4e20f95be745015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3234261
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    Tested-by: Brent Lu <brent.lu@intel.com>
    Commit-Queue: Curtis Malainey <cujomalainey@chromium.org>

--- sound/soc/intel/boards/glk_rt5682_max98357a.c
+++ sound/soc/intel/boards/glk_rt5682_max98357a.c
@@ -19,13 +19,17 @@
 #include <sound/soc-acpi.h>
 #include "../skylake/skl.h"
 #include "../../codecs/rt5682.h"
+#include "../../codecs/rt5682s.h"
 #include "../../codecs/hdac_hdmi.h"
 
 /* The platform clock outputs 19.2Mhz clock to codec as I2S MCLK */
 #define GLK_PLAT_CLK_FREQ 19200000
 #define RT5682_PLL_FREQ (48000 * 512)
-#define GLK_REALTEK_CODEC_DAI "rt5682-aif1"
+#define RT5682_DAI_NAME "rt5682-aif1"
+#define RT5682S_DAI_NAME "rt5682s-aif1"
 #define GLK_MAXIM_CODEC_DAI "HiFi"
+#define RT5682_DEV0_NAME "i2c-10EC5682:00"
+#define RT5682S_DEV0_NAME "i2c-RTL5682:00"
 #define MAXIM_DEV0_NAME "MX98357A:00"
 #define DUAL_CHANNEL 2
 #define QUAD_CHANNEL 4
@@ -43,6 +47,7 @@ struct glk_hdmi_pcm {
 struct glk_card_private {
 	struct snd_soc_jack geminilake_headset;
 	struct list_head hdmi_pcm_list;
+	int is_rt5682s;
 };
 
 enum {
@@ -138,9 +143,19 @@ static int geminilake_rt5682_codec_init(struct snd_soc_pcm_runtime *rtd)
 	struct snd_soc_component *component = rtd->codec_dai->component;
 	struct snd_soc_dai *codec_dai = rtd->codec_dai;
 	struct snd_soc_jack *jack;
-	int ret;
+	int pll_id, pll_source, clk_id, ret;
 
-	ret = snd_soc_dai_set_pll(codec_dai, 0, RT5682_PLL1_S_MCLK,
+	if (ctx->is_rt5682s) {
+		pll_id = RT5682S_PLL2;
+		pll_source = RT5682S_PLL_S_MCLK;
+		clk_id = RT5682S_SCLK_S_PLL2;
+	} else {
+		pll_id = RT5682_PLL1;
+		pll_source = RT5682_PLL1_S_MCLK;
+		clk_id = RT5682_SCLK_S_PLL1;
+	}
+
+	ret = snd_soc_dai_set_pll(codec_dai, pll_id, pll_source,
 					GLK_PLAT_CLK_FREQ, RT5682_PLL_FREQ);
 	if (ret < 0) {
 		dev_err(rtd->dev, "can't set codec pll: %d\n", ret);
@@ -148,7 +163,7 @@ static int geminilake_rt5682_codec_init(struct snd_soc_pcm_runtime *rtd)
 	}
 
 	/* Configure sysclk for codec */
-	ret = snd_soc_dai_set_sysclk(codec_dai, RT5682_SCLK_S_PLL1,
+	ret = snd_soc_dai_set_sysclk(codec_dai, clk_id,
 					RT5682_PLL_FREQ, SND_SOC_CLOCK_IN);
 	if (ret < 0)
 		dev_err(rtd->dev, "snd_soc_dai_set_sysclk err = %d\n", ret);
@@ -490,8 +505,8 @@ static struct snd_soc_dai_link geminilake_dais[] = {
 		.cpu_dai_name = "SSP2 Pin",
 		.platform_name = "0000:00:0e.0",
 		.no_pcm = 1,
-		.codec_name = "i2c-10EC5682:00",
-		.codec_dai_name = GLK_REALTEK_CODEC_DAI,
+		.codec_name = RT5682_DEV0_NAME,
+		.codec_dai_name = RT5682_DAI_NAME,
 		.init = geminilake_rt5682_codec_init,
 		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
 			SND_SOC_DAIFMT_CBS_CFS,
@@ -604,11 +619,27 @@ static int geminilake_audio_probe(struct platform_device *pdev)
 	struct snd_soc_acpi_mach *mach;
 	const char *platform_name;
 	struct snd_soc_card *card;
-	int ret;
+	int ret, i;
 
 	ctx = devm_kzalloc(&pdev->dev, sizeof(*ctx), GFP_KERNEL);
 	if (!ctx)
 		return -ENOMEM;
+
+	/* Detect the headset codec variant */
+	if (acpi_dev_present("RTL5682", NULL, -1)) {
+		/* ALC5682I-VS is detected */
+		ctx->is_rt5682s = 1;
+
+		for (i = 0; i < glk_audio_card_rt5682_m98357a.num_links; i++) {
+			if (strcmp(geminilake_dais[i].name, "SSP2-Codec"))
+				continue;
+
+			/* update the dai link to use rt5682s codec */
+			geminilake_dais[i].codec_name = RT5682S_DEV0_NAME;
+			geminilake_dais[i].codec_dai_name = RT5682S_DAI_NAME;
+			break;
+		}
+	}
 
 	INIT_LIST_HEAD(&ctx->hdmi_pcm_list);
 
