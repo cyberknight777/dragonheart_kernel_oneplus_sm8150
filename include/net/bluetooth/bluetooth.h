3183af187a45f7b76419a3537908cc8af4cc4713
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2021-12-03 16:15:40 -0800

    FROMGIT: Bluetooth: mgmt: Introduce mgmt_alloc_skb and mgmt_send_event_skb
    This introduces mgmt_alloc_skb and mgmt_send_event_skb which are
    convenient when building MGMT events that have variable length as the
    likes of skb_put_data can be used to insert portion directly on the skb
    instead of having to first build an intermediate buffer just to be
    copied over the skb.
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    (cherry picked from commit 8aca46f91c42020bc58cd56e464a1101e517aa10
     git://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git
     master)
    
    BUG=None
    TEST=Build
    
    Signed-off-by: Manish Mandlik <mmandlik@google.com>
    Change-Id: I1ed21bf57c2a70b7bb2b415ff20ae6b3b0dbbf64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3344345
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Tested-by: Manish Mandlik <mmandlik@chromium.org>
    Commit-Queue: Manish Mandlik <mmandlik@chromium.org>

--- include/net/bluetooth/bluetooth.h
+++ include/net/bluetooth/bluetooth.h
@@ -354,6 +354,11 @@ struct hci_ctrl {
 	};
 };
 
+struct mgmt_ctrl {
+	struct hci_dev *hdev;
+	u16 opcode;
+};
+
 struct bt_skb_cb {
 	u8 pkt_type;
 	u8 force_active;
@@ -363,6 +368,7 @@ struct bt_skb_cb {
 		struct l2cap_ctrl l2cap;
 		struct sco_ctrl sco;
 		struct hci_ctrl hci;
+		struct mgmt_ctrl mgmt;
 	};
 };
 #define bt_cb(skb) ((struct bt_skb_cb *)((skb)->cb))
