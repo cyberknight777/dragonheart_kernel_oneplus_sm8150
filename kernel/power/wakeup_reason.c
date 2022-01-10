b9f077d3c1705c3a0c92633e50d995a59ced7a4e
Cyber Knight <cyberknight755@gmail.com>
2022-01-10 20:33:36 +0800

    kernel/power: Reduce verbosity of logging
    Silences:
    [41385.565295] Restarting tasks ...
    [41385.579844] Resume caused by IRQ 178, WLAN_CE_2
    [41385.579855] PM: PM: suspend exit 2022-01-10 10:54:26.808561566 UTC
    [41385.579857] PM: suspend exit
    [41385.680504] PM: PM: suspend entry 2022-01-10 10:54:26.909195420 UTC
    [41385.680519] PM: suspend entry (deep)
    [41385.704201] Freezing user space processes ...
    [41385.716293] Freezing remaining freezable tasks ...
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -266,10 +266,10 @@ static void print_wakeup_sources(void)
 
 	if (!list_empty(&leaf_irqs))
 		list_for_each_entry(n, &leaf_irqs, siblings)
-			pr_info("Resume caused by IRQ %d, %s\n", n->irq,
+			pr_debug("Resume caused by IRQ %d, %s\n", n->irq,
 				n->irq_name);
 	else if (abnormal_wake)
-		pr_info("Resume caused by %s\n", non_irq_wake_reason);
+		pr_debug("Resume caused by %s\n", non_irq_wake_reason);
 	else
 		pr_info("Resume cause unknown\n");
 
