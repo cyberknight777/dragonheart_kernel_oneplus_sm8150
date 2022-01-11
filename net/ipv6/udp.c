b8f44ff0de7dda41911009bbdce4c4d20d243b10
Pavel Begunkov <asml.silence@gmail.com>
2022-01-11 01:21:34 +0000

    BACKPORT: ipv6: shuffle up->pending AF_INET bits
    Corked AF_INET for ipv6 socket doesn't appear to be the hottest case,
    so move it out of the common path under up->pending check to remove
    overhead.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    [cyberknight777: backport to 4.14]
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- net/ipv6/udp.c
+++ net/ipv6/udp.c
@@ -1236,9 +1236,6 @@ do_udp_sendmsg:
 		}
 	}
 
-	if (up->pending == AF_INET)
-		return udp_sendmsg(sk, msg, len);
-
 	/* Rough check on arithmetic overflow,
 	   better check is made in ip6_append_data().
 	   */
@@ -1247,6 +1244,8 @@ do_udp_sendmsg:
 
 	getfrag  =  is_udplite ?  udplite_getfrag : ip_generic_getfrag;
 	if (up->pending) {
+		if (up->pending == AF_INET)
+			return udp_sendmsg(sk, msg, len);
 		/*
 		 * There are pending frames.
 		 * The socket lock must be held while it's corked.
