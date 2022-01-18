0af3aa1e4555b5011da1d0cc3860fc8b87db5d7b
Cyber Knight <cyberknight755@gmail.com>
2022-01-19 02:55:01 +0800

    media/usb: uvc: Fix build when uvc_debugfs.c is not compiled
    Fixes: ac540d928b26 {"media/usb: uvc: Build uvc_debugfs if CONFIG_DEBUG_FS is enabled"}
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -1890,8 +1890,9 @@ static void uvc_unregister_video(struct uvc_device *dev)
 			continue;
 
 		video_unregister_device(&stream->vdev);
-
+#ifdef CONFIG_DEBUG_FS
 		uvc_debugfs_cleanup_stream(stream);
+#endif
 	}
 }
 
@@ -1915,9 +1916,9 @@ static int uvc_register_video(struct uvc_device *dev,
 			"(%d).\n", ret);
 		return ret;
 	}
-
+#ifdef CONFIG_DEBUG_FS
 	uvc_debugfs_init_stream(stream);
-
+#endif
 	/* Register the device with V4L. */
 
 	/* We already hold a reference to dev->udev. The video device will be
@@ -2773,12 +2774,14 @@ struct uvc_driver uvc_driver = {
 static int __init uvc_init(void)
 {
 	int ret;
-
+#ifdef CONFIG_DEBUG_FS
 	uvc_debugfs_init();
-
+#endif
 	ret = usb_register(&uvc_driver.driver);
 	if (ret < 0) {
+#ifdef CONFIG_DEBUG_FS
 		uvc_debugfs_cleanup();
+#endif
 		return ret;
 	}
 
@@ -2789,7 +2792,9 @@ static int __init uvc_init(void)
 static void __exit uvc_cleanup(void)
 {
 	usb_deregister(&uvc_driver.driver);
+#ifdef CONFIG_DEBUG_FS
 	uvc_debugfs_cleanup();
+#endif
 }
 
 module_init(uvc_init);
