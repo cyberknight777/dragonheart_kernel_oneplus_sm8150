8e39fd5cc203a2986dd53ddf82690f26eb9f1e34
John Galt <johngaltfirstrun@gmail.com>
2022-01-18 11:48:31 -0500

    power/supply/qcom/smb5-lib: improve f65c61262615
    With a fun caveat
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -7981,8 +7981,8 @@ static void set_usb_switch(struct smb_charger *chg, bool enable)
 
 	if (chg->pd_active) {
 		pr_info("%s:pd_active return\n", __func__);
-		if (chg->typec_mode == POWER_SUPPLY_TYPEC_SOURCE_HIGH ||
-				chg->typec_mode == POWER_SUPPLY_TYPEC_SOURCE_MEDIUM)
+		//Will work with any accessories that don't use high
+		if (chg->typec_mode == POWER_SUPPLY_TYPEC_SOURCE_HIGH)
 			chg->disconnect_pd = true;
 		return;
 	}
