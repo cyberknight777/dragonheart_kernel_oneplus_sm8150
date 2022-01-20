817ba0b08189f468bb66574488a0d01dc336d8b1
Cyber Knight <cyberknight755@gmail.com>
2022-01-20 15:34:09 +0800

    kprofiles: Introduce KPROFILES_HELPER
    Based on https://github.com/0ctobot/neutrino_kernel_oneplus_sm8150/commit/92d07930a99e06930ea43f3aaa1a1e09e634915b:
    
    This is a simplified driver made to assist in injecting sepolicy for the sysfs nodes of kprofiles avoiding the need for root or a magisk module. This is achieved by temporarily disabling selinux for a split second for the userspace function to be executed.
    
    Co-authored-by: celtare21 <celtare21@gmail.com>
    Co-authored-by: Adam W. Willis <return.of.octobot@gmail.com>
    [@cyberknight777: Adapt and refactor vbswap_helper as a dedicated kprofiles_helper]
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

---
+++ drivers/misc/kprofiles_helper.c
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * KProfiles configuration helper based on userland_worker
+ *
+ * Copyright (C) 2020 Vlad Adumitroaie <celtare21@gmail.com>.
+ *                    Adam W. Willis <return.of.octobot@gmail.com>
+ *                    Cyber Knight <cyberknight755@gmail.com>
+ */
+
+#define pr_fmt(fmt) "kprofiles_helper: " fmt
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kmod.h>
+#include <linux/slab.h>
+#include <linux/string.h>
+#include <linux/security.h>
+#include <linux/delay.h>
+
+#include "../security/selinux/include/security.h"
+
+#define INITIAL_SIZE 4
+#define MAX_CHAR 128
+#define DELAY 500
+
+static char** argv;
+
+static struct delayed_work userland_work;
+
+static void free_memory(char** argv, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		kfree(argv[i]);
+	kfree(argv);
+}
+
+static char** alloc_memory(int size)
+{
+	char** argv;
+	int i;
+
+	argv = kmalloc(size * sizeof(char*), GFP_KERNEL);
+	if (!argv)
+		return NULL;
+
+	for (i = 0; i < size; i++) {
+		argv[i] = kmalloc(MAX_CHAR * sizeof(char), GFP_KERNEL);
+		if (!argv[i]) {
+			kfree(argv);
+			return NULL;
+		}
+	}
+
+	return argv;
+}
+
+static int call_userland(char** argv)
+{
+	static char* envp[] = {
+		"SHELL=/bin/sh",
+		"HOME=/",
+		"USER=shell",
+		"TERM=xterm-256color",
+		"PATH=/product/bin:/apex/com.android.runtime/bin:/apex/com.android.art/bin:/system_ext/bin:/system/bin:/system/xbin:/odm/bin:/vendor/bin:/vendor/xbin",
+		"DISPLAY=:0",
+		NULL
+	};
+
+	return call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
+}
+
+static inline int nix_sh(const char* cmd)
+{
+  int ret;
+
+  strcpy(argv[0], "/system/bin/sh");
+  strcpy(argv[1], "-c");
+  strcpy(argv[2], cmd);
+  argv[3] = NULL;
+  
+  ret = call_userland(argv);
+  if (!ret)
+    pr_info("%s executed successfully!", cmd);
+  else
+    pr_err("%s failed to execute! %d", cmd, ret);
+  
+  return ret;
+}
+
+static void kprofiles_helper(void)
+{
+
+  nix_sh("/system/xbin/sepolicy-inject -s system_app -t sysfs -c file -p read,write,open,getattr -P /vendor/etc/selinux/precompiled_sepolicy -l");
+  free_memory(argv, INITIAL_SIZE);
+}
+
+static void kprofiles_init(struct work_struct *work)
+{
+	bool is_enforcing;
+
+	argv = alloc_memory(INITIAL_SIZE);
+	if (!argv) {
+		pr_err("Couldn't allocate memory!");
+		return;
+	}
+
+	is_enforcing = get_enforce_value();
+	if (is_enforcing) {
+		pr_info("Setting selinux state: permissive");
+		set_selinux(0);
+	}
+
+	kprofiles_helper();
+
+	if (is_enforcing) {
+		pr_info("Setting selinux state: enforcing");
+		set_selinux(1);
+	}
+}
+
+static int __init kprofiles_helper_entry(void)
+{
+	INIT_DELAYED_WORK(&userland_work, kprofiles_init);
+	queue_delayed_work(system_power_efficient_wq,
+			&userland_work, DELAY);
+
+	return 0;
+}
+
+module_init(kprofiles_helper_entry);
