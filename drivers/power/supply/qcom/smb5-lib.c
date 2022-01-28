8a4e23205dd141c7d38f9deb2d709bfa78d0821c
John Galt <johngaltfirstrun@gmail.com>
2022-01-28 09:52:59 -0500

    power/supply/qcom/smb5-lib: properly fix pd accessory behavior
    Only apply fix on data devices which need it.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -8095,7 +8095,10 @@ static void set_usb_switch(struct smb_charger *chg, bool enable)
 
 	if (chg->pd_active) {
 		pr_info("%s:pd_active return\n", __func__);
-		return;
+		if (chg->typec_mode == POWER_SUPPLY_TYPEC_SINK ||
+				chg->typec_mode == POWER_SUPPLY_TYPEC_SINK_DEBUG_ACCESSORY ||
+				chg->typec_mode == POWER_SUPPLY_TYPEC_SINK_AUDIO_ADAPTER)
+			return;
 	}
 
 	if (enable) {
