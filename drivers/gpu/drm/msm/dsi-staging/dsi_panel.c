25a4b750dbbcb33cedd21ec914f99e9fafea65db
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 16:37:05 +0200

    display: dsi_panel: expose attribute to notify of fod changes
    Change-Id: I6bdb767567c09e1a4cbd3b2d626e1173baf9089f
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1192,6 +1192,18 @@ static int dsi_panel_pwm_register(struct dsi_panel *panel)
 	return 0;
 }
 
+bool dsi_panel_get_fod_ui(struct dsi_panel *panel)
+{
+	return panel->fod_ui;
+}
+
+void dsi_panel_set_fod_ui(struct dsi_panel *panel, bool status)
+{
+	panel->fod_ui = status;
+
+	sysfs_notify(&panel->parent->kobj, NULL, "fod_ui");
+}
+
 static int dsi_panel_bl_register(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -4237,7 +4249,24 @@ error:
 	return ERR_PTR(rc);
 }
 
+static ssize_t sysfs_fod_ui_read(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct dsi_display *display = dev_get_drvdata(dev);
+	struct dsi_panel *panel = display->panel;
+	bool status;
+
+	mutex_lock(&panel->panel_lock);
+	status = panel->fod_ui;
+	mutex_unlock(&panel->panel_lock);
+
+	return snprintf(buf, PAGE_SIZE, "%u\n", status);
+}
+
+static DEVICE_ATTR(fod_ui, 0444, sysfs_fod_ui_read, NULL);
+
 static struct attribute *panel_attrs[] = {
+	&dev_attr_fod_ui.attr,
 	NULL,
 };
 
