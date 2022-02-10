f94b0250b17eb194bb0f31b6fc7a3574ad437edb
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 16:37:05 +0200

    display: dsi_panel: expose attribute to notify of fod changes
    Change-Id: I6bdb767567c09e1a4cbd3b2d626e1173baf9089f
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -316,6 +316,7 @@ struct dsi_panel {
 	unsigned int fod_dim_lut_len;
 	u8 fod_dim_alpha;
 	bool fod_hbm_enabled;
+	bool fod_ui;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -463,5 +464,7 @@ int dsi_panel_set_customer_p3_mode(struct dsi_panel *panel, int level);
 u8 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
+bool dsi_panel_get_fod_ui(struct dsi_panel *panel);
+void dsi_panel_set_fod_ui(struct dsi_panel *panel, bool status);
 
 #endif /* _DSI_PANEL_H_ */
