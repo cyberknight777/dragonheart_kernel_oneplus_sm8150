80e4b994541923891604b140d7a51b99c6e7da9a
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-03-26 02:58:44 +0530

    zswap: Make use of movable memory
    - Recent commits to z3fold made it capable of supporting highmem and
    movable pages, we had only reverted this commit because at the time
    only zsmalloc had support for it but that is not the case anymore.
    
    This reverts commit ce5cce4adce11.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- mm/zswap.c
+++ mm/zswap.c
@@ -445,7 +445,7 @@ static struct zswap_pool *zswap_pool_create(char *type, char *compressor)
 {
 	struct zswap_pool *pool;
 	char name[38]; /* 'zswap' + 32 char (max) num + \0 */
-	gfp_t gfp = __GFP_NORETRY | __GFP_NOWARN | __GFP_KSWAPD_RECLAIM | __GFP_CMA;
+	gfp_t gfp = __GFP_NORETRY | __GFP_NOWARN | __GFP_KSWAPD_RECLAIM | __GFP_HIGHMEM | __GFP_MOVABLE | __GFP_CMA;
 	int ret;
 
 	if (!zswap_has_pool) {
@@ -776,6 +776,7 @@ static int zswap_frontswap_store(unsigned type, pgoff_t offset,
 	unsigned long handle, value;
 	char *buf;
 	u8 *src, *dst;
+	gfp_t gfp = __GFP_NORETRY | __GFP_NOWARN | __GFP_KSWAPD_RECLAIM | __GFP_HIGHMEM | __GFP_MOVABLE | __GFP_CMA;
 
 	/* THP isn't supported */
 	if (PageTransHuge(page)) {
@@ -832,8 +833,7 @@ static int zswap_frontswap_store(unsigned type, pgoff_t offset,
 	if (dlen > PAGE_SIZE)
 		dlen = PAGE_SIZE;
 	ret = zpool_malloc(entry->pool->zpool, dlen,
-			   __GFP_NORETRY | __GFP_NOWARN | __GFP_KSWAPD_RECLAIM | __GFP_CMA,
-			   &handle);
+			   gfp, &handle);
 	if (ret == -ENOSPC) {
 		zswap_reject_compress_poor++;
 		goto put_dstmem;
