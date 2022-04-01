6052d4af9b86d5519d63108eb237f2ce386afb90
John Galt <johngaltfirstrun@gmail.com>
2022-04-01 05:49:45 -0400

    esoc: dont panic on BOOT_FAIL_ACTION_PANIC
    Present on oos10 for sdx50, needed to avoid crash on mdm interface
    service run at boot
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/esoc/esoc-mdm-drv.c
+++ drivers/esoc/esoc-mdm-drv.c
@@ -417,10 +417,7 @@ static int mdm_handle_boot_fail(struct esoc_clink *esoc_clink, u8 *pon_trial)
 		break;
 	case BOOT_FAIL_ACTION_PANIC:
 		esoc_mdm_log("Calling panic!!\n");
-		if (get_second_board_absent() == 0 && oem_get_download_mode())
-		  panic("Panic requested on external modem boot failure\n");
-		else
-		  pr_err("Panic requested on external modem boot failure\n");
+		pr_err("Panic requested on external modem boot failure\n");
 		break;
 	case BOOT_FAIL_ACTION_NOP:
 		esoc_mdm_log("Leaving the modem in its curent state\n");
