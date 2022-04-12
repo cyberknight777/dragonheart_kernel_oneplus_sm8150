5f89c5771f98ff6e21776bd277566e5764a523e3
Cyber Knight <cyberknight755@gmail.com>
2022-04-13 01:00:55 +0800

    treewide: Suppress logspam
    - These logs are totally unnecessary on a production device.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/usb/gadget/function/f_accessory.c
+++ drivers/usb/gadget/function/f_accessory.c
@@ -753,7 +753,7 @@ static long acc_ioctl(struct file *fp, unsigned code, unsigned long value)
 
 static int acc_open(struct inode *ip, struct file *fp)
 {
-	printk(KERN_INFO "acc_open\n");
+	pr_debug_once("acc_open\n");
 	if (atomic_xchg(&_acc_dev->open_excl, 1))
 		return -EBUSY;
 
@@ -764,7 +764,7 @@ static int acc_open(struct inode *ip, struct file *fp)
 
 static int acc_release(struct inode *ip, struct file *fp)
 {
-	printk(KERN_INFO "acc_release\n");
+	pr_debug_once("acc_release\n");
 
 	WARN_ON(!atomic_xchg(&_acc_dev->open_excl, 0));
 	/* indicate that we are disconnected
