2164d996f774b2f735a9c03f8465136b202f32ec
Cyber Knight <cyberknight755@gmail.com>
2022-04-14 23:06:55 +0800

    kramel: Refactor `--upr()`
    - Use scripts/config to change localversion.
    - Use rgn() to regen.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kramel.sh
+++ kramel.sh
@@ -192,10 +192,8 @@ rgn() {
 }
 
 upr() {
-    make "${MAKE[@]}" $CONFIG
-    source "${KDIR}"/out/.config
-    sed -i "s#${CONFIG_LOCALVERSION}#-DragonHeart-${1}#" out/.config
-    cp -rf "${KDIR}"/out/.config "${KDIR}"/arch/arm64/configs/$CONFIG
+    "${KDIR}"/scripts/config --file "${KDIR}"/arch/arm64/configs/$CONFIG --set-str CONFIG_LOCALVERSION "-DragonHeart-${1}"
+    rgn
 if [ "${ci}" != 1 ];then
     git add arch/arm64/configs/$CONFIG
     git commit -S -s -m "dragonheart_defconfig: Bump to \`${1}\`"
