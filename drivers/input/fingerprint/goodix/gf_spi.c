bd1a4c93207a4f05484f3476021c4fc9497d3f4f
John Galt <johngaltfirstrun@gmail.com>
2022-04-17 08:43:55 -0400

    goodix: Wakeup and suspend optimization
    - Improves FOD wakeup.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -260,7 +260,7 @@ static inline int irq_setup(struct gf_dev *gf_dev)
 	int status;
 	gf_dev->irq = gpio_to_irq(gf_dev->irq_gpio);
 	status = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
-			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
+			IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_NO_SUSPEND | IRQF_FORCE_RESUME,
 			"gf", gf_dev);
 
 	if (status) {
