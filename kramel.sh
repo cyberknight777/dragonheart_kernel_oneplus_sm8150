d2c0ed89132ab4aa9d6bea8d4648866f4cd89f78
Cyber Knight <cyberknight755@gmail.com>
2022-05-12 22:24:30 +0800

    kramel: Do personal builds when `PERSONAL=1`
    - To control how personal builds are made.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kramel.sh
+++ kramel.sh
@@ -40,7 +40,13 @@ export COMMIT_HASH
 
 # Telegram Information. Set 1 to enable. | Set 0 to disable.
 export TGI=1
-export CHATID=-1001361882613
+
+# Personal Builds. Set 1 to enable. | Set 0 to disable.
+if [ "${PERSONAL}" = 1 ]; then
+    export CHATID=607425846
+else
+    export CHATID=-1001361882613
+fi
 
 # Necessary variables to be exported.
 export ci
