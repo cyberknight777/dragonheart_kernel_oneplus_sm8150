895abc7184026aec58d61ca0e02d1b3b2e4373d7
John Galt <johngaltfirstrun@gmail.com>
2022-05-18 17:23:53 -0400

    power/qcom/smb5-lib: further extend pd fix
     - Some devices change sink type after initial connect, with the only
       workaround as connecting/disconnecting until type is correct. Example
       would be Nissan android auto 2022 vehicles. However they are
       POWER_SUPPLY_TYPEC_SINK_POWERED_CABLE originally, and in various
       testing this is unused on any PD charge only device. Additionally, on
       a desktop PD data/charge hub, this allows earlier data detection.
    
     - Only log the early return if we're actually doing the early return.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -8094,11 +8094,13 @@ static void set_usb_switch(struct smb_charger *chg, bool enable)
 	}
 
 	if (chg->pd_active) {
-		pr_info("%s:pd_active return\n", __func__);
 		if (chg->typec_mode == POWER_SUPPLY_TYPEC_SINK ||
 				chg->typec_mode == POWER_SUPPLY_TYPEC_SINK_DEBUG_ACCESSORY ||
-				chg->typec_mode == POWER_SUPPLY_TYPEC_SINK_AUDIO_ADAPTER)
+				chg->typec_mode == POWER_SUPPLY_TYPEC_SINK_AUDIO_ADAPTER ||
+				chg->typec_mode == POWER_SUPPLY_TYPEC_SINK_POWERED_CABLE) {
+			pr_info("%s:OP FIXUP: pd_active return\n", __func__);
 			return;
+		}
 	}
 
 	if (enable) {
