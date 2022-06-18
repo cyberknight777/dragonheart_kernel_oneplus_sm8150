390264bb8ee0149d2ca73d10f1b1169783ccbb26
Cyber Knight <cyberknight755@gmail.com>
2022-06-18 09:02:53 +0800

    cpu_input_boost: Skip boosting when KProfiles is set to 1
    - We don't need boosting on mode 1 so let's skip it.
    
    Co-authored-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    [cyberknight777: adapt to latest impl of KProfiles]
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -156,9 +156,10 @@ static void update_online_cpu_policy(void)
 	put_online_cpus();
 }
 
+extern int kp_active_mode(void);
 static void __cpu_input_boost_kick(struct boost_drv *b)
 {
-	if (!test_bit(SCREEN_ON, &b->state))
+       if (!test_bit(SCREEN_ON, &b->state) || kp_active_mode() == 1)
 		return;
 
 	if (!input_boost_duration)
@@ -183,7 +184,7 @@ static void __cpu_input_boost_kick_max(struct boost_drv *b,
 	unsigned long boost_jiffies = msecs_to_jiffies(duration_ms);
 	unsigned long curr_expires, new_expires;
 
-	if (!test_bit(SCREEN_ON, &b->state))
+	if (!test_bit(SCREEN_ON, &b->state) || kp_active_mode() == 1)
 		return;
 
 	do {
