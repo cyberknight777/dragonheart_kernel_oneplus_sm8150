41d58c37ab9d827f382c2904b660bd1828632809
John Galt <johngaltfirstrun@gmail.com>
2022-10-16 14:29:30 -0400

    Partial Revert "sde_crtc: Use higher DC threshold"
    This reverts commit 7116f194ca5ba2557f875480df5ef20c3b572c32.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -5778,7 +5778,7 @@ static int _sde_crtc_check_secure_state(struct drm_crtc *crtc,
 	return 0;
 }
 
-int op_dimlayer_bl_alpha = 460;
+int op_dimlayer_bl_alpha = 320;
 int op_dimlayer_bl_enabled = 0;
 int op_dimlayer_bl_enable_real = 0;
 int op_dimlayer_bl = 0;
