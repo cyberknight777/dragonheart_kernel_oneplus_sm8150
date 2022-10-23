Neternels/android_kernel_xiaomi_sunny

init/userland_worker: Don't initialize if is_inline is set to true
- We setup is_libcam and vbswap from our device tree hence the userland worker has no relevant usecase in ROMs that inline the kernel.
- Therefore it is thoroughly useless and overheady to let the userland_worker do its work.
- Hence don't initialize it if is_inline is set to true.

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

init/userland_worker.c
@@ -6,6 +6,7 @@
  */
 
 #define pr_fmt(fmt) "userland_worker: " fmt
+#define nt_info(fmt, ...) printk(KERN_INFO "NetErnels: " fmt, ##__VA_ARGS__)
 
 #include <linux/module.h>
 #include <linux/init.h>
@@ -30,6 +31,8 @@ static struct delayed_work userland_work;
 
 unsigned int is_libcam;
 module_param(is_libcam, uint, 0644);
+
+extern bool is_inline;
 
 static void free_memory(char** argv, int size)
 {
@@ -159,6 +162,12 @@ static void userland_worker(struct work_struct *work)
 
 static int __init userland_worker_entry(void)
 {
+
+	if (is_inline) {
+                nt_info("Inline ROM detected! Killing UserLand Worker...\n");
+                return 0;
+        }
+
 	INIT_DELAYED_WORK(&userland_work, userland_worker);
 	queue_delayed_work(system_power_efficient_wq,
 			&userland_work, DELAY);

cyberknight777/dragonheart_kernel_oneplus_sm8150

init/userland_worker.c
@@ -5,6 +5,7 @@
  */
 
 #define pr_fmt(fmt) "userland_worker: " fmt
+#define nt_info(fmt, ...) printk(KERN_INFO "DragonHeart: " fmt, ##__VA_ARGS__)
 
 #include <linux/module.h>
 #include <linux/init.h>
@@ -29,6 +30,7 @@ static struct delayed_work userland_work;
 unsigned int is_a12;
 unsigned int fod_new = 1;
 module_param(fod_new, uint, 0644);
+extern bool is_inline;
 
 static void free_memory(char** argv, int size)
 {
@@ -217,6 +219,12 @@ static void userland_worker(struct work_struct *work)
 
 static int __init userland_worker_entry(void)
 {
+
+	if (is_inline) {
+                nt_info("Inline ROM detected! Killing UserLand Worker...\n");
+                return 0;
+        }
+
 	INIT_DELAYED_WORK(&userland_work, userland_worker);
 	queue_delayed_work(system_power_efficient_wq,
 			&userland_work, DELAY);