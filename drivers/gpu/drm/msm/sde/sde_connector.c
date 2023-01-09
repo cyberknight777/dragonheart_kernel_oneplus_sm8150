7a14eadf36f036452c172c09187d21675a61946e
John Galt <johngaltfirstrun@gmail.com>
2023-01-09 09:02:51 -0500

    sde_connector: improve 7T fod down timing
    Needed for both 60/90hz. This makes 90hz press and release flawless.
    60hz on release needs too long before dim is released, have done up to 5
    frames but better to have a minor flicker on 60hz release on 7T.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -656,8 +656,11 @@ static void sde_connector_pre_update_fod_hbm(struct sde_connector *c_conn)
 		finger_type = true;
 	}
 
-	dsi_panel_set_hbm_mode(panel, level);
+	if (panel->hw_type == DSI_PANEL_SAMSUNG_SOFEF03F_M && status)
+		sde_encoder_wait_for_event(c_conn->encoder,
+				MSM_ENC_VBLANK);
 
+	dsi_panel_set_hbm_mode(panel, level);
 	dsi_panel_set_fod_ui(panel, status);
 }
 
