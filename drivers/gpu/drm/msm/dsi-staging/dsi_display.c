89deb0379257f43ac34f297c328e5abeb9b4779c
John Galt <johngaltfirstrun@gmail.com>
2023-02-28 11:00:37 -0500

    dsi_display: lp2 (doze) interpolated brightness handling
    To save power on AOD when in doze. Interpolate values from a bl:aod_bl lut.
    
    Add a minor sleep before entering lp2 to workaround raciness of
    brightness change + lp2.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -1638,6 +1638,46 @@ static bool dsi_display_get_cont_splash_status(struct dsi_display *display)
 }
 extern int dsi_panel_set_aod_mode(struct dsi_panel *panel, int level);
 
+static u32 interpolate(uint32_t x, uint32_t xa, uint32_t xb,
+		uint32_t ya, uint32_t yb)
+{
+	return ya - (ya - yb) * (x - xa) / (xb - xa);
+}
+
+struct blbl {
+        u32 bl;
+        u32 aod_bl;
+};
+
+struct blbl aod_bl_lut[] = {
+	{0, 1},
+	{10, 1},
+	{40, 9},
+	{90, 30},
+	{120, 40},
+};
+
+u32 dsi_panel_get_aod_bl(struct dsi_display *display) {
+	int i;
+	//cached value is better than reading display->panel->bl_config.bl_level
+	u32 cur_bl = dsi_panel_backlight_get();
+
+	for (i = 0; i < 5; i++)
+                if (aod_bl_lut[i].bl >= cur_bl)
+                        break;
+        if (i == 0)
+                return aod_bl_lut[i].aod_bl;
+
+        if (i == 4)
+                return aod_bl_lut[i - 1].aod_bl;
+
+        return interpolate(cur_bl,
+                           aod_bl_lut[i - 1].bl,
+                           aod_bl_lut[i].bl,
+                           aod_bl_lut[i - 1].aod_bl,
+                           aod_bl_lut[i].aod_bl);
+}
+
 int dsi_display_set_power(struct drm_connector *connector,
 		int power_mode, void *disp)
 {
@@ -1671,6 +1711,8 @@ int dsi_display_set_power(struct drm_connector *connector,
 	break;
 	case SDE_MODE_DPMS_LP2:
 		pr_err("SDE_MODE_DPMS_LP2\n");
+		dsi_panel_set_backlight(display->panel, dsi_panel_get_aod_bl(display));
+		usleep_range(20000, 30000);
 		rc = dsi_panel_set_lp2(display->panel);
 		blank = MSM_DRM_BLANK_POWERDOWN_CUST;
 		notifier_data.data = &blank;
