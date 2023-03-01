5e833cada4620f731d41cdc9adef04429db4ab4c
John Galt <johngaltfirstrun@gmail.com>
2023-03-01 10:21:28 -0500

    sde: optimize fod drm event
    Also save on heavy checks at the end of commits.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -19,6 +19,7 @@
 #include "sde_encoder.h"
 #include <linux/backlight.h>
 #include <linux/string.h>
+#include <linux/msm_drm_notify.h>
 #include "dsi_drm.h"
 #include "dsi_display.h"
 #include "sde_crtc.h"
@@ -639,6 +640,8 @@ struct dsi_panel *sde_connector_panel(struct sde_connector *c_conn)
 static void sde_connector_pre_update_fod_hbm(struct sde_connector *c_conn)
 {
 	struct dsi_panel *panel;
+	struct msm_drm_notifier notifier_data;
+	int blank;
 	int level = 0;
 	bool status;
 
@@ -651,12 +654,15 @@ static void sde_connector_pre_update_fod_hbm(struct sde_connector *c_conn)
 		return;
 
 	if (status) {
+		blank = 1;
 		level = 5;
 		dsi_panel_set_nolp(panel);
 		if (panel->cur_mode->timing.refresh_rate < 90
 			|| panel->hw_type == DSI_PANEL_SAMSUNG_SOFEF03F_M)
 			sde_encoder_wait_for_event(c_conn->encoder,
 					MSM_ENC_VBLANK);
+	} else {
+		blank = 0;
 	}
 	dsi_panel_set_hbm_mode(panel, level);
 
@@ -667,6 +673,10 @@ static void sde_connector_pre_update_fod_hbm(struct sde_connector *c_conn)
 	dsi_panel_set_fod_ui(panel, status);
 	if (!status)
 		_sde_connector_update_bl_scale(c_conn);
+
+	notifier_data.data = &blank;
+	notifier_data.id = connector_state_crtc_index;
+	msm_drm_notifier_call_chain(MSM_DRM_ONSCREENFINGERPRINT_EVENT, &notifier_data);
 }
 
 int sde_connector_pre_kickoff(struct drm_connector *connector)
