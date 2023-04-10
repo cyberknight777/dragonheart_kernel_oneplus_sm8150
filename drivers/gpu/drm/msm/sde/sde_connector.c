0674b506b1bb9a51b92297db6ef9ac92530f8d2e
John Galt <johngaltfirstrun@gmail.com>
2023-04-10 11:45:58 -0400

    sde_connector/fod_hbm: cleanup 60hz + 7T handling
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -640,7 +640,6 @@ extern bool HBM_flag;
 static void sde_connector_pre_update_fod_hbm(struct sde_connector *c_conn)
 {
 	struct dsi_panel *panel;
-	u32 refresh_rate;
 	int level = 0;
 	bool status;
 
@@ -652,23 +651,21 @@ static void sde_connector_pre_update_fod_hbm(struct sde_connector *c_conn)
 	if (status == dsi_panel_get_fod_ui(panel))
 		return;
 
-	mutex_lock(&panel->panel_lock);
-	refresh_rate = panel->cur_mode->timing.refresh_rate;
-	mutex_unlock(&panel->panel_lock);
-
 	if (status) {
 		level = 5;
 		oneplus_dim_status = 5;
 		finger_type = true;
 		dsi_panel_set_nolp(panel);
+		if (panel->cur_mode->timing.refresh_rate < 90
+			|| panel->hw_type == DSI_PANEL_SAMSUNG_SOFEF03F_M)
+			sde_encoder_wait_for_event(c_conn->encoder,
+					MSM_ENC_VBLANK);
 	}
+	dsi_panel_set_hbm_mode(panel, level);
 
-	if (status && (panel->hw_type == DSI_PANEL_SAMSUNG_SOFEF03F_M ||
-		refresh_rate < 90))
+	if (!status && panel->cur_mode->timing.refresh_rate < 90)
 		sde_encoder_wait_for_event(c_conn->encoder,
 				MSM_ENC_VBLANK);
-
-	dsi_panel_set_hbm_mode(panel, level);
 
 	dsi_panel_set_fod_ui(panel, status);
 }
