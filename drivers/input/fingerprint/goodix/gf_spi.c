d393bfa56ccef401d245d5f0fa99e198ddcdd8fd
John Galt <johngaltfirstrun@gmail.com>
2023-04-25 16:19:23 -0400

    [bugfix] goodix/opticalfp_irq: hold wakelock before event notification
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -455,6 +455,7 @@ int __always_inline gf_opticalfp_irq_handler(int event)
 	if (gf.spi == NULL) {
 		return 0;
 	}
+	__pm_wakeup_event(&fp_wakelock, 10*HZ);
 	if (event == 1) {
 	        gf_dev->udfps_pressed = 1;
 		msg = GF_NET_EVENT_TP_TOUCHDOWN;
@@ -464,7 +465,6 @@ int __always_inline gf_opticalfp_irq_handler(int event)
 		msg = GF_NET_EVENT_TP_TOUCHUP;
 		sendnlmsg(&msg);
 	}
-	__pm_wakeup_event(&fp_wakelock, 10*HZ);
 
         sysfs_notify(&gf_dev->spi->dev.kobj, NULL, dev_attr_udfps_pressed.attr.name);
 
