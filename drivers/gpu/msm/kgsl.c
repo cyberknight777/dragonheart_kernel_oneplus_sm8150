6e5accdda0e517338d9cad55e9330d39f847c72b
Cyber Knight <cyberknight755@gmail.com>
2023-07-13 14:50:44 +0200

    kgsl: Fix incorrect pointer casting
    - This was caught with Clang 17.0.2.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4775,7 +4775,7 @@ kgsl_get_unmapped_area(struct file *file, unsigned long addr,
 			}
 
 			if (private->pid != current_pid) {
-				current_pid = private->pid;
+				current_pid = (uintptr_t)private->pid;
 				kgsl_send_uevent_notify(device, current->group_leader->comm,
 					len, mm->total_vm, largest_gap_cpu, largest_gap_gpu);
 			}
