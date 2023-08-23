177b0d05bb0ca0cf37d783e3fffef955537549f8
John Galt <johngaltfirstrun@gmail.com>
2023-08-23 14:12:06 -0400

    dsi_panel: set aod modes on lp2 and off on nolp
    D-IC supply command decreases power usage considerably on panels which
    support these commands (hotdogb and guacamoleb).
    
    There are only two supported commands with hardcoded brightnesses.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -5093,6 +5093,7 @@ exit:
 int dsi_panel_set_lp2(struct dsi_panel *panel)
 {
 	int rc = 0;
+	u32 cur_bl = dsi_panel_backlight_get();
 
 	if (!panel) {
 		pr_debug("invalid params\n");
@@ -5112,6 +5113,15 @@ int dsi_panel_set_lp2(struct dsi_panel *panel)
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_LP2);
 	if (rc)
 		pr_debug("[%s] failed to send DSI_CMD_SET_LP2 cmd, rc=%d\n",
+		       panel->name, rc);
+
+	if (cur_bl > 30)
+		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_ON_5);
+	else
+		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_ON_1);
+
+	if (rc)
+		pr_debug("[%s] failed to send DSI_CMD_SET_AOD_ON_5 cmd, rc=%d\n",
 		       panel->name, rc);
 
 	panel->need_power_on_backlight = true;
@@ -5144,6 +5154,11 @@ int dsi_panel_set_nolp(struct dsi_panel *panel)
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_NOLP);
 	if (rc)
 		pr_debug("[%s] failed to send DSI_CMD_SET_NOLP cmd, rc=%d\n",
+		       panel->name, rc);
+
+	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_OFF);
+	if (rc)
+		pr_debug("[%s] failed to send DSI_CMD_SET_AOD_OFF cmd, rc=%d\n",
 		       panel->name, rc);
 exit:
 	mutex_unlock(&panel->panel_lock);
