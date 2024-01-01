86ea01b511435bd9a3f717936c42b3259f8332a1
Tim Zimmermann <tim@linux4.de>
2024-01-01 08:54:37 +0100

    syscall: Fake uname to 4.19 also for netbpfload
    * This is required for U QPR2
    
    Change-Id: I0321c64f77fccf74ff2472c3abd29e8b6b4be1ce
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1189,6 +1189,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
 		strcpy(tmp.release, "4.19.0");
 		pr_debug("fake uname: %s/%d release=%s\n",
