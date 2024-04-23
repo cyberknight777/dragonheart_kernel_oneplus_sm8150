e078e02674bd2bc2b3377bf4ce3e26bd70353f2e
John Galt <johngaltfirstrun@gmail.com>
2024-04-23 09:39:51 -0400

    display: interpolated doze brightness for QHD panel
    QHD panel cannot do low power AOD modes without hitting a touchpanel
    bug and breaking gestures and input. Utilize this instead.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1145,8 +1145,12 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 
 	bl->real_bl_level = bl_lvl;
 
-	if (!panel->force_fod_dim_alpha)
-		panel->fod_dim_alpha = dsi_panel_calc_fod_dim_alpha(panel, bl_lvl);
+	if (!panel->force_fod_dim_alpha) {
+		if (panel->aod_state > 1)
+			panel->fod_dim_alpha = dsi_panel_calc_fod_dim_alpha(panel, panel->aod_state);
+		else
+			panel->fod_dim_alpha = dsi_panel_calc_fod_dim_alpha(panel, bl_lvl);
+	}
 
 	panel->dc_dim_alpha = dsi_panel_calc_dc_dim_alpha(panel, bl_lvl);
 
@@ -5053,6 +5057,39 @@ error:
 	return rc;
 }
 
+struct blbl {
+	u32 bl;
+	u32 aod_bl;
+};
+
+struct blbl aod_bl_lut[] = {
+	{0, 1},
+	{10, 1},
+	{40, 9},
+	{90, 30},
+	{120, 60},
+	{280, 100},
+};
+
+u32 dsi_panel_get_aod_bl(u32 cur_bl) {
+	int i;
+
+	for (i = 0; i < 6; i++)
+		if (aod_bl_lut[i].bl >= cur_bl)
+			break;
+	if (i == 0)
+		return aod_bl_lut[i].aod_bl;
+
+	if (i == 5)
+		return aod_bl_lut[i - 1].aod_bl;
+
+	return interpolate(cur_bl,
+		aod_bl_lut[i - 1].bl,
+		aod_bl_lut[i].bl,
+		aod_bl_lut[i - 1].aod_bl,
+		aod_bl_lut[i].aod_bl);
+}
+
 int dsi_panel_set_lp1(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -5085,17 +5122,21 @@ int dsi_panel_set_lp1(struct dsi_panel *panel)
 		       panel->name, rc);
 
 	if (!panel->aod_state) {
-		panel->aod_state = true;
-		if (cur_bl > 90)
-			rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_ON_5);
-		else
-			rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_ON_1);
+		if (panel->hw_type == DSI_PANEL_SAMSUNG_S6E3HC2) {
+			panel->aod_state = dsi_panel_get_aod_bl(cur_bl);
+			dsi_panel_set_backlight(panel, panel->aod_state);
+		} else {
+			panel->aod_state = 1;
+			if (cur_bl > 90)
+				rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_ON_5);
+			else
+				rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_ON_1);
 
-		if (rc)
-			pr_debug("[%s] failed to send DSI_CMD_SET_AOD_ON_5 cmd, rc=%d\n",
-			       panel->name, rc);
+			if (rc)
+				pr_debug("[%s] failed to send DSI_CMD_SET_AOD_ON_5 cmd, rc=%d\n",
+				       panel->name, rc);
+		}
 	}
-
 	panel->need_power_on_backlight = true;
 
 exit:
@@ -5129,17 +5170,21 @@ int dsi_panel_set_lp2(struct dsi_panel *panel)
 		       panel->name, rc);
 
 	if (!panel->aod_state) {
-		panel->aod_state = true;
-		if (cur_bl > 90)
-			rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_ON_5);
-		else
-			rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_ON_1);
+		if (panel->hw_type == DSI_PANEL_SAMSUNG_S6E3HC2) {
+			panel->aod_state = dsi_panel_get_aod_bl(cur_bl);
+			dsi_panel_set_backlight(panel, panel->aod_state);
+		} else {
+			panel->aod_state = 1;
+			if (cur_bl > 90)
+				rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_ON_5);
+			else
+				rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_ON_1);
 
-		if (rc)
-			pr_debug("[%s] failed to send DSI_CMD_SET_AOD_ON_5 cmd, rc=%d\n",
-			       panel->name, rc);
+			if (rc)
+				pr_debug("[%s] failed to send DSI_CMD_SET_AOD_ON_5 cmd, rc=%d\n",
+				       panel->name, rc);
+		}
 	}
-
 	panel->need_power_on_backlight = true;
 exit:
 	mutex_unlock(&panel->panel_lock);
@@ -5173,7 +5218,7 @@ int dsi_panel_set_nolp(struct dsi_panel *panel)
 		       panel->name, rc);
 
 	if (panel->aod_state) {
-		panel->aod_state = false;
+		panel->aod_state = 0;
 		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_AOD_OFF);
 		if (rc)
 			pr_debug("[%s] failed to send DSI_CMD_SET_AOD_OFF cmd, rc=%d\n",
--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -324,8 +324,8 @@ struct dsi_panel {
 	bool force_fod_dim_alpha;
 	bool dc_dim;
 	bool was_dc_dim;
-	bool aod_state;
 	bool p3_mode;
+	u32 aod_state;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
